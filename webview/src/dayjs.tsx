14773da6bbae0021fe8bcbcda802c970494424d1
RMP <rogermparent@gmail.com>
2020-11-13 00:03:51 -0500

    Add "isToday" to dayjs

--- webview/src/dayjs.tsx
+++ webview/src/dayjs.tsx
@@ -1,4 +1,6 @@
 import * as dayjs from "dayjs";
 import * as relativeTime from "dayjs/plugin/relativeTime";
+import * as isToday from "dayjs/plugin/isToday"
 dayjs.extend(relativeTime);
+dayjs.extend(isToday)
 export default dayjs;
