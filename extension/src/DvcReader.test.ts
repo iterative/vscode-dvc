19ecdf1bbe2c485c2c3182320320923743563478
rogermparent <rogermparent@gmail.com>
2021-01-29 16:01:09 -0500

    Remove tests that depend on demo submodule

--- extension/src/DvcReader.test.ts
+++ extension/src/DvcReader.test.ts
@@ -1,25 +1,12 @@
 import path from 'path'
 
-import { getExperiments, inferDefaultOptions } from './DvcReader'
+import { inferDefaultOptions } from './DvcReader'
 
 const extensionDirectory = path.resolve(__dirname, '..')
-const testDvcDirectory = path.resolve(
-  extensionDirectory,
-  '..',
-  'demo',
-  'example-get-started'
-)
-
-const testRepoOptions = inferDefaultOptions(testDvcDirectory)
 
 test('Inferring default options on a directory without .env', async () => {
   expect(await inferDefaultOptions(extensionDirectory)).toEqual({
     bin: 'dvc',
     cwd: extensionDirectory
   })
-})
-
-test('Comparing a table in the test repo to a snapshot', async () => {
-  const tableData = getExperiments(await testRepoOptions)
-  return expect(await tableData).toMatchSnapshot()
 })
