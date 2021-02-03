2c25740fb9f6a11ec642aceb69580441a0e65b23
rogermparent <rogermparent@gmail.com>
2021-02-03 11:47:27 -0500

    Add scripts

---
+++ virtualenv-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cd to demo and setup venv if needed
+source ./activate-demo-venv.sh
+
+# Ensure pip is upgraded to we can install tensorflow>=2
+pip install -U pip
+pip install -r ./requirements.txt
