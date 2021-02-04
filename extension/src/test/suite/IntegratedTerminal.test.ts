b8cbec6f893baa6905b0e01ca523a4070ffd2273
Matt Seddon <mattseddon@hotmail.com>
2021-02-05 08:36:45 +1100

    increase timeout of first test

--- extension/src/test/suite/IntegratedTerminal.test.ts
+++ extension/src/test/suite/IntegratedTerminal.test.ts
@@ -49,7 +49,7 @@ suite('Integrated Terminal Test Suite', () => {
       await waitForAndDispose(disposable)
 
       expect(eventCount).to.equal(1)
-    }).timeout(12000)
+    }).timeout(20000)
 
     it('should be able to run a command', async () => {
       const disposable = Disposable.fn()
