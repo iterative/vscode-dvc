b3267e03ab9fa025a145b483ea9369b7e5dda4be
Roger <rogermparent@gmail.com>
2021-02-11 12:49:09 -0500

    Add mock-based tests for getExperiments and inferDefaultOptions (#99)
    * Re-add mock-based tests for getExperiments and inferDefaultOptions
    
    This reverts commit e454c7c177b96d3a1b674906a64485473a6f73df.
    
    * Remove old janky parentheses notation in dvcreader test
    
    * Remove pattern change attempt to ignore dist mocks
    
    I don't think this actually did anything, and dist mocks were instead solved by
    simply not compiling them.
    
    * Make fs mock inline and add Jest types to DvcReader test
    
    * Remove manual mock for DvcReader
    
    * Use mockReturnValue over mockImplementationOnce

--- extension/src/DvcReader.test.ts
+++ extension/src/DvcReader.test.ts
@@ -1,12 +1,52 @@
 import path from 'path'
+import { mocked } from 'ts-jest/utils'
 
-import { inferDefaultOptions } from './DvcReader'
+import { inferDefaultOptions, getExperiments } from './DvcReader'
+import fs from 'fs'
+import { execPromise } from './util'
+import complexExperimentsOutput from 'dvc-vscode-webview/src/stories/complex-experiments-output.json'
+import { PromiseWithChild } from 'child_process'
+
+jest.mock('fs')
+jest.mock('./util')
+
+const mockedFs = mocked(fs)
+const mockedExecPromise = mocked(execPromise)
+
+mockedFs.accessSync.mockReturnValue()
 
 const extensionDirectory = path.resolve(__dirname, '..')
 
+test('Inferring default options on a directory with accessible .env', async () => {
+  expect(await inferDefaultOptions(extensionDirectory)).toEqual({
+    bin: path.join(extensionDirectory, '.env', 'bin', 'dvc'),
+    cwd: extensionDirectory
+  })
+})
+
 test('Inferring default options on a directory without .env', async () => {
+  mockedFs.accessSync.mockImplementationOnce(() => {
+    throw new Error('Mocked access fail')
+  })
+
   expect(await inferDefaultOptions(extensionDirectory)).toEqual({
     bin: 'dvc',
     cwd: extensionDirectory
   })
+})
+
+test('Command-mocked getExperiments matches a snapshot when parsed', async () => {
+  mockedExecPromise.mockReturnValue(
+    (Promise.resolve({
+      stdout: JSON.stringify(complexExperimentsOutput),
+      stderr: ''
+    }) as any) as PromiseWithChild<{ stdout: string; stderr: string }>
+  )
+
+  expect(
+    await getExperiments({
+      bin: 'dvc',
+      cwd: path.resolve()
+    })
+  ).toMatchSnapshot()
 })
