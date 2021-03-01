d2e4372473193d5df5a8e212225aa056be93556c
Matt Seddon <mattseddon@hotmail.com>
2021-03-01 13:46:42 +1100

    hoist exported function to top of file

--- extension/src/git.ts
+++ extension/src/git.ts
@@ -5,111 +5,17 @@ import { dirname } from 'path'
 
 const isWindows = process.platform === 'win32'
 
-const enum CharCode {
-  /**
-   * The `/` character.
-   */
-  Slash = 47,
-  /**
-   * The `\` character.
-   */
-  Backslash = 92,
-  A = 65,
-  Z = 90,
-  a = 97,
-  z = 122
-}
-
-const emptyStr = ''
-const driveLetterNormalizeRegex = /(?<=^\/?)([A-Z])(?=:\/)/
-const pathNormalizeRegex = /\\/g
-const pathStripTrailingSlashRegex = /\/$/g
-
-const normalizePath = (
-  fileName: string,
-  options: { addLeadingSlash?: boolean; stripTrailingSlash?: boolean } = {
-    stripTrailingSlash: true
-  }
-) => {
-  if (fileName == null || fileName.length === 0) {
-    return fileName
-  }
-
-  let normalized = fileName.replace(pathNormalizeRegex, '/')
-
-  const { addLeadingSlash, stripTrailingSlash } = {
-    stripTrailingSlash: true,
-    ...options
-  }
-
-  if (stripTrailingSlash) {
-    normalized = normalized.replace(pathStripTrailingSlashRegex, emptyStr)
-  }
-
-  if (addLeadingSlash && normalized.charCodeAt(0) !== CharCode.Slash) {
-    normalized = `/${normalized}`
-  }
-
-  if (isWindows) {
-    // Ensure that drive casing is normalized (lower case)
-    normalized = normalized.replace(
-      driveLetterNormalizeRegex,
-      (drive: string) => drive.toLowerCase()
-    )
-  }
-
-  return normalized
-}
-
-const rev_parse__show_toplevel = async (
-  cwd: string
-): Promise<string | undefined> => {
-  try {
-    const { stdout: data } = await execPromise(
-      'git rev-parse --show-toplevel',
-      {
-        cwd
-      }
-    )
-    // Make sure to normalize: https://github.com/git-for-windows/git/issues/2478
-    // Keep trailing spaces which are part of the directory name
-    return data.length === 0
-      ? undefined
-      : normalizePath(data.trimLeft().replace(/[\r|\n]+$/, ''))
-  } catch (ex) {
-    if (ex.code === 'ENOENT') {
-      // If the `cwd` doesn't exist, walk backward to see if any parent folder exists
-      let exists = await pathExists(cwd)
-      if (!exists) {
-        do {
-          const parent = dirname(cwd)
-          if (parent === cwd || parent.length === 0) {
-            return undefined
-          }
-
-          cwd = parent
-          exists = await pathExists(cwd)
-        } while (!exists)
-
-        return rev_parse__show_toplevel(cwd)
-      }
-    }
-    return undefined
-  }
-}
-
-const driveLetterRegex = /(?<=^\/?)([a-zA-Z])(?=:\/)/
-
 export const getRepoPathCore = async (
   dirPath: string
 ): Promise<string | undefined> => {
   let repoPath: string | undefined
   try {
-    repoPath = await rev_parse__show_toplevel(dirPath)
+    repoPath = await revParseShowToplevel(dirPath)
     if (repoPath == null) return repoPath
 
     if (isWindows) {
-      // On Git 2.25+ if you call `rev-parse --show-toplevel` on a mapped drive, instead of getting the mapped drive path back, you get the UNC path for the mapped drive.
+      // On Git 2.25+ if you call `rev-parse --show-toplevel` on a mapped drive,
+      // instead of getting the mapped drive path back, you get the UNC path for the mapped drive.
       // So try to normalize it back to the mapped drive path, if possible
 
       const repoUri = Uri.file(repoPath)
@@ -183,6 +89,101 @@ export const getRepoPathCore = async (
     }
   }
 }
+
+const revParseShowToplevel = async (
+  cwd: string
+): Promise<string | undefined> => {
+  try {
+    const { stdout: data } = await execPromise(
+      'git rev-parse --show-toplevel',
+      {
+        cwd
+      }
+    )
+    // Make sure to normalize: https://github.com/git-for-windows/git/issues/2478
+    // Keep trailing spaces which are part of the directory name
+    return data.length === 0
+      ? undefined
+      : normalizePath(data.trimLeft().replace(/[\r|\n]+$/, ''))
+  } catch (ex) {
+    if (ex.code === 'ENOENT') {
+      // If the `cwd` doesn't exist, walk backward to see if any parent folder exists
+      let exists = await pathExists(cwd)
+      if (!exists) {
+        do {
+          const parent = dirname(cwd)
+          if (parent === cwd || parent.length === 0) {
+            return undefined
+          }
+
+          cwd = parent
+          exists = await pathExists(cwd)
+        } while (!exists)
+
+        return revParseShowToplevel(cwd)
+      }
+    }
+    return undefined
+  }
+}
+
+const enum CharCode {
+  /**
+   * The `/` character.
+   */
+  Slash = 47,
+  /**
+   * The `\` character.
+   */
+  Backslash = 92,
+  A = 65,
+  Z = 90,
+  a = 97,
+  z = 122
+}
+
+const emptyStr = ''
+const driveLetterNormalizeRegex = /(?<=^\/?)([A-Z])(?=:\/)/
+const pathNormalizeRegex = /\\/g
+const pathStripTrailingSlashRegex = /\/$/g
+
+const normalizePath = (
+  fileName: string,
+  options: { addLeadingSlash?: boolean; stripTrailingSlash?: boolean } = {
+    stripTrailingSlash: true
+  }
+) => {
+  if (fileName == null || fileName.length === 0) {
+    return fileName
+  }
+
+  let normalized = fileName.replace(pathNormalizeRegex, '/')
+
+  const { addLeadingSlash, stripTrailingSlash } = {
+    stripTrailingSlash: true,
+    ...options
+  }
+
+  if (stripTrailingSlash) {
+    normalized = normalized.replace(pathStripTrailingSlashRegex, emptyStr)
+  }
+
+  if (addLeadingSlash && normalized.charCodeAt(0) !== CharCode.Slash) {
+    normalized = `/${normalized}`
+  }
+
+  if (isWindows) {
+    // Ensure that drive casing is normalized (lower case)
+    normalized = normalized.replace(
+      driveLetterNormalizeRegex,
+      (drive: string) => drive.toLowerCase()
+    )
+  }
+
+  return normalized
+}
+
+const driveLetterRegex = /(?<=^\/?)([a-zA-Z])(?=:\/)/
 
 const ensureProperWorkspaceCasing = (repoPath: string, filePath: string) => {
   filePath = filePath.replace(/\\/g, '/')
