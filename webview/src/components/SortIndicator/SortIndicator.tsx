4592cff3886365fe0b685469e18b9ebd7c5e92f4
Diwanshi Pandey <diwanshipandey@gmail.com>
2021-03-04 13:42:17 +0530

    (feat): breakdown SortMenu into multiple chihld components

--- webview/src/components/SortIndicator/SortIndicator.tsx
+++ webview/src/components/SortIndicator/SortIndicator.tsx
@@ -1,9 +1,15 @@
-import { ColumnInstance } from 'react-table'
 import React from 'react'
-import menuStyles from './SortMenu.scss'
-import { Experiment } from '../Experiments'
+import { ColumnInstance } from 'react-table'
 import { InstanceProp } from '../Table'
 import { isEmpty } from 'lodash'
+import {
+  SortMenuToggle,
+  SortMenu,
+  SortMenuItemGroup,
+  SortMenuItem,
+  SortMenuSeparator
+} from './SortMenu'
+import { Experiment } from '../../util/parse-experiments'
 
 const ColumnOptionsRow: React.FC<{
   column: ColumnInstance<Experiment>
@@ -32,26 +38,8 @@ const ColumnOptionsRow: React.FC<{
   </div>
 )
 
-const SelectedItem = (
-  <span className={menuStyles.sortMenu__menuItem__icon}>
-    <svg
-      fill="currentColor"
-      height="1em"
-      width="1em"
-      viewBox="0 0 512 512"
-      aria-hidden="true"
-      role="img"
-      style={{ verticalAlign: -0.125 + 'em' }}
-    >
-      <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
-    </svg>
-  </span>
-)
-
 const SortIndicator: React.FC<InstanceProp> = ({ instance }) => {
   const { allColumns } = instance
-  const sortMenuRef = React.useRef(null)
-  const [isClickedOutside, setClickedOutside] = React.useState(false)
   const [sortIndicatorState, setSortIndicatorState] = React.useState({
     isOpen: false,
     sortColumn: '',
@@ -59,171 +47,83 @@ const SortIndicator: React.FC<InstanceProp> = ({ instance }) => {
     hideColumn: false
   })
 
-  React.useEffect(() => {
-    if (isClickedOutside) {
-      setSortIndicatorState({
-        ...sortIndicatorState,
-        isOpen: false
-      })
-    }
-  }, [isClickedOutside])
+  const onToggle = (isOpen: any) => {
+    setSortIndicatorState({
+      ...sortIndicatorState,
+      isOpen: isOpen
+    })
+  }
 
-  // hook for handling a click that happens outside the SortIndicator component
-  React.useEffect(() => {
-    const outsideClickEventHandler = (e: any) => {
-      const target: any = sortMenuRef.current
-      if (target !== null && !target.contains(e.target)) {
-        setClickedOutside(true)
-      } else {
-        setClickedOutside(false)
-      }
-    }
+  const onSelectDirection = (event: any, direction: string) => {
+    setSortIndicatorState({
+      ...sortIndicatorState,
+      sortDirection: direction
+    })
+  }
 
-    if (sortIndicatorState.isOpen) {
-      document.addEventListener('click', outsideClickEventHandler)
-    }
+  const onSelectColumn = (event: any, column: string) => {
+    setSortIndicatorState({
+      ...sortIndicatorState,
+      sortColumn: column,
+      sortDirection: isEmpty(sortIndicatorState.sortColumn)
+        ? 'Asc'
+        : sortIndicatorState.sortDirection
+    })
+  }
 
-    return () => {
-      document.removeEventListener('click', outsideClickEventHandler)
-    }
-  }, [sortIndicatorState.isOpen])
+  const toggle = (
+    <SortMenuToggle onToggle={onToggle} toggleTemplate="Sort By" id="toggle" />
+  )
+
+  const directionGroup = (
+    <SortMenuItemGroup id="sort-direction" key="sort-direction-group">
+      <SortMenuItem
+        key="sort-direction-asc"
+        id="asc"
+        onSelect={onSelectDirection}
+        isSelected={'Asc' === sortIndicatorState.sortDirection}
+      >
+        Asc
+      </SortMenuItem>
+      <SortMenuItem
+        key="sort-direction-desc"
+        id="desc"
+        onSelect={onSelectDirection}
+        isSelected={'Desc' === sortIndicatorState.sortDirection}
+      >
+        Desc
+      </SortMenuItem>
+    </SortMenuItemGroup>
+  )
+
+  const columnGroup = (
+    <SortMenuItemGroup id="sort-column" key="sort-column-group">
+      {allColumns.map(column => {
+        return (
+          <SortMenuItem
+            id={column.id}
+            onSelect={onSelectColumn}
+            isSelected={column.Header === sortIndicatorState.sortColumn}
+            key={`sort-column-${column.id}`}
+          >
+            {column.Header}
+          </SortMenuItem>
+        )
+      })}
+    </SortMenuItemGroup>
+  )
+
+  const menuSeparator = <SortMenuSeparator key="sort-menu-separator" />
+
+  const menuItems = [columnGroup, menuSeparator, directionGroup]
 
   return (
-    <div className={menuStyles.sortMenu}>
-      <button
-        onClick={() => {
-          setSortIndicatorState({
-            ...sortIndicatorState,
-            isOpen: !sortIndicatorState.isOpen //Toggle
-          })
-        }}
-        className={menuStyles.sortMenu__toggle}
-      >
-        <span className={menuStyles.sortMenu__toggle__text}>Sort By</span>
-        <span className={menuStyles.sortMenu__toggle__icon}>
-          <svg
-            fill="currentColor"
-            height="1em"
-            width="1em"
-            viewBox="0 0 320 512"
-            aria-hidden="true"
-            role="img"
-            style={{ verticalAlign: -0.125 + 'em' }}
-          >
-            <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
-          </svg>
-        </span>
-      </button>
-      {sortIndicatorState.isOpen ? (
-        <div
-          ref={sortMenuRef}
-          className={menuStyles.sortMenu__menu}
-          role="menu"
-        >
-          <section className={menuStyles.sortMenu__group}>
-            <ul>
-              {allColumns.map(column => {
-                return (
-                  <li id={column.id} key={column.id} role="menuitem">
-                    <button
-                      key={`button-${column.id}`}
-                      className={menuStyles.sortMenu__menuItem}
-                      onClick={() => {
-                        setSortIndicatorState({
-                          ...sortIndicatorState,
-                          sortColumn: column.Header as string,
-                          sortDirection: isEmpty(sortIndicatorState.sortColumn)
-                            ? 'Asc'
-                            : sortIndicatorState.sortDirection
-                        })
-                      }}
-                    >
-                      {column.Header}
-                      {sortIndicatorState.sortColumn === column.Header
-                        ? SelectedItem
-                        : null}
-                    </button>
-                  </li>
-                )
-              })}
-            </ul>
-          </section>
-          <li role="separator"></li>
-          <section className={menuStyles.sortMenu__group}>
-            <ul>
-              <li role="menuitem">
-                <button
-                  className={menuStyles.sortMenu__menuItem}
-                  onClick={() => {
-                    setSortIndicatorState({
-                      ...sortIndicatorState,
-                      sortDirection: 'Asc'
-                    })
-                  }}
-                >
-                  Asc
-                  {sortIndicatorState.sortDirection === 'Asc'
-                    ? SelectedItem
-                    : null}
-                </button>
-              </li>
-              <li role="menuitem">
-                <button
-                  className={menuStyles.sortMenu__menuItem}
-                  onClick={() => {
-                    setSortIndicatorState({
-                      ...sortIndicatorState,
-                      sortDirection: 'Desc'
-                    })
-                  }}
-                >
-                  Desc
-                  {sortIndicatorState.sortDirection === 'Desc'
-                    ? SelectedItem
-                    : null}
-                </button>
-              </li>
-            </ul>
-          </section>
-          <li role="separator"></li>
-          <section className={menuStyles.sortMenu__group}>
-            <ul>
-              <li role="menuitem">
-                <button
-                  className={menuStyles.sortMenu__menuItem}
-                  onClick={() => {
-                    setSortIndicatorState({
-                      ...sortIndicatorState,
-                      hideColumn: true
-                    })
-                  }}
-                >
-                  Hide
-                  {sortIndicatorState.hideColumn ? SelectedItem : null}
-                </button>
-              </li>
-              <li role="menuitem">
-                <button
-                  className={menuStyles.sortMenu__menuItem}
-                  onClick={() => {
-                    setSortIndicatorState({
-                      ...sortIndicatorState,
-                      hideColumn: false
-                    })
-                  }}
-                >
-                  Show
-                  {sortIndicatorState.sortColumn &&
-                  !sortIndicatorState.hideColumn
-                    ? SelectedItem
-                    : null}
-                </button>
-              </li>
-            </ul>
-          </section>
-        </div>
-      ) : null}
-    </div>
+    <SortMenu
+      id="sort-menu"
+      menuItems={menuItems}
+      isOpen={sortIndicatorState.isOpen}
+      toggle={toggle}
+    />
   )
 }
 
--- webview/src/components/SortIndicator/SortMenu.tsx
+++ webview/src/components/SortIndicator/SortMenu.tsx
@@ -1,1 +1,157 @@
-// This file will contain the SortMenu as a separate component and moved out of the SortIdicator.tsx file
+import React from 'react'
+import menuStyles from './SortMenu.scss'
+import { useOutsideClickHook } from '../../util/useOutsideClickHook'
+
+export interface SortMenuToggleProps {
+  onToggle?: (isOpen: boolean) => void
+  isOpen?: boolean
+  toggleTemplate?: React.ReactNode
+  id: string
+}
+
+export interface SortMenuItemProps {
+  children?: React.ReactNode
+  isSelected?: boolean
+  isDisabled?: boolean
+  onSelect?: (
+    event?: React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent,
+    value?: any
+  ) => void
+  id?: string
+}
+
+export interface SortMenuProps {
+  menuItems: React.ReactNode[]
+  isOpen: boolean
+  toggle: React.ReactElement
+  id: string
+}
+
+export interface SortMenuItemGroupProps {
+  children?: React.ReactNode
+  id: string
+}
+
+export const SortMenuSeparator: React.FC = () => {
+  return <li role="separator"></li>
+}
+
+export const SortMenuToggle: React.FC<SortMenuToggleProps> = ({
+  isOpen,
+  onToggle,
+  id,
+  toggleTemplate
+}) => {
+  return (
+    <button
+      onClick={() => onToggle && onToggle(!isOpen)}
+      className={menuStyles.sortMenu__toggle}
+      id={id}
+    >
+      <span className={menuStyles.sortMenu__toggle__text}>
+        {toggleTemplate}
+      </span>
+      <span className={menuStyles.sortMenu__toggle__icon}>
+        <svg
+          fill="currentColor"
+          height="1em"
+          width="1em"
+          viewBox="0 0 320 512"
+          aria-hidden="true"
+          role="img"
+          style={{ verticalAlign: -0.125 + 'em' }}
+        >
+          <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+        </svg>
+      </span>
+    </button>
+  )
+}
+
+export const SortMenuItem: React.FC<SortMenuItemProps> = ({
+  children,
+  isSelected,
+  isDisabled,
+  onSelect,
+  id
+}) => {
+  return (
+    <li id={id} key={id} role="menuitem">
+      <button
+        key={`button-${id}`}
+        className={menuStyles.sortMenu__menuItem}
+        onClick={event => {
+          onSelect && onSelect(event, children)
+        }}
+        disabled={isDisabled}
+      >
+        {children}
+        {isSelected && (
+          <span className={menuStyles.sortMenu__menuItem__icon}>
+            <svg
+              fill="currentColor"
+              height="1em"
+              width="1em"
+              viewBox="0 0 512 512"
+              aria-hidden="true"
+              role="img"
+              style={{ verticalAlign: -0.125 + 'em' }}
+            >
+              <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
+            </svg>
+          </span>
+        )}
+      </button>
+    </li>
+  )
+}
+
+export const SortMenuItemGroup: React.FC<SortMenuItemGroupProps> = ({
+  children,
+  id
+}) => {
+  return (
+    <section className={menuStyles.sortMenu__group} id={id}>
+      <ul>{children}</ul>
+    </section>
+  )
+}
+
+export const SortMenu: React.FC<SortMenuProps> = ({
+  menuItems,
+  isOpen,
+  toggle,
+  id
+}) => {
+  const sortMenuRef = React.useRef(null)
+  let renderedMenuItems
+  let onClick
+  const [isActive, setIsActive] = useOutsideClickHook(sortMenuRef, false)
+
+  if (menuItems && menuItems.length) {
+    renderedMenuItems = menuItems
+  }
+
+  // we need to add this check to determine if setIsActive is a function or not othherwise we'll get type compatible call signature error.
+  if (typeof setIsActive === 'function' && !(setIsActive instanceof Array)) {
+    onClick = () => setIsActive(!isActive)
+  }
+
+  return (
+    <div className={menuStyles.sortMenu} id={id}>
+      {React.cloneElement(toggle, {
+        isOpen,
+        onToggle: onClick // this passes the current toggle status to SortToggle component
+      })}
+      <div
+        ref={sortMenuRef}
+        className={`${menuStyles.sortMenu__menu} ${
+          !isActive && !isOpen ? menuStyles.sortMenu__menu__inactive : ''
+        }`}
+        role="menu"
+      >
+        {renderedMenuItems}
+      </div>
+    </div>
+  )
+}
