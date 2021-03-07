92dcd38f3e586edff91d97fc4baec0a932d35a61
Diwanshi Pandey <diwanshipandey@gmail.com>
2021-03-07 13:38:49 +0530

    (chore): create a separate component for SortIconToggle

--- webview/src/components/Table/index.tsx
+++ webview/src/components/Table/index.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames'
 import { Experiment } from '../../util/parse-experiments'
 import styles from './module.scss'
 import { Menu, MenuToggle, MenuItemGroup, MenuItem } from '../Menu/index'
+import SortIconToggle from '../SortIconToggle'
 
 export interface InstanceProp {
   instance: TableInstance<Experiment>
@@ -124,34 +125,9 @@ export const PrimaryHeaderGroup: React.FC<{
                   )}
                 >
                   Sort Column
-                  <span>
-                    {header.isSorted &&
-                      (header.isSortedDesc ? (
-                        <svg
-                          fill="currentColor"
-                          height="1em"
-                          width="1em"
-                          viewBox="0 0 256 512"
-                          aria-hidden="true"
-                          role="img"
-                          style={{ verticalAlign: -0.125 + 'em' }}
-                        >
-                          <path d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path>
-                        </svg>
-                      ) : (
-                        <svg
-                          fill="currentColor"
-                          height="1em"
-                          width="1em"
-                          viewBox="0 0 256 512"
-                          aria-hidden="true"
-                          role="img"
-                          style={{ verticalAlign: -0.125 + 'em' }}
-                        >
-                          <path d="M88 166.059V468c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12V166.059h46.059c21.382 0 32.09-25.851 16.971-40.971l-86.059-86.059c-9.373-9.373-24.569-9.373-33.941 0l-86.059 86.059c-15.119 15.119-4.411 40.971 16.971 40.971H88z"></path>
-                        </svg>
-                      ))}
-                  </span>
+                  {header.isSorted && (
+                    <SortIconToggle isSortedDesc={header.isSortedDesc} />
+                  )}
                 </MenuItem>
                 <MenuItem
                   id={'Visibility'}
