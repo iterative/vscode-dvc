6b8b9721b263a1b07871c89b42579a4700beb3fc
Diwanshi Pandey <diwanshipandey@gmail.com>
2021-03-07 13:23:31 +0530

    (fix) converted activeElement comment into code

--- webview/src/util/useOutsideClickHook.tsx
+++ webview/src/util/useOutsideClickHook.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react'
+import React from 'react'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const useOutsideClickHook = (el: any, initialState: boolean) => {
-  const [isActive, setIsActive] = useState(initialState)
+  const [isActive, setIsActive] = React.useState(initialState)
 
-  useEffect(() => {
+  React.useEffect(() => {
     const onClick = (e: Event) => {
-      // If the active element exists and is clicked outside of the referenced component
-      if (el.current !== null && !el.current.contains(e.target)) {
+      const activeElement = el.current
+      if (activeElement && !activeElement.contains(e.target)) {
         setIsActive(!isActive)
       }
     }
