eed7cd4fb95f13b5eea4e35e404da09a88a3e887
Sean <itsmesean@pm.me>
2021-03-07 17:31:57 -0800

    Makes this change aimed solely at Windows for now

--- activate-demo-venv.sh
+++ activate-demo-venv.sh
@@ -4,23 +4,15 @@ DEMO_DIR="./demo"
 ENV_DIR=".env"
 cd "$DEMO_DIR" || exit
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
-        echo $OSTYPE
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        [[ -d "$ENV_DIR" ]] || python3 -m venv "$ENV_DIR"
-        source "$ENV_DIR/bin/activate"
-
-elif [[ "$OSTYPE" == "msys" ]]; then
+if [[ "$OSTYPE" == "msys" ]]; then
         # Windows
         [[ -d "$ENV_DIR" ]] || python -m venv "$ENV_DIR"
         source "$ENV_DIR/scripts/activate"
 
 else
-        # Unknown.
-        echo "unknown OS"
+        # All others
+        [[ -d "$ENV_DIR" ]] || python3 -m venv "$ENV_DIR"
+        source "$ENV_DIR/bin/activate"
 fi
 
 
