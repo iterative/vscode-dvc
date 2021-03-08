d7fa874241f84f552270c8b5a24ea984ac9241ff
Sean <itsmesean@pm.me>
2021-03-07 17:21:16 -0800

    Fix demo setup for Windows users

--- activate-demo-venv.sh
+++ activate-demo-venv.sh
@@ -2,9 +2,26 @@
 
 DEMO_DIR="./demo"
 ENV_DIR=".env"
-
 cd "$DEMO_DIR" || exit
 
-[[ -d "$ENV_DIR" ]] || python3 -m venv "$ENV_DIR"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+        echo $OSTYPE
 
-source "$ENV_DIR/bin/activate"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        [[ -d "$ENV_DIR" ]] || python3 -m venv "$ENV_DIR"
+        source "$ENV_DIR/bin/activate"
+
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Windows
+        [[ -d "$ENV_DIR" ]] || python -m venv "$ENV_DIR"
+        source "$ENV_DIR/scripts/activate"
+
+else
+        # Unknown.
+        echo "unknown OS"
+fi
+
+
+
