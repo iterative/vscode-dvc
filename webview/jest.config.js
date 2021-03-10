32bb683325d180a095aeb27ac5122f8a5f8fd204
Matt Seddon <mattseddon@hotmail.com>
2021-03-10 21:35:56 +1100

    use src instead of dist

--- webview/jest.config.js
+++ webview/jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
   collectCoverage: true,
   moduleNameMapper: {
     '\\.(scss|css|less)$': 'identity-obj-proxy',
-    '^dvc/common$': '<rootDir>/../extension/dist/common',
+    '^dvc/common$': '<rootDir>/../extension/src/common',
     '^dvc/experiments-contract$':
-      '<rootDir>/../extension/dist/webviews/experiments/contract'
+      '<rootDir>/../extension/src/webviews/experiments/contract'
   },
   globals: {
     __webpack_public_path__: true
