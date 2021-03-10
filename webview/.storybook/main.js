a0ba336bfaddca41db7215592f7588d214ac82af
Matt Seddon <mattseddon@hotmail.com>
2021-03-10 21:50:32 +1100

    add resolution to storybook

--- webview/.storybook/main.js
+++ webview/.storybook/main.js
@@ -1,3 +1,5 @@
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -13,5 +15,12 @@ module.exports = {
         }
       }
     }
-  ]
+  ],
+
+  webpackFinal: async config => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({ extensions: config.resolve.extensions })
+    ]
+    return config
+  }
 }
