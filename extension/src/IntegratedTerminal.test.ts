09e5dbdc2aaf707445e87b5b9d1d1f8ca4d5c05c
Sean <itsmesean@pm.me>
2021-03-15 00:00:20 -0700

    issues with tests

--- extension/src/IntegratedTerminal.test.ts
+++ extension/src/IntegratedTerminal.test.ts
@@ -1,10 +1,9 @@
 import {
   IntegratedTerminal,
   runExperiment,
-  initializeDirectory,
+  initialize,
   add,
-  checkout,
-  checkoutRecursive
+  checkout
 } from './IntegratedTerminal'
 import { resolve, relative } from 'path'
 
@@ -17,35 +16,7 @@ describe('runExperiment', () => {
     const undef = await runExperiment()
     expect(undef).toBeUndefined()
 
-    expect(terminalSpy).toBeCalledWith('dvc exp run ')
-  })
-})
-
-describe('initializeDirectory', () => {
-  it('should run the correct command in the IntegratedTerminal', async () => {
-    const terminalSpy = jest
-      .spyOn(IntegratedTerminal, 'run')
-      .mockResolvedValueOnce(undefined)
-
-    const undef = await initializeDirectory('./test/dir')
-    expect(undef).toBeUndefined()
-
-    expect(terminalSpy).toBeCalledWith('cd ./test/dir && dvc init --subdir ')
-  })
-})
-
-describe('add', () => {
-  it('should run the correct command in the IntegratedTerminal', async () => {
-    const terminalSpy = jest
-      .spyOn(IntegratedTerminal, 'run')
-      .mockResolvedValueOnce(undefined)
-
-    const path = resolve(__dirname, 'fileSystem.js')
-    const relPath = relative(resolve(__dirname, '..'), path)
-    const undef = await add(path)
-    expect(undef).toBeUndefined()
-
-    expect(terminalSpy).toBeCalledWith(`dvc add ${relPath}`)
+    expect(terminalSpy).toBeCalledWith('dvc exp run')
   })
 })
 
@@ -55,22 +26,9 @@ describe('checkout', () => {
       .spyOn(IntegratedTerminal, 'run')
       .mockResolvedValueOnce(undefined)
 
-    const undef = await checkout('../test/dir')
+    const undef = await checkout('/test/dir')
     expect(undef).toBeUndefined()
 
-    expect(terminalSpy).toBeCalledWith(`dvc checkout ../test/dir`)
-  })
-})
-
-describe('checkout recursive', () => {
-  it('should run the correct command in the IntegratedTerminal', async () => {
-    const terminalSpy = jest
-      .spyOn(IntegratedTerminal, 'run')
-      .mockResolvedValueOnce(undefined)
-
-    const undef = await checkoutRecursive('../test/dir')
-    expect(undef).toBeUndefined()
-
-    expect(terminalSpy).toBeCalledWith(`dvc checkout --recursive ../test/dir`)
+    expect(terminalSpy).toBeCalledWith(`cd test/dir && dvc checkout`)
   })
 })
