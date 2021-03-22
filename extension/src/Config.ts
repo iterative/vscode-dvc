862d1af582025da896badd8cff96aacb039cdde7
Roger <rogermparent@gmail.com>
2021-03-22 17:56:09 -0400

    Add EventEmitters to Config Class and convert existing manual updates (#194)
    This is done in a way that doesn't touch other files, and will probably need
    refactoring later

--- extension/src/Config.ts
+++ extension/src/Config.ts
@@ -1,6 +1,9 @@
 import {
   ColorTheme,
   ColorThemeKind,
+  ConfigurationChangeEvent,
+  EventEmitter,
+  Event,
   StatusBarItem,
   window,
   workspace
@@ -14,6 +17,9 @@ export class Config {
   public readonly dispose = Disposable.fn()
   public readonly workspaceRoot: string
   public dvcCliPath = 'dvc'
+
+  private onDidChangeEmitter: EventEmitter<ConfigurationChangeEvent>
+  readonly onDidChange: Event<ConfigurationChangeEvent>
 
   @observable
   private _vsCodeTheme: ColorTheme
@@ -107,15 +113,22 @@ export class Config {
       })
     )
 
+    this.onDidChangeEmitter = this.dispose.track(new EventEmitter())
+
+    this.onDidChange = this.onDidChangeEmitter.event
+
     this.dvcPathStatusBarItem = this.createDvcPathStatusBarItem()
     this.dispose.track(
       workspace.onDidChangeConfiguration(e => {
         if (e.affectsConfiguration('dvc.dvcPath')) {
-          this.updateDvcPathStatusBarItem()
-          this.setDvcCliPath()
+          this.onDidChangeEmitter.fire(e)
         }
       })
     )
+    this.dispose.track(
+      this.onDidChange(() => this.updateDvcPathStatusBarItem())
+    )
+    this.dispose.track(this.onDidChange(() => this.setDvcCliPath()))
     this.overrideStatusBar()
   }
 }
