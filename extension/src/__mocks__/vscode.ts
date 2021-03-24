e04e9546a691fee7c792c347d742ee5105279dd3
rogermparent <rogermparent@gmail.com>
2021-03-24 19:16:54 -0400

    Add extra implementation to vscode mock

--- extension/src/__mocks__/vscode.ts
+++ extension/src/__mocks__/vscode.ts
@@ -11,6 +11,51 @@ export const workspace = {
         fsPath: join(__dirname, '..', '..')
       }
     }
-  ]
+  ],
+  getConfiguration: jest.fn(),
+  onDidSaveTextDocument: jest.fn()
 }
-export const Uri = { file: jest.fn() }
+
+const Uri = {
+  file: jest.fn(),
+  parse: jest.fn()
+}
+
+const languages = {
+  createDiagnosticCollection: jest.fn()
+}
+
+const StatusBarAlignment = {}
+
+const OverviewRulerLane = {
+  Left: null
+}
+
+const Range = jest.fn()
+const Diagnostic = jest.fn()
+const DiagnosticSeverity = { Error: 0, Warning: 1, Information: 2, Hint: 3 }
+
+const debug = {
+  onDidTerminateDebugSession: jest.fn(),
+  startDebugging: jest.fn()
+}
+
+const commands = {
+  executeCommand: jest.fn()
+}
+
+const vscode = {
+  languages,
+  StatusBarAlignment,
+  window,
+  workspace,
+  OverviewRulerLane,
+  Uri,
+  Range,
+  Diagnostic,
+  DiagnosticSeverity,
+  debug,
+  commands
+}
+
+module.exports = vscode
