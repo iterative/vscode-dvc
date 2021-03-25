6dc58a433ddbb23df43ce83d0358705fbb5baa2c
rogermparent <rogermparent@gmail.com>
2021-03-25 19:42:03 -0400

    Roll getActivatedPythonExtension into getReadyPythonExtension

--- extension/src/extensions/python.ts
+++ extension/src/extensions/python.ts
@@ -14,20 +14,12 @@ export type PythonExtension = Extension<PythonExtensionAPI>
 export const getPythonExtension: () => PythonExtension | undefined = () =>
   extensions.getExtension('ms-python.python')
 
-export const getActivatedPythonExtension: () => Thenable<
+export const getReadyPythonExtension: () => Thenable<
   PythonExtension | undefined
 > = async () => {
   const extension = getPythonExtension()
   if (!extension) return extension
   if (!extension.isActive) await extension.activate()
-  return extension
-}
-
-export const getReadyPythonExtension: () => Thenable<
-  PythonExtension | undefined
-> = async () => {
-  const extension = await getActivatedPythonExtension()
-  if (!extension) return extension
   await extension.exports.ready
   return extension
 }
