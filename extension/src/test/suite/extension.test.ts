0177e83b04f05eced540423c3cbd775d7becc93a
Matt Seddon <mattseddon@hotmail.com>
2021-03-30 16:41:14 +1100

    add learning test for API (to be removed)

--- extension/src/test/suite/extension.test.ts
+++ extension/src/test/suite/extension.test.ts
@@ -1,13 +1,23 @@
-import { describe, it, before, beforeEach } from 'mocha'
+import { before, beforeEach, describe, it } from 'mocha'
 import chai from 'chai'
 import { stub, spy } from 'sinon'
 import sinonChai from 'sinon-chai'
-import { window, commands, workspace, Uri, extensions, Extension } from 'vscode'
+import { ensureFile, remove } from 'fs-extra'
+import {
+  window,
+  commands,
+  workspace,
+  Uri,
+  extensions,
+  Extension,
+  Event
+} from 'vscode'
 import { join, resolve } from 'path'
 import * as DvcReader from '../../cli/reader'
 import * as FileSystem from '../../fileSystem'
 import complexExperimentsOutput from '../../webviews/experiments/complex-output-example.json'
 import { ExperimentsWebview } from '../../webviews/experiments/ExperimentsWebview'
+import { delay } from '../../util'
 
 chai.use(sinonChai)
 const { expect } = chai
@@ -15,7 +25,9 @@ const { expect } = chai
 suite('Extension Test Suite', () => {
   window.showInformationMessage('Start all extension tests.')
 
-  before(() => {
+  const demoFolderLocation = resolve(__dirname, '..', '..', '..', '..', 'demo')
+
+  before(async () => {
     stub(DvcReader, 'listDvcOnlyRecursive').resolves([
       join('data', 'MNIST', 'raw', 't10k-images-idx3-ubyte'),
       join('data', 'MNIST', 'raw', 't10k-images-idx3-ubyte.gz'),
@@ -31,50 +43,9 @@ suite('Extension Test Suite', () => {
     ])
   })
 
-  const demoFolderLocation = resolve(__dirname, '..', '..', '..', '..', 'demo')
-
   beforeEach(async () => {
     await workspace.getConfiguration().update('dvc.dvcPath', undefined, false)
     return commands.executeCommand('workbench.action.closeAllEditors')
-  })
-
-  describe('git extension', () => {
-    it('should return a usable API', async () => {
-      interface API {
-        // readonly state: APIState
-        // readonly onDidChangeState: Event<APIState>
-        // readonly onDidPublish: Event<PublishEvent>
-        // readonly git: Git
-        // readonly repositories: Repository[]
-        // readonly onDidOpenRepository: Event<Repository>
-        // readonly onDidCloseRepository: Event<Repository>
-        toGitUri(uri: Uri, ref: string): Uri
-        // getRepository(uri: Uri): Repository | null
-        // init(root: Uri): Promise<Repository | null>
-        // openRepository(root: Uri): Promise<Repository | null>
-        // registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable
-        // registerCredentialsProvider(provider: CredentialsProvider): Disposable
-        // registerPushErrorHandler(handler: PushErrorHandler): Disposable
-      }
-
-      interface GitExtensionAPI {
-        getAPI(version: number): Thenable<API>
-      }
-
-      type GitExtension = Extension<GitExtensionAPI>
-      const gitExtension = extensions.getExtension('vscode.git') as GitExtension
-      expect(gitExtension).not.to.be.undefined
-      const api = await (await gitExtension.activate()).getAPI(1)
-
-      const gitUri = api.toGitUri(Uri.file(__filename), 'HEAD')
-      expect(gitUri).not.to.be.undefined
-
-      expect(gitUri.scheme).to.equal('git')
-
-      const query = JSON.parse(gitUri.query)
-      expect(query.path).to.equal(__filename)
-      expect(query.ref).to.equal('HEAD')
-    })
   })
 
   describe('dvc.showExperiments', () => {
@@ -207,5 +178,194 @@ suite('Extension Test Suite', () => {
       mockFindDvcRoots.restore()
       mockShowInputBox.restore()
     })
+  })
+
+  describe('git extension', () => {
+    it('should return a usable API', async () => {
+      const untrackedDir = join(demoFolderLocation, 'folder-with-stuff')
+      const untrackedFile = join(
+        demoFolderLocation,
+        'folder-with-stuff',
+        'text.txt'
+      )
+
+      await ensureFile(untrackedFile)
+      await delay(5000)
+
+      const enum Status {
+        INDEX_MODIFIED,
+        INDEX_ADDED,
+        INDEX_DELETED,
+        INDEX_RENAMED,
+        INDEX_COPIED,
+
+        MODIFIED,
+        DELETED,
+        UNTRACKED,
+        IGNORED,
+        INTENT_TO_ADD,
+
+        ADDED_BY_US,
+        ADDED_BY_THEM,
+        DELETED_BY_US,
+        DELETED_BY_THEM,
+        BOTH_ADDED,
+        BOTH_DELETED,
+        BOTH_MODIFIED
+      }
+
+      interface Change {
+        /**
+         * Returns either `originalUri` or `renameUri`, depending
+         * on whether this change is a rename change. When
+         * in doubt always use `uri` over the other two alternatives.
+         */
+        readonly uri: Uri
+        readonly originalUri: Uri
+        readonly renameUri: Uri | undefined
+        readonly status: Status
+      }
+
+      interface RepositoryState {
+        // readonly HEAD: Branch | undefined
+        // readonly refs: Ref[]
+        // readonly remotes: Remote[]
+        // readonly submodules: Submodule[]
+        // readonly rebaseCommit: Commit | undefined
+
+        readonly mergeChanges: Change[]
+        readonly indexChanges: Change[]
+        readonly workingTreeChanges: Change[]
+
+        readonly onDidChange: Event<void>
+      }
+
+      interface Repository {
+        // readonly rootUri: Uri
+        // readonly inputBox: InputBox
+        readonly state: RepositoryState
+        // readonly ui: RepositoryUIState
+
+        // getConfigs(): Promise<{ key: string; value: string }[]>
+        // getConfig(key: string): Promise<string>
+        // setConfig(key: string, value: string): Promise<string>
+        // getGlobalConfig(key: string): Promise<string>
+
+        // getObjectDetails(
+        //   treeish: string,
+        //   path: string
+        // ): Promise<{ mode: string; object: string; size: number }>
+        // detectObjectType(
+        //   object: string
+        // ): Promise<{ mimetype: string; encoding?: string }>
+        // buffer(ref: string, path: string): Promise<Buffer>
+        // show(ref: string, path: string): Promise<string>
+        // getCommit(ref: string): Promise<Commit>
+
+        // clean(paths: string[]): Promise<void>
+
+        // apply(patch: string, reverse?: boolean): Promise<void>
+        // diff(cached?: boolean): Promise<string>
+        // diffWithHEAD(): Promise<Change[]>
+        // diffWithHEAD(path: string): Promise<string>
+        // diffWith(ref: string): Promise<Change[]>
+        // diffWith(ref: string, path: string): Promise<string>
+        // diffIndexWithHEAD(): Promise<Change[]>
+        // diffIndexWithHEAD(path: string): Promise<string>
+        // diffIndexWith(ref: string): Promise<Change[]>
+        // diffIndexWith(ref: string, path: string): Promise<string>
+        // diffBlobs(object1: string, object2: string): Promise<string>
+        // diffBetween(ref1: string, ref2: string): Promise<Change[]>
+        // diffBetween(ref1: string, ref2: string, path: string): Promise<string>
+
+        // hashObject(data: string): Promise<string>
+
+        // createBranch(
+        //   name: string,
+        //   checkout: boolean,
+        //   ref?: string
+        // ): Promise<void>
+        // deleteBranch(name: string, force?: boolean): Promise<void>
+        // getBranch(name: string): Promise<Branch>
+        // getBranches(query: BranchQuery): Promise<Ref[]>
+        // setBranchUpstream(name: string, upstream: string): Promise<void>
+
+        // getMergeBase(ref1: string, ref2: string): Promise<string>
+
+        // status(): Promise<void>
+        // checkout(treeish: string): Promise<void>
+
+        // addRemote(name: string, url: string): Promise<void>
+        // removeRemote(name: string): Promise<void>
+        // renameRemote(name: string, newName: string): Promise<void>
+
+        // fetch(options?: FetchOptions): Promise<void>
+        // fetch(remote?: string, ref?: string, depth?: number): Promise<void>
+        // pull(unshallow?: boolean): Promise<void>
+        // push(
+        //   remoteName?: string,
+        //   branchName?: string,
+        //   setUpstream?: boolean,
+        //   force?: ForcePushMode
+        // ): Promise<void>
+
+        // blame(path: string): Promise<string>
+        // log(options?: LogOptions): Promise<Commit[]>
+
+        // commit(message: string, opts?: CommitOptions): Promise<void>
+      }
+
+      interface API {
+        // readonly state: APIState
+        // readonly onDidChangeState: Event<APIState>
+        // readonly onDidPublish: Event<PublishEvent>
+        // readonly git: Git
+        readonly repositories: Repository[]
+        // readonly onDidOpenRepository: Event<Repository>
+        // readonly onDidCloseRepository: Event<Repository>
+        toGitUri(uri: Uri, ref: string): Uri
+        // getRepository(uri: Uri): Repository | null
+        // init(root: Uri): Promise<Repository | null>
+        // openRepository(root: Uri): Promise<Repository | null>
+        // registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable
+        // registerCredentialsProvider(provider: CredentialsProvider): Disposable
+        // registerPushErrorHandler(handler: PushErrorHandler): Disposable
+      }
+
+      interface GitExtensionAPI {
+        getAPI(version: number): Thenable<API>
+      }
+
+      type GitExtension = Extension<GitExtensionAPI>
+      const gitExtension = extensions.getExtension('vscode.git') as GitExtension
+      expect(gitExtension).not.to.be.undefined
+      const api = await (await gitExtension.activate()).getAPI(1)
+
+      const gitUri = api.toGitUri(Uri.file(__filename), 'HEAD')
+      expect(gitUri).not.to.be.undefined
+
+      expect(gitUri.scheme).to.equal('git')
+
+      const query = JSON.parse(gitUri.query)
+      expect(query.path).to.equal(__filename)
+      expect(query.ref).to.equal('HEAD')
+
+      const repositories = api.repositories
+      const repository = repositories[0]
+
+      try {
+        remove(untrackedDir)
+      } catch {}
+
+      expect(repositories.length).to.equal(1)
+
+      const untrackedChanges = repository.state.workingTreeChanges.filter(
+        c => c.status === Status.UNTRACKED
+      )
+
+      expect(untrackedChanges).to.have.lengthOf.at.least(1)
+      expect(untrackedChanges.find(c => c.uri.fsPath === untrackedFile)).not.to
+        .be.undefined
+    }).timeout(6000)
   })
 })
