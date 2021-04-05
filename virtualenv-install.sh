c2f8835077616bbd0e1f61774eee4f077ea6753f
Matt Seddon <mattseddon@hotmail.com>
2021-04-05 18:06:53 +1000

    use python instead of python3

--- virtualenv-install.sh
+++ virtualenv-install.sh
@@ -3,5 +3,5 @@
 # cd to demo and setup venv if needed
 source ./activate-demo-venv.sh
 
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -U -r ./requirements.txt
