fbc8e7b2ff19d02e558a8292f73c1d1de350c719
Matt Seddon <mattseddon@hotmail.com>
2021-04-06 15:49:27 +1000

    pull out common function

--- extension/src/git.ts
+++ extension/src/git.ts
@@ -2,6 +2,9 @@ import { Uri } from 'vscode'
 import { extname, resolve } from 'path'
 import { execPromise, trimAndSplit } from './util'
 
+const getUris = (repositoryRoot: string, relativePaths: string[]) =>
+  relativePaths.map(path => Uri.file(resolve(repositoryRoot, path)))
+
 const getUntrackedDirectories = async (
   repositoryRoot: string
 ): Promise<Uri[]> => {
@@ -11,9 +14,10 @@ const getUntrackedDirectories = async (
       cwd: repositoryRoot
     }
   )
-  return trimAndSplit(stdout)
-    .filter(path => extname(path) === '')
-    .map(path => Uri.file(resolve(repositoryRoot, path)))
+  return getUris(
+    repositoryRoot,
+    trimAndSplit(stdout).filter(path => extname(path) === '')
+  )
 }
 
 const getUntrackedFiles = async (repositoryRoot: string): Promise<Uri[]> => {
@@ -23,9 +27,7 @@ const getUntrackedFiles = async (repositoryRoot: string): Promise<Uri[]> => {
       cwd: repositoryRoot
     }
   )
-  return trimAndSplit(stdout).map(path =>
-    Uri.file(resolve(repositoryRoot, path))
-  )
+  return getUris(repositoryRoot, trimAndSplit(stdout))
 }
 
 export const getAllUntracked = async (
