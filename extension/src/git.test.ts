ac72873b80c9fcaca3ee87a1c1876d36ab7d80c0
Matt Seddon <mattseddon@hotmail.com>
2021-04-06 10:37:05 +1000

    rely on git extension for getExperimentsRefsPath

--- extension/src/git.test.ts
+++ extension/src/git.test.ts
@@ -1,41 +1,6 @@
-import { getAllUntracked, getExperimentsRefsPath, getRepoRootPath } from './git'
-import { ensureDir, ensureFile, lstatSync, remove } from 'fs-extra'
+import { getAllUntracked } from './git'
+import { ensureFile, remove } from 'fs-extra'
 import { join, resolve } from 'path'
-
-describe('getExperimentsRefsPath', () => {
-  it('should find the path of the custom experiments refs given a directory in this project', async () => {
-    const refsPath = (await getExperimentsRefsPath(__dirname)) as string
-    expect(refsPath).toBeDefined()
-    const isValidPath = await ensureDir(refsPath)
-    const existing = undefined
-    const created = refsPath
-
-    expect([existing, created]).toContain(isValidPath)
-    expect(lstatSync(refsPath).isDirectory).toBeTruthy()
-  })
-
-  it('should return undefined given a non-existent path', async () => {
-    const refsPath = await getExperimentsRefsPath(
-      '/some/path/that/does/not/exist'
-    )
-    expect(refsPath).toBeUndefined()
-  })
-})
-
-describe('getRepoRootPath', () => {
-  it('should find the root directory given a directory in this project', async () => {
-    const gitRoot = (await getRepoRootPath(__dirname)) as string
-
-    expect(gitRoot).toBeDefined()
-    const gitDir = resolve(gitRoot, '.git')
-    expect(lstatSync(gitDir).isDirectory).toBeTruthy()
-  })
-
-  it('should return undefined given a non-existent path', async () => {
-    const gitRoot = await getRepoRootPath('/some/path/that/does/not/exist')
-    expect(gitRoot).toBeUndefined()
-  })
-})
 
 describe('getAllUntracked', () => {
   it('should return a list of all untracked paths', async () => {
