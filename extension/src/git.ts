3459cb85db0f715263230d76da81d4d551f588e9
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-04-07 01:20:14 +1000

    1/3 - Add untracked to scm view (#240)
    * split Git class
    
    * hook up new class to scm view
    
    * refactor onDidChange stack
    
    * clean variable names
    
    * hide implementation of subclass
    
    * add getAllUntracked git function
    
    * improve test
    
    * wire up git function to extension
    
    * ensure non-empty path
    
    * extend git test
    
    * wire up with config
    
    * remove unused properties from extension wrapper
    
    * load scm view before file decorations
    
    * remove export
    
    * rename internal class
    
    * remove unused Config properties
    
    * destructure array for variable
    
    * move gitExtension handling down
    
    * reword extension tests
    
    * move to onDidUntrackedChange
    
    * hide implementation again
    
    * rename variables
    
    * improve test
    
    * add isequal to dictionary
    
    * reinstate readonly property
    
    * get Uris from git
    
    * fix integration test
    
    * 2/3 - Spike source control view (dvc add functionality) (#236)
    
    * remove dvc commands from explorer/context
    
    * switch scm/resourceState/context command
    
    * update registered add command
    
    * pull out common function
    
    * return add to registered command
    
    * 3/3 - Remove unnecessary git functions (#242)
    
    * rely on git extension for getExperimentsRefsPath
    
    * import Uri

--- extension/src/git.ts
+++ extension/src/git.ts
@@ -1,236 +1,44 @@
-import { pathExists, realpath } from 'fs-extra'
+import { Uri } from 'vscode'
+import { extname, resolve } from 'path'
 import { execPromise } from './util'
-import { Uri, window } from 'vscode'
-import { dirname, resolve } from 'path'
-import { Logger } from './common/Logger'
 
-const enum CharCode {
-  /**
-   * The `/` character.
-   */
-  Slash = 47,
-  /**
-   * The `\` character.
-   */
-  Backslash = 92,
-  A = 65,
-  Z = 90,
-  a = 97,
-  z = 122
-}
-
-const isWindows = process.platform === 'win32'
-
-const normalizePath = (
-  fileName: string,
-  options: { addLeadingSlash?: boolean; stripTrailingSlash?: boolean } = {
-    stripTrailingSlash: true
-  }
-) => {
-  if (fileName == null || fileName.length === 0) {
-    return fileName
-  }
-
-  let normalized = fileName.replace(/\\/g, '/')
-
-  const { addLeadingSlash, stripTrailingSlash } = {
-    stripTrailingSlash: true,
-    ...options
-  }
-
-  if (stripTrailingSlash) {
-    normalized = normalized.replace(/\/$/g, '')
-  }
-
-  if (addLeadingSlash && normalized.charCodeAt(0) !== CharCode.Slash) {
-    normalized = `/${normalized}`
-  }
-
-  if (isWindows) {
-    // Ensure that drive casing is normalized (lower case)
-    const driveLetterNormalizeRegex = /(?<=^\/?)([A-Z])(?=:\/)/
-    normalized = normalized.replace(
-      driveLetterNormalizeRegex,
-      (drive: string) => drive.toLowerCase()
-    )
-  }
-
-  return normalized
-}
-
-const getWindowsRepoRootPath = async (
-  dirPath: string,
-  rootPath: string
-): Promise<string | undefined> => {
-  // On Git 2.25+ if you call `rev-parse --show-toplevel` on a mapped drive,
-  // instead of getting the mapped drive path back, you get the UNC path for the mapped drive.
-  // So try to normalize it back to the mapped drive path, if possible
-
-  const repoUri = Uri.file(rootPath)
-  const pathUri = Uri.file(dirPath)
-  if (repoUri.authority.length !== 0 && pathUri.authority.length === 0) {
-    const driveLetterRegex = /(?<=^\/?)([a-zA-Z])(?=:\/)/
-    const match = driveLetterRegex.exec(pathUri.path)
-    if (match != null) {
-      const [, letter] = match
-
-      try {
-        const networkPath = await new Promise<string | undefined>(resolve =>
-          realpath(`${letter}:\\`, { encoding: 'utf8' }, (err, resolvedPath) =>
-            resolve(err != null ? undefined : resolvedPath)
-          )
-        )
-        if (networkPath != null) {
-          rootPath = normalizePath(
-            repoUri.fsPath.replace(
-              networkPath,
-              `${letter.toLowerCase()}:${
-                networkPath.endsWith('\\') ? '\\' : ''
-              }`
-            )
-          )
-          return rootPath
-        }
-      } catch (e) {
-        Logger.error(e)
-      }
+const getUntrackedDirectories = async (
+  repositoryRoot: string
+): Promise<Uri[]> => {
+  const { stdout } = await execPromise(
+    'git ls-files --others --exclude-standard --directory --no-empty-directory',
+    {
+      cwd: repositoryRoot
     }
-
-    rootPath = normalizePath(pathUri.fsPath)
-  }
-
-  return rootPath
-}
-
-const getNonWindowsRepoRootPath = async (
-  dirPath: string,
-  rootPath?: string
-) => {
-  // If we are not on Windows (symlinks don't seem to have the same issue on Windows),
-  // check if we are a symlink and if so, use the symlink path (not its resolved path)
-  // This is because VS Code will provide document Uris using the symlinked path
-  rootPath = await new Promise<string | undefined>(resolve => {
-    realpath(dirPath, { encoding: 'utf8' }, (err, resolvedPath) => {
-      if (err != null) {
-        resolve(rootPath)
-        return
-      }
-
-      if (dirPath.toLowerCase() === resolvedPath.toLowerCase()) {
-        resolve(rootPath)
-        return
-      }
-
-      const linkPath = normalizePath(resolvedPath, {
-        stripTrailingSlash: true
-      })
-      rootPath = rootPath?.replace(linkPath, dirPath)
-      resolve(rootPath)
-    })
-  })
-
-  return rootPath
-}
-
-const showIncorrectWorkspaceCasingWarningMessage = async (): Promise<void> => {
-  await window.showWarningMessage(
-    'This workspace was opened with a different casing than what exists on disk. Please re-open this workspace with the exact casing as it exists on disk, otherwise you may experience issues with certain Git features, such as missing blame or history.'
   )
+  return stdout
+    .trim()
+    .split('\n')
+    .filter(path => path && extname(path) === '')
+    .map(path => Uri.file(resolve(repositoryRoot, path)))
 }
 
-const ensureProperWorkspaceCasing = (rootPath: string, filePath: string) => {
-  filePath = filePath.replace(/\\/g, '/')
-
-  let regexPath
-  let testPath
-  if (filePath > rootPath) {
-    regexPath = filePath
-    testPath = rootPath
-  } else {
-    testPath = filePath
-    regexPath = rootPath
-  }
-
-  let pathRegex = new RegExp(`^${regexPath}`)
-  if (!pathRegex.test(testPath)) {
-    pathRegex = new RegExp(pathRegex, 'i')
-    if (pathRegex.test(testPath)) {
-      return showIncorrectWorkspaceCasingWarningMessage()
+const getUntrackedFiles = async (repositoryRoot: string): Promise<Uri[]> => {
+  const { stdout } = await execPromise(
+    'git ls-files --others --exclude-standard',
+    {
+      cwd: repositoryRoot
     }
-  }
+  )
+  return stdout
+    .trim()
+    .split('\n')
+    .filter(path => path)
+    .map(path => Uri.file(resolve(repositoryRoot, path)))
 }
 
-const revParseShowToplevel = async (
-  cwd: string
-): Promise<string | undefined> => {
-  try {
-    const { stdout: data } = await execPromise(
-      'git rev-parse --show-toplevel',
-      { cwd }
-    )
-    // Make sure to normalize: https://github.com/git-for-windows/git/issues/2478
-    // Keep trailing spaces which are part of the directory name
-    return data.length === 0
-      ? undefined
-      : normalizePath(data.trimLeft().replace(/[\r|\n]+$/, ''))
-  } catch (ex) {
-    if (ex.code === 'ENOENT') {
-      // If the `cwd` doesn't exist, walk backward to see if any parent folder exists
-      let exists = await pathExists(cwd)
-      if (!exists) {
-        do {
-          const parent = dirname(cwd)
-          if (parent === cwd || parent.length === 0) {
-            return undefined
-          }
+export const getAllUntracked = async (
+  repositoryRoot: string
+): Promise<Uri[]> => {
+  const [files, dirs] = await Promise.all([
+    getUntrackedFiles(repositoryRoot),
 
-          cwd = parent
-          exists = await pathExists(cwd)
-        } while (!exists)
-
-        return revParseShowToplevel(cwd)
-      }
-    }
-    return undefined
-  }
-}
-
-// getRepoRootPath was originally adapted from GitService.getRepoPathCore in GitLens
-// project: https://github.com/eamodio/vscode-gitlens
-// Copyright (c) 2016-2021 Eric Amodio
-// License (MIT): https://github.com/eamodio/vscode-gitlens/blob/main/LICENSE
-export const getRepoRootPath = async (
-  dirPath: string
-): Promise<string | undefined> => {
-  let rootPath: string | undefined
-  try {
-    rootPath = await revParseShowToplevel(dirPath)
-    if (rootPath == null) {
-      return rootPath
-    }
-
-    if (isWindows) {
-      return getWindowsRepoRootPath(dirPath, rootPath)
-    }
-
-    return getNonWindowsRepoRootPath(dirPath, rootPath)
-  } catch (e) {
-    Logger.error(e)
-    rootPath = undefined
-    return rootPath
-  } finally {
-    if (rootPath) {
-      ensureProperWorkspaceCasing(rootPath, dirPath)
-    }
-  }
-}
-
-export const getExperimentsRefsPath = async (
-  dirPath: string
-): Promise<string | undefined> => {
-  const rootPath = await getRepoRootPath(dirPath)
-  if (!rootPath) {
-    return
-  }
-  return resolve(rootPath, '.git', 'refs', 'exps')
+    getUntrackedDirectories(repositoryRoot)
+  ])
+  return [...files, ...dirs]
 }
