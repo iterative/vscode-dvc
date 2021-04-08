383b3626e402de012cfff325018cb661e188cf80
Matt Seddon <mattseddon@hotmail.com>
2021-04-08 16:50:00 +1000

    refactor out functions

--- extension/src/cli/index.ts
+++ extension/src/cli/index.ts
@@ -25,59 +25,76 @@ enum Status {
   NOT_IN_CACHE = 'not in cache'
 }
 
-const reduceToPathStatuses = (
-  statusOutput: Record<
-    string,
-    (Record<string, Record<string, Status>> | string)[]
-  >,
-  dvcRoot: string
-): Partial<Record<Status, string[]>> => {
+type StatusOutput = Record<
+  string,
+  (Record<string, Record<string, Status>> | string)[]
+>
+
+type FilteredStatusOutput = Record<
+  string,
+  Record<string, Record<string, Status>>[]
+>
+
+const filterExcludedStagesOrFiles = (
+  statusOutput: StatusOutput
+): FilteredStatusOutput => {
   const excludeAlwaysChanged = (stageOrFile: string): boolean =>
     !statusOutput[stageOrFile].includes('always changed')
 
-  const getStatuses = (
-    status: Record<string, Record<string, Status>>[]
-  ): Record<string, Status>[] =>
-    status
-      .map(entry => entry?.['changed outs'] || entry?.['changed deps'])
-      .filter(value => value)
+  return Object.keys(statusOutput)
+    .filter(excludeAlwaysChanged)
+    .reduce((filteredStatusOutput, stageOrFile: string) => {
+      filteredStatusOutput[stageOrFile] = statusOutput[stageOrFile] as Record<
+        string,
+        Record<string, Status>
+      >[]
+      return filteredStatusOutput
+    }, {} as FilteredStatusOutput)
+}
 
-  const reduceStatuses = (
-    reducedStatus: Partial<Record<Status, string[]>>,
-    statuses: Record<string, Status>[]
-  ) =>
-    statuses.map(entry =>
-      Object.entries(entry).map(([relativePath, status]) => {
-        const absolutePath = join(dvcRoot, relativePath)
-        const existingPaths = reducedStatus[status] || []
-        reducedStatus[status] = [...new Set([...existingPaths, absolutePath])]
-      })
-    )
+const getFileOrStageStatuses = (
+  fileOrStage: Record<string, Record<string, Status>>[]
+): Record<string, Status>[] =>
+  fileOrStage
+    .map(entry => entry?.['changed outs'] || entry?.['changed deps'])
+    .filter(value => value)
 
+const reduceStatuses = (
+  reducedStatus: Partial<Record<Status, string[]>>,
+  statuses: Record<string, Status>[]
+) =>
+  statuses.map(entry =>
+    Object.entries(entry).map(([relativePath, status]) => {
+      const existingPaths = reducedStatus[status] || []
+      reducedStatus[status] = [...new Set([...existingPaths, relativePath])]
+    })
+  )
+
+const reduceToPathStatuses = (
+  filteredStatusOutput: FilteredStatusOutput
+): Partial<Record<Status, string[]>> => {
   const statusReducer = (
     reducedStatus: Partial<Record<Status, string[]>>,
-    stageOrFile: string
+    entry: Record<string, Record<string, Status>>[]
   ): Partial<Record<Status, string[]>> => {
-    const entry = statusOutput[stageOrFile] as Record<
-      string,
-      Record<string, Status>
-    >[]
-    const statuses = getStatuses(entry)
+    const statuses = getFileOrStageStatuses(entry)
 
     reduceStatuses(reducedStatus, statuses)
 
     return reducedStatus
   }
-  return Object.keys(statusOutput)
-    .filter(excludeAlwaysChanged)
-    .reduce(statusReducer, {})
+
+  return Object.values(filteredStatusOutput).reduce(statusReducer, {})
 }
 
 const getUriStatuses = (
-  pathStatuses: Partial<Record<Status, string[]>>
+  pathStatuses: Partial<Record<Status, string[]>>,
+  dvcRoot: string
 ): Partial<Record<Status, Uri[]>> => {
   return Object.entries(pathStatuses).reduce((uriStatuses, [status, paths]) => {
-    uriStatuses[status as Status] = paths?.map(path => Uri.file(path))
+    uriStatuses[status as Status] = paths?.map(path =>
+      Uri.file(join(dvcRoot, path))
+    )
     return uriStatuses
   }, {} as Partial<Record<Status, Uri[]>>)
 }
@@ -93,7 +110,8 @@ export const getStatus = async (options: {
     (Record<string, Record<string, Status>> | string)[]
   >
 
-  const pathStatuses = reduceToPathStatuses(statusOutput, dvcRoot)
+  const filteredStatusOutput = filterExcludedStagesOrFiles(statusOutput)
+  const pathStatuses = reduceToPathStatuses(filteredStatusOutput)
 
-  return getUriStatuses(pathStatuses)
+  return getUriStatuses(pathStatuses, dvcRoot)
 }
