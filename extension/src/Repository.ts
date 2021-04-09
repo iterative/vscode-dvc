5bd3d60319e292ce31237e17aa6824a08d5e46be
Matt Seddon <mattseddon@hotmail.com>
2021-04-09 12:52:12 +1000

    give up on repository level decoration for now

--- extension/src/Repository.ts
+++ extension/src/Repository.ts
@@ -1,17 +1,13 @@
 import { Config } from './Config'
 import { Disposable } from '@hediet/std/disposable'
-// import { Deferred } from '@hediet/std/synchronization'
-import { findDvcTrackedPaths } from './fileSystem'
 import { getAllUntracked } from './git'
 import { SourceControlManagement } from './views/SourceControlManagement'
-import { DecorationProvider } from './DecorationProvider'
 import { Status } from './Status'
 import { GitExtensionRepository } from './extensions/Git'
 
 export class Repository {
   private config: Config
   public readonly dispose = Disposable.fn()
-  private readonly decorationProvider: DecorationProvider
 
   constructor(
     config: Config,
@@ -19,14 +15,7 @@ export class Repository {
     gitExtensionRepository: GitExtensionRepository
   ) {
     this.config = config
-    this.decorationProvider = this.dispose.track(new DecorationProvider())
-    Promise.all([
-      findDvcTrackedPaths(dvcRoot, this.config.dvcPath),
-      getAllUntracked(dvcRoot)
-    ]).then(values => {
-      const [tracked, untracked] = values
-
-      this.decorationProvider.setTracked(tracked)
+    getAllUntracked(dvcRoot).then(untracked => {
       const status = this.dispose.track(new Status(this.config, dvcRoot))
       const scm = this.dispose.track(
         new SourceControlManagement(dvcRoot, untracked, status)
@@ -34,7 +23,6 @@ export class Repository {
 
       gitExtensionRepository.onDidUntrackedChange(async () => {
         const untrackedChanges = await getAllUntracked(dvcRoot)
-        this.decorationProvider.setTracked(tracked)
         return scm.updateUntracked(untrackedChanges)
       })
     })
