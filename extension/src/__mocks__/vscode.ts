fd3a9c1e48cf5a235be19cc3620a73c8e0994b2e
Matt Seddon <mattseddon@hotmail.com>
2021-04-11 12:13:24 +1000

    remove workspace implementation

--- extension/src/__mocks__/vscode.ts
+++ extension/src/__mocks__/vscode.ts
@@ -1,18 +1,8 @@
-import { join } from 'path'
-
 export const Extension = jest.fn()
 export const extensions = jest.fn()
 export const Terminal = jest.fn()
 export const window = jest.fn()
-export const workspace = {
-  workspaceFolders: [
-    {
-      uri: {
-        fsPath: join(__dirname, '..', '..')
-      }
-    }
-  ]
-}
+export const workspace = jest.fn()
 export const Uri = {
   file: jest.fn(),
   joinPath: jest.fn()
