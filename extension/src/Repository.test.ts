03f4b701ba6572d90edb27c891b0ba30645d6c66
Matt Seddon <mattseddon@hotmail.com>
2021-04-12 11:27:28 +1000

    add simple instantiation test

---
+++ extension/src/Repository.test.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { Config } from './Config'
+import { mocked } from 'ts-jest/utils'
+import { DecorationProvider } from './DecorationProvider'
+import { Repository } from './Repository'
+import { window } from 'vscode'
+
+jest.mock('./Config')
+
+const mockedWindow = mocked(window)
+mockedWindow.registerFileDecorationProvider = jest.fn()
+
+describe('Repository', () => {
+  it('should be able to be instantiated', async () => {
+    const config = new Config()
+    const dvcRoot = resolve(__dirname, '..', '..', 'demo')
+    const decorationProvider = new DecorationProvider()
+    const repository = new Repository(config, dvcRoot, decorationProvider)
+    expect(repository.ready).toBeDefined()
+  })
+})
