489c966e827f45e9182ecf3c81c2027f3e5b9f85
rogermparent <rogermparent@gmail.com>
2021-04-12 21:03:34 -0400

    Move individual test file into index test file

--- extension/src/cli/index.test.ts
+++ extension/src/cli/index.test.ts
@@ -1,13 +1,18 @@
+import { Config } from '../Config'
+import { queueExperimentCommand } from './index'
 import { mocked } from 'ts-jest/utils'
 import { execPromise } from '../util'
 import { basename, resolve } from 'path'
 import { add } from '.'
+import { window } from 'vscode'
 
 jest.mock('fs')
 jest.mock('../util')
 jest.mock('vscode')
 
 const mockedExecPromise = mocked(execPromise)
+const mockedShowErrorMessage = mocked(window.showErrorMessage)
+const mockedShowInformationMessage = mocked(window.showInformationMessage)
 
 beforeEach(() => {
   jest.resetAllMocks()
@@ -40,5 +45,26 @@ describe('add', () => {
     expect(mockedExecPromise).toBeCalledWith(`dvc add ${file}`, {
       cwd: dir
     })
+  })
+})
+
+describe('queueExperimentCommand', () => {
+  const exampleConfig = ({
+    dvcPath: 'dvc',
+    cwd: resolve()
+  } as unknown) as Config
+
+  test('it displays an info message with the contents of stdout when the command succeeds', async () => {
+    const stdout = 'Example stdout that will be resolved literally\n'
+    mockedExecPromise.mockResolvedValue({ stdout, stderr: '' })
+    await queueExperimentCommand(exampleConfig)
+    expect(mockedShowInformationMessage).toBeCalledWith(stdout)
+  })
+
+  test('it displays an error message with the contents of stderr when the command fails', async () => {
+    const stderr = 'Example stderr that will be resolved literally\n'
+    mockedExecPromise.mockRejectedValue({ stderr, stdout: '' })
+    await queueExperimentCommand(exampleConfig)
+    expect(mockedShowErrorMessage).toBeCalledWith(stderr)
   })
 })
