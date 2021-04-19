3dcd385054006691f3989c1afb5bb3604573e0fc
Matt Seddon <mattseddon@hotmail.com>
2021-04-19 19:16:55 +1000

    rename to options

--- extension/src/cli/reader.ts
+++ extension/src/cli/reader.ts
@@ -56,47 +56,45 @@ interface ExecutionOptions {
 }
 
 const executeProcess = async (
-  executionOptions: ExecutionOptions,
+  options: ExecutionOptions,
   command: Commands
 ): Promise<string> => {
-  const executionDetails = getExecutionDetails(executionOptions.config, command)
+  const executionDetails = getExecutionDetails(options.config, command)
   const { stdout } = await execPromise(executionDetails.command, {
-    cwd: executionOptions.cwd,
+    cwd: options.cwd,
     env: executionDetails.env
   })
   return stdout
 }
 
 const executeWithTrimAndSplit = async (
-  executionOptions: ExecutionOptions,
+  options: ExecutionOptions,
   command: Commands
 ): Promise<string[]> => {
-  const stdout = await executeProcess(executionOptions, command)
+  const stdout = await executeProcess(options, command)
   return trimAndSplit(stdout)
 }
 
 const executeAndParseJson = async <T>(
-  executionOptions: ExecutionOptions,
+  options: ExecutionOptions,
   command: Commands
 ): Promise<T> => {
-  const stdout = await executeProcess(executionOptions, command)
+  const stdout = await executeProcess(options, command)
   return JSON.parse(stdout)
 }
 
 export const listDvcOnlyRecursive = async (
-  executionOptions: ExecutionOptions
+  options: ExecutionOptions
 ): Promise<string[]> =>
-  executeWithTrimAndSplit(executionOptions, Commands.LIST_DVC_ONLY_RECURSIVE)
+  executeWithTrimAndSplit(options, Commands.LIST_DVC_ONLY_RECURSIVE)
 
 type Status = Record<
   string,
   (Record<string, Record<string, string>> | string)[]
 >
 
-export const status = async (
-  executionOptions: ExecutionOptions
-): Promise<Status> =>
-  executeAndParseJson<Status>(executionOptions, Commands.STATUS)
+export const status = async (options: ExecutionOptions): Promise<Status> =>
+  executeAndParseJson<Status>(options, Commands.STATUS)
 
 export const queueExperiment = async (
   options: ReaderOptions
