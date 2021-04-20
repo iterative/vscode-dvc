15eb68c8e66f4f3685320d5c01c9296502b7eaa8
Matt Seddon <mattseddon@hotmail.com>
2021-04-20 10:00:18 +1000

    pull out common code into function

--- extension/src/Repository.ts
+++ extension/src/Repository.ts
@@ -74,6 +74,14 @@ export class Repository {
   private decorationProvider?: DecorationProvider
   private sourceControlManagement: SourceControlManagement
 
+  private getCliExecutionOptions() {
+    return {
+      cliPath: this.config.dvcPath,
+      pythonBinPath: this.config.pythonBinPath,
+      cwd: this.dvcRoot
+    }
+  }
+
   private filterRootDir(dirs: string[] = []) {
     return dirs.filter(dir => dir !== this.dvcRoot)
   }
@@ -89,11 +97,8 @@ export class Repository {
   }
 
   public async updateTracked(): Promise<void> {
-    const tracked = await listDvcOnlyRecursive({
-      cliPath: this.config.dvcPath,
-      pythonBinPath: this.config.pythonBinPath,
-      cwd: this.dvcRoot
-    })
+    const options = this.getCliExecutionOptions()
+    const tracked = await listDvcOnlyRecursive(options)
     this.state.tracked = new Set([
       ...this.getAbsolutePath(tracked),
       ...this.getAbsoluteParentPath(tracked)
@@ -163,11 +168,11 @@ export class Repository {
   }
 
   private async getStatus(): Promise<Partial<Record<Status, Set<string>>>> {
-    const statusOutput = (await status({
-      cliPath: this.config.dvcPath,
-      pythonBinPath: this.config.pythonBinPath,
-      cwd: this.dvcRoot
-    })) as Record<string, (ValidStageOrFileStatuses | string)[]>
+    const options = this.getCliExecutionOptions()
+    const statusOutput = (await status(options)) as Record<
+      string,
+      (ValidStageOrFileStatuses | string)[]
+    >
 
     const filteredStatusOutput = this.filterExcludedStagesOrFiles(statusOutput)
     return this.reduceToPathStatuses(filteredStatusOutput)
