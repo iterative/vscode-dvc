ce8dabf967cd23ac02f309c22bfbb8aac865c20d
Matt Seddon <mattseddon@hotmail.com>
2021-04-28 14:19:07 +1000

    reword tests

--- extension/src/cli/execution.test.ts
+++ extension/src/cli/execution.test.ts
@@ -20,7 +20,7 @@ beforeEach(() => {
 })
 
 describe('createCliProcess', () => {
-  it('should pass the correct details to execa given no path to the cli or python binary path', async () => {
+  it('should pass the correct details to the underlying process given no path to the cli or python binary path', async () => {
     const existingPath = '/Users/robot/some/path:/Users/robot/yarn/path'
     const processEnv = { PATH: existingPath, SECRET_KEY: 'abc123' }
     const cwd = __dirname
@@ -44,7 +44,7 @@ describe('createCliProcess', () => {
     })
   })
 
-  it('should pass the correct details to execa given a path to the cli but no python binary path', async () => {
+  it('should pass the correct details to the underlying process given a path to the cli but no python binary path', async () => {
     const existingPath = '/do/not/need/a/path'
     const processEnv = { PATH: existingPath }
     const cliPath = '/some/path/to/dvc'
@@ -69,7 +69,7 @@ describe('createCliProcess', () => {
     })
   })
 
-  it('should pass the correct details to execa given a path to the cli, an existing PATH variable and a python binary path', async () => {
+  it('should pass the correct details to the underlying process given a path to the cli, an existing PATH variable and a python binary path', async () => {
     const existingPath =
       '/var/folders/q_/jpcf1bld2vz9fs5n62mgqshc0000gq/T/yarn--1618526061412-0.878957498634626:' +
       '/Users/robot/PP/vscode-dvc/extension/node_modules/.bin:' +
@@ -99,7 +99,7 @@ describe('createCliProcess', () => {
     })
   })
 
-  it('should pass a sane path to spawn if there is a python binary path but no existing PATH variable', async () => {
+  it('should pass a sane path to the underlying process if there is a python binary path but no existing PATH variable', async () => {
     const existingPath = ''
     mockedGetEnv.mockReturnValueOnce({ PATH: existingPath })
 
