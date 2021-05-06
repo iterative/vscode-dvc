ed31194a63f59ba007ab1659bf181d55ba2429ab
Roger <rogermparent@gmail.com>
2021-05-06 18:54:34 -0400

    Add `Experiments` class to Extension (#383)
    * Add class for extension-based exp show data storage
    
    * Refactor update
    
    * Add onFailedUpdate and rename ExperimentsManager
    
    * rename experimentsManager instance to experiments
    
    * Reorder emitters to be next to their events
    
    * Rename private field to data as well
    
    * Remove constructor update and dispose emitters
    
    * Stop awaiting config

---
+++ extension/src/experiments/index.ts
@@ -0,0 +1,65 @@
+import { EventEmitter } from 'vscode'
+import { experimentShow } from '../cli/reader'
+import { Config } from '../Config'
+import { ExperimentsRepoJSONOutput } from '../webviews/experiments/contract'
+
+export class Experiments {
+  private config: Config
+
+  private _currentUpdatePromise?: Thenable<ExperimentsRepoJSONOutput>
+
+  private _data?: ExperimentsRepoJSONOutput
+  public get data() {
+    return this._data
+  }
+
+  private onStartedUpdateEmitter: EventEmitter<
+    Thenable<ExperimentsRepoJSONOutput>
+  > = new EventEmitter()
+
+  public readonly onStartedUpdate = this.onStartedUpdateEmitter.event
+
+  private onDidUpdateEmitter: EventEmitter<
+    ExperimentsRepoJSONOutput
+  > = new EventEmitter()
+
+  public readonly onDidUpdate = this.onDidUpdateEmitter.event
+
+  private onFailedUpdateEmitter: EventEmitter<Error> = new EventEmitter()
+  public readonly onFailedUpdate = this.onFailedUpdateEmitter.event
+
+  public async update(): Promise<ExperimentsRepoJSONOutput> {
+    if (!this._currentUpdatePromise) {
+      try {
+        const updatePromise = experimentShow({
+          pythonBinPath: this.config.pythonBinPath,
+          cliPath: this.config.dvcPath,
+          cwd: this.config.workspaceRoot
+        })
+        this._currentUpdatePromise = updatePromise
+        this.onStartedUpdateEmitter.fire(updatePromise)
+        const experimentData = await updatePromise
+        this.onDidUpdateEmitter.fire(experimentData)
+        return experimentData
+      } catch (e) {
+        this.onFailedUpdateEmitter.fire(e)
+      } finally {
+        this._currentUpdatePromise = undefined
+      }
+    }
+    return this._currentUpdatePromise as Promise<ExperimentsRepoJSONOutput>
+  }
+
+  constructor(config: Config) {
+    if (!config) {
+      throw new Error('The Experiments class requires a Config instance!')
+    }
+    this.config = config
+  }
+
+  public dispose() {
+    this.onStartedUpdateEmitter.dispose()
+    this.onDidUpdateEmitter.dispose()
+    this.onFailedUpdateEmitter.dispose()
+  }
+}
