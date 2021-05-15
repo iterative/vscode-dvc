c7bc6bbb592861edd0a3b9339a12a0cd7f4094c4
Matt Seddon <mattseddon@hotmail.com>
2021-05-15 20:53:05 +1000

    fix config

--- extension/nyc.config.js
+++ extension/nyc.config.js
@@ -5,6 +5,5 @@ const defaultExclude = require('@istanbuljs/schema/default-exclude')
 module.exports = {
   extends: '@istanbuljs/nyc-config-typescript',
   extension: ['.ts', '.tsx'],
-  tempDir: './coverage/combined',
   exclude: [...defaultExclude, '**/.vscode-test/**', '**/test/**']
 }
