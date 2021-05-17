de5f45fc3f631bc73d859a7daedf9e1234ef82fe
Matt Seddon <mattseddon@hotmail.com>
2021-05-17 22:28:17 +1000

    try self instrumenting and delay activation

--- extension/src/test/suite/index.ts
+++ extension/src/test/suite/index.ts
@@ -1,9 +1,34 @@
-import { resolve as resolvePath } from 'path'
+/* eslint-disable sonarjs/cognitive-complexity */
+import { join, resolve as resolvePath } from 'path'
 import Mocha from 'mocha'
 import glob from 'glob'
 import { Logger } from '../../common/Logger'
 
-export function run(): Promise<void> {
+function setupNyc() {
+  const NYC = require('nyc')
+  const defaultExclude = require('@istanbuljs/schema/default-exclude')
+
+  // create an nyc instance, config here is the same as your package.json
+  const nyc = new NYC({
+    cache: false,
+    cwd: join(__dirname, '..', '..', '..'),
+    exclude: [...defaultExclude],
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true,
+    instrument: true,
+    reporter: ['text', 'html'],
+    sourceMap: true,
+    extensions: ['ts'],
+    tempDirectory: './coverage/integration'
+  })
+  nyc.reset()
+  nyc.wrap()
+  return nyc
+}
+
+export async function run() {
+  const nyc = setupNyc()
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -12,29 +37,35 @@ export function run(): Promise<void> {
 
   const testsRoot = resolvePath(__dirname, '..')
 
-  // eslint-disable-next-line sonarjs/cognitive-complexity
-  return new Promise((resolve, reject) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err)
-      }
+  try {
+    await new Promise<void>((resolve, reject) => {
+      glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+        if (err) {
+          return reject(err)
+        }
 
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(resolvePath(testsRoot, f)))
+        // Add files to the test suite
+        files.forEach(f => mocha.addFile(resolvePath(testsRoot, f)))
 
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`))
-          } else {
-            resolve()
-          }
-        })
-      } catch (e) {
-        Logger.error(e)
-        e(e)
-      }
+        try {
+          // Run the mocha test
+          mocha.run(failures => {
+            if (failures > 0) {
+              reject(new Error(`${failures} tests failed.`))
+            } else {
+              resolve()
+            }
+          })
+        } catch (e) {
+          Logger.error(e)
+          e(e)
+        }
+      })
     })
-  })
+  } finally {
+    if (nyc) {
+      nyc.writeCoverageFile()
+      nyc.report()
+    }
+  }
 }
