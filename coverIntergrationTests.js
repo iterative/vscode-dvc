8c5b02299430a51995b74d74333af830c720b4dc
Matt Seddon <mattseddon@hotmail.com>
2021-05-18 14:42:23 +1000

    use root script

---
+++ coverIntergrationTests.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const { resolve, join } = require('path')
+const { writeFileSync } = require('fs-extra')
+const execa = require('execa')
+
+let activationEvents = []
+
+const packageJsonPath = resolve(__dirname, 'extension', 'package.json')
+
+const cwd = join(__dirname, 'extension')
+
+const packageJson = require(packageJsonPath)
+activationEvents = packageJson.activationEvents
+packageJson.activationEvents = ['onStartupFinished']
+writeFileSync(packageJsonPath, JSON.stringify(packageJson))
+
+const tsc = execa('tsc', ['-p', './'], {
+  cwd
+})
+
+tsc.stdout.pipe(process.stdout)
+tsc.stderr.pipe(process.stderr)
+tsc.then(() => {
+  const tests = execa('node', [join(cwd, 'dist', 'test', 'runTest.js')], {
+    cwd
+  })
+
+  tests.stdout.pipe(process.stdout)
+  tests.stderr.pipe(process.stderr)
+  tests.then(() => {
+    packageJson.activationEvents = activationEvents
+    writeFileSync(packageJsonPath, JSON.stringify(packageJson))
+
+    const prettier = execa('prettier', ['--write', 'package.json'], { cwd })
+    prettier.stdout.pipe(process.stdout)
+    prettier.stderr.pipe(process.stderr)
+  })
+})
