e4c3c8eee6c53d8d73472b58b02a94c22f1ea2bd
Matt Seddon <mattseddon@hotmail.com>
2021-05-19 11:54:01 +1000

    add a wrapper test

---
+++ extension/src/vscode/quickPick.test.ts
@@ -0,0 +1,18 @@
+import { window } from 'vscode'
+import { mocked } from 'ts-jest/utils'
+import { quickPickSingle } from './quickPick'
+
+const mockedWindow = mocked(window)
+
+jest.mock('vscode')
+
+describe('quickPickSingle', () => {
+  it('should call window.quickPick with the correct arguments', async () => {
+    const placeHolder = 'my placeholder'
+    await quickPickSingle(['a', 'b', 'c'], placeHolder)
+    expect(mockedWindow.showQuickPick).toBeCalledWith(['a', 'b', 'c'], {
+      canPickMany: false,
+      placeHolder
+    })
+  })
+})
