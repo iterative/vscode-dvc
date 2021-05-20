b689152a50440309f7d644a7d74c9102f8547087
Matt Seddon <mattseddon@hotmail.com>
2021-05-20 11:20:57 +1000

    test getChildren

--- extension/src/fileSystem/views/TrackedExplorerTree.test.ts
+++ extension/src/fileSystem/views/TrackedExplorerTree.test.ts
@@ -4,25 +4,26 @@ import { mocked } from 'ts-jest/utils'
 import { Config } from '../../Config'
 import { TrackedExplorerTree } from './TrackedExplorerTree'
 import { join } from 'path'
+import { listDvcOnly } from '../../cli/reader'
 
 const mockedWorkspaceChanged = mocked(new EventEmitter<void>())
 const mockedWorkspaceChangedFire = jest.fn()
 mockedWorkspaceChanged.fire = mockedWorkspaceChangedFire
-
 const mockedTreeDataChanged = mocked(new EventEmitter<void>())
 const mockedTreeDataChangedFire = jest.fn()
 mockedTreeDataChanged.fire = mockedTreeDataChangedFire
-
 const mockedCommands = mocked(commands)
 mockedCommands.registerCommand = jest.fn()
 const mockedWindow = mocked(window)
 mockedWindow.registerTreeDataProvider = jest.fn()
+
 const mockedDisposable = mocked(Disposable)
+
 const mockedGetDefaultProject = jest.fn()
-const mockedGetExecutionOptions = jest.fn()
+const mockedGetCliPath = jest.fn()
 const mockedConfig = ({
   getDefaultProject: mockedGetDefaultProject,
-  getExecutionOptions: mockedGetExecutionOptions
+  getCliPath: mockedGetCliPath
 } as unknown) as Config
 
 jest.mock('vscode')
@@ -51,6 +52,39 @@ describe('TrackedTreeView', () => {
       trackedTreeView.initialize([dvcDemoPath])
 
       expect(mockedTreeDataChangedFire).toBeCalledTimes(1)
+    })
+  })
+
+  describe('getChildren', () => {
+    it('should get the root directory children when no element is provided', async () => {
+      const trackedTreeView = new TrackedExplorerTree(
+        mockedConfig,
+        mockedWorkspaceChanged,
+        mockedTreeDataChanged
+      )
+      const dvcDemoPath = join(__dirname, '..', '..', '..', 'demo')
+      trackedTreeView.initialize([dvcDemoPath])
+
+      const mockedListDvcOnly = mocked(listDvcOnly)
+      mockedListDvcOnly.mockResolvedValueOnce([
+        { isout: false, isdir: true, isexec: false, path: 'data' },
+        { isout: true, isdir: true, isexec: false, path: 'logs' },
+        { isout: true, isdir: false, isexec: false, path: 'model.pt' }
+      ])
+
+      const rootElements = await trackedTreeView.getChildren()
+      expect(rootElements).toEqual([
+        join(dvcDemoPath, 'data'),
+        join(dvcDemoPath, 'logs'),
+        join(dvcDemoPath, 'model.pt')
+      ])
+
+      mockedListDvcOnly.mockResolvedValueOnce([
+        { isout: false, isdir: true, isexec: false, path: 'MNIST' }
+      ])
+
+      const child = await trackedTreeView.getChildren(join(dvcDemoPath, 'data'))
+      expect(child).toEqual([join(dvcDemoPath, 'data', 'MNIST')])
     })
   })
 })
