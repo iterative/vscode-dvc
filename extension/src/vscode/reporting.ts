66f647bbc6963cf0ad0f99b3ce71759c4eb2f470
Matt Seddon <mattseddon@hotmail.com>
2021-05-24 10:41:48 +1000

    remove early return for exp run issues

--- extension/src/vscode/reporting.ts
+++ extension/src/vscode/reporting.ts
@@ -4,10 +4,6 @@ export const reportStderrOrThrow = (
   error: Error & { stdout?: string; stderr?: string }
 ) => {
   if (error.stderr) {
-    if (error.stderr.includes('dvc exp run')) {
-      return
-    }
-
     return window.showErrorMessage(error.stderr)
   }
   throw error
