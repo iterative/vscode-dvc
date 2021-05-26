ea705052826874a48001b0805397a77d9ed8bbd9
Matt Seddon <mattseddon@hotmail.com>
2021-05-26 15:30:29 +1000

    strip out more code

--- extension/src/extensions/git.ts
+++ extension/src/extensions/git.ts
@@ -1,60 +1,11 @@
-import { Event, Extension, extensions, Uri } from 'vscode'
-
-const enum GitStatus {
-  INDEX_MODIFIED,
-  INDEX_ADDED,
-  INDEX_DELETED,
-  INDEX_RENAMED,
-  INDEX_COPIED,
-
-  MODIFIED,
-  DELETED,
-  UNTRACKED,
-  IGNORED,
-  INTENT_TO_ADD,
-
-  ADDED_BY_US,
-  ADDED_BY_THEM,
-  DELETED_BY_US,
-  DELETED_BY_THEM,
-  BOTH_ADDED,
-  BOTH_DELETED,
-  BOTH_MODIFIED
-}
-
-interface Change {
-  /**
-   * Returns either `originalUri` or `renameUri`, depending
-   * on whether this change is a rename change. When
-   * in doubt always use `uri` over the other two alternatives.
-   */
-  readonly uri: Uri
-  readonly originalUri: Uri
-  readonly renameUri: Uri | undefined
-  readonly status: GitStatus
-}
-
-interface RepositoryState {
-  readonly mergeChanges: Change[]
-  readonly indexChanges: Change[]
-  readonly workingTreeChanges: Change[]
-
-  readonly onDidChange: Event<void>
-}
+import { Extension, extensions, Uri } from 'vscode'
 
 interface Repository {
   readonly rootUri: Uri
-  readonly state: RepositoryState
 }
 
-type APIState = 'uninitialized' | 'initialized'
-
 interface ExtensionAPI {
-  readonly state: APIState
-  readonly onDidChangeState: Event<APIState>
   readonly repositories: Repository[]
-
-  toGitUri(uri: Uri, ref: string): Uri
 }
 
 interface VscodeGit {
