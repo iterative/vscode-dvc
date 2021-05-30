ca58b6913db119df54fa13979202a519b356c1a7
Matt Seddon <mattseddon@hotmail.com>
2021-05-31 09:16:25 +1000

    remove original pullTarget function

--- extension/src/cli/executor.ts
+++ extension/src/cli/executor.ts
@@ -148,10 +148,6 @@ const runCliProcessOnTarget = async (
   return executeCliProcess({ cwd, cliPath, pythonBinPath }, ...args, target)
 }
 
-export const pullTarget = (
-  options: ExecutionOnTargetOptions
-): Promise<string> => runCliProcessOnTarget(options, Command.PULL)
-
 export const pushTarget = (
   options: ExecutionOnTargetOptions
 ): Promise<string> => runCliProcessOnTarget(options, Command.PUSH)
