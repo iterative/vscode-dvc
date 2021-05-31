a0933d2ec825c541defd485f5890177ef15987d5
Matt Seddon <mattseddon@hotmail.com>
2021-06-01 08:48:26 +1000

    do not export reportErrorMessage

--- extension/src/vscode/reporting.ts
+++ extension/src/vscode/reporting.ts
@@ -1,7 +1,7 @@
 import { window } from 'vscode'
 import { MaybeConsoleError } from '../cli/error'
 
-export const reportErrorMessage = (error: MaybeConsoleError) =>
+const reportErrorMessage = (error: MaybeConsoleError) =>
   window.showErrorMessage(error.stderr || error.message)
 
 export const report = async (stdout: Promise<string>) => {
