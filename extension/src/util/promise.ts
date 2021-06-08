1cf7ac1d6f1d80db2820e357649e5378e8d2451e
Matt Seddon <mattseddon@hotmail.com>
2021-06-08 12:47:50 +1000

    clean up types and name of function

--- extension/src/util/promise.ts
+++ extension/src/util/promise.ts
@@ -1,21 +1,23 @@
 import { delay } from './time'
 import { Logger } from '../common/logger'
 
-const resolve = (promiseOrPromises: Promise<unknown>[] | Promise<unknown>) => {
-  if (Array.isArray(promiseOrPromises)) {
-    return Promise.all(promiseOrPromises)
+const promiseOrPromiseAll = (
+  promiseOrPromises: Promise<unknown> | Promise<unknown>[]
+): Promise<unknown> | Promise<unknown[]> => {
+  if (!Array.isArray(promiseOrPromises)) {
+    return promiseOrPromises
   }
-  return promiseOrPromises
+  return Promise.all(promiseOrPromises)
 }
 
 export const retryUntilAllResolved = async <T>(
-  getNewPromiseOrPromises: () => Promise<unknown>[] | Promise<unknown>,
+  getNewPromiseOrPromises: () => Promise<unknown> | Promise<unknown>[],
   type: string,
   waitBeforeRetry = 500
 ): Promise<T> => {
   try {
     const promiseOrPromises = getNewPromiseOrPromises()
-    const data = await resolve(promiseOrPromises)
+    const data = await promiseOrPromiseAll(promiseOrPromises)
     return (data as unknown) as T
   } catch (e) {
     Logger.error(`${type} failed with ${e} retrying...`)
