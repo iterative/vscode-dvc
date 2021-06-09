4d6e86cae6999e3d7d91429b2fa121938b24c997
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-06-10 02:59:09 +1000

    move array util into array file (#543)

--- extension/src/util/array.test.ts
+++ extension/src/util/array.test.ts
@@ -1,4 +1,24 @@
-import { joinTruthyItems } from './array'
+import { definedAndNonEmpty, joinTruthyItems } from './array'
+
+describe('definedAndNonEmpty', () => {
+  it('should return true given a non empty array', () => {
+    const maybeArray = ['a', 'b']
+
+    expect(definedAndNonEmpty(maybeArray)).toBe(true)
+  })
+
+  it('should return false given an empty array', () => {
+    const maybeArray = [] as unknown[]
+
+    expect(definedAndNonEmpty(maybeArray)).toBe(false)
+  })
+
+  it('should return false given no array', () => {
+    const maybeArray = undefined
+
+    expect(definedAndNonEmpty(maybeArray)).toBe(false)
+  })
+})
 
 describe('joinTruthyItems', () => {
   it('should join the truthy items in the given array with the default separator', () => {
