48ff26bce78bbb29aedc3c5c65792e3a3846c969
rogermparent <rogermparent@gmail.com>
2021-06-08 21:09:04 -0400

    invert root check conditional so we run workspaceChanged on blank workspace.

--- extension/src/fileSystem/views/trackedExplorerTree.ts
+++ extension/src/fileSystem/views/trackedExplorerTree.ts
@@ -218,10 +218,10 @@ export class TrackedExplorerTree implements TreeDataProvider<string> {
     this.dispose.track(
       commands.registerCommand('dvc.init', async () => {
         const root = this.config.firstWorkspaceFolderRoot
-        if (!root) {
-          return undefined
+
+        if (root) {
+          await this.cliExecutor.init(root)
         }
-        await this.cliExecutor.init(root)
 
         workspaceChanged.fire()
       })
