36e87ae5e64937f076ad718bba0a62b3bab0a021
Matt Seddon <mattseddon@hotmail.com>
2021-06-11 19:13:59 +1000

    delete unused class property

--- extension/src/config.ts
+++ extension/src/config.ts
@@ -34,8 +34,6 @@ export class Config {
     return this.initialized
   }
 
-  public readonly firstWorkspaceFolderRoot?: string
-
   private readonly executionDetailsChanged: EventEmitter<
     void
   > = this.dispose.track(new EventEmitter())
