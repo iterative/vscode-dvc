caa28d5b7679c83984e46e57c1cf8a8517a7ff9c
Matt Seddon <mattseddon@hotmail.com>
2021-06-16 15:27:19 +1000

    remove return value from functions

--- extension/src/cli/executor.ts
+++ extension/src/cli/executor.ts
@@ -17,27 +17,27 @@ export class CliExecutor extends Cli {
     return this.executeProcess(cwd, command, Flag.FORCE, ...args)
   }
 
-  public addTarget(cwd: string, target: string): Promise<string> {
+  public addTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.ADD, target)
   }
 
-  public checkout(cwd: string): Promise<string> {
+  public checkout(cwd: string) {
     return this.executeProcess(cwd, Command.CHECKOUT)
   }
 
-  public checkoutTarget(cwd: string, target: string): Promise<string> {
+  public checkoutTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.CHECKOUT, target)
   }
 
-  public commit(cwd: string): Promise<string> {
+  public commit(cwd: string) {
     return this.executeProcess(cwd, Command.COMMIT)
   }
 
-  public commitTarget(cwd: string, target: string): Promise<string> {
+  public commitTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.COMMIT, target)
   }
 
-  public experimentApply(cwd: string, experimentName: string): Promise<string> {
+  public experimentApply(cwd: string, experimentName: string) {
     return this.executeExperimentProcess(
       cwd,
       ExperimentSubCommand.APPLY,
@@ -49,7 +49,7 @@ export class CliExecutor extends Cli {
     cwd: string,
     experimentName: string,
     branchName: string
-  ): Promise<string> {
+  ) {
     return this.executeExperimentProcess(
       cwd,
       ExperimentSubCommand.BRANCH,
@@ -61,7 +61,7 @@ export class CliExecutor extends Cli {
   public experimentGarbageCollect(
     cwd: string,
     preserveFlags: GcPreserveFlag[]
-  ): Promise<string> {
+  ) {
     return this.executeExperimentProcess(
       cwd,
       ExperimentSubCommand.GARBAGE_COLLECT,
@@ -71,10 +71,7 @@ export class CliExecutor extends Cli {
     )
   }
 
-  public experimentRemove(
-    cwd: string,
-    experimentName: string
-  ): Promise<string> {
+  public experimentRemove(cwd: string, experimentName: string) {
     return this.executeExperimentProcess(
       cwd,
       ExperimentSubCommand.REMOVE,
@@ -82,7 +79,7 @@ export class CliExecutor extends Cli {
     )
   }
 
-  public experimentRunQueue(cwd: string): Promise<string> {
+  public experimentRunQueue(cwd: string) {
     return this.executeExperimentProcess(
       cwd,
       ExperimentSubCommand.RUN,
@@ -90,51 +87,51 @@ export class CliExecutor extends Cli {
     )
   }
 
-  public forceCheckout(cwd: string): Promise<string> {
+  public forceCheckout(cwd: string) {
     return this.executeForcedProcess(cwd, Command.CHECKOUT)
   }
 
-  public forceCheckoutTarget(cwd: string, target: string): Promise<string> {
+  public forceCheckoutTarget(cwd: string, target: string) {
     return this.executeForcedProcess(cwd, Command.CHECKOUT, target)
   }
 
-  public forceCommit(cwd: string): Promise<string> {
+  public forceCommit(cwd: string) {
     return this.executeForcedProcess(cwd, Command.COMMIT)
   }
 
-  public forceCommitTarget(cwd: string, target: string): Promise<string> {
+  public forceCommitTarget(cwd: string, target: string) {
     return this.executeForcedProcess(cwd, Command.COMMIT, target)
   }
 
-  public forcePull(cwd: string): Promise<string> {
+  public forcePull(cwd: string) {
     return this.executeForcedProcess(cwd, Command.PULL)
   }
 
-  public forcePush(cwd: string): Promise<string> {
+  public forcePush(cwd: string) {
     return this.executeForcedProcess(cwd, Command.PUSH)
   }
 
-  public init(cwd: string): Promise<string> {
+  public init(cwd: string) {
     return this.executeProcess(cwd, Command.INITIALIZE, Flag.SUBDIRECTORY)
   }
 
-  public pull(cwd: string): Promise<string> {
+  public pull(cwd: string) {
     return this.executeProcess(cwd, Command.PULL)
   }
 
-  public pullTarget(cwd: string, target: string): Promise<string> {
+  public pullTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.PULL, target)
   }
 
-  public push(cwd: string): Promise<string> {
+  public push(cwd: string) {
     return this.executeProcess(cwd, Command.PUSH)
   }
 
-  public pushTarget(cwd: string, target: string): Promise<string> {
+  public pushTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.PUSH, target)
   }
 
-  public removeTarget(cwd: string, target: string): Promise<string> {
+  public removeTarget(cwd: string, target: string) {
     return this.executeProcess(cwd, Command.REMOVE, target)
   }
 }
