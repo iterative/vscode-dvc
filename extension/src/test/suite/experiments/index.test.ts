554909b1393b7508b9132ea16b11a263d4284276
Matt Seddon <mattseddon@hotmail.com>
2021-06-23 16:11:37 +1000

    move reader stub directly below instantiation

--- extension/src/test/suite/experiments/index.test.ts
+++ extension/src/test/suite/experiments/index.test.ts
@@ -114,8 +114,6 @@ suite('Experiments Test Suite', () => {
         new InternalCommands(config, cliReader)
       )
 
-      stub(cliReader, 'experimentShow').resolves(complexExperimentsOutput)
-
       const resourceLocator = disposable.track(
         new ResourceLocator(Uri.file(resourcePath))
       )
