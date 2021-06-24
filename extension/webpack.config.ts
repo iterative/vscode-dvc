ffdc4a2517f8e41cf8813a19e3fbfdd2ad9bfbb2
Matt Seddon <mattseddon@hotmail.com>
2021-06-24 20:59:45 +1000

    remove redundant code package and types

--- extension/webpack.config.ts
+++ extension/webpack.config.ts
@@ -1,50 +1,14 @@
-import { readFileSync } from 'fs'
-import { join, resolve } from 'path'
+import { resolve } from 'path'
 import * as webpack from 'webpack'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
-import CopyPlugin = require('copy-webpack-plugin')
 
 const r = (file: string) => resolve(__dirname, file)
-
-function includeDependency(location: string) {
-  const pJson = join(location, 'package.json')
-  const content = readFileSync(pJson, {
-    encoding: 'utf8'
-  })
-  const pkgName = JSON.parse(content).name
-
-  return new CopyPlugin({
-    patterns: [
-      {
-        from: pJson,
-        to: r(`./dist/node_modules/${pkgName}/package.json`)
-      },
-      {
-        from: join(location, 'index.d.ts'),
-        to: r(`./dist/node_modules/${pkgName}/index.d.ts`)
-      },
-      {
-        from: join(location, 'index.js'),
-        to: r(`./dist/node_modules/${pkgName}/index.js`)
-      },
-      {
-        from: join(location, 'dist'),
-        to: r(`./dist/node_modules/${pkgName}/dist`)
-      }
-    ]
-  })
-}
 
 module.exports = {
   devtool: 'source-map',
   entry: r('./src/extension'),
   externals: {
-    'dvc-vscode-webview': resolve(
-      __dirname,
-      'dist',
-      'node_modules',
-      'dvc-vscode-webview'
-    ),
+    'dvc-vscode-webview': r('../webview/'),
     fsevents: "require('fsevents')",
     vscode: 'commonjs vscode'
   },
@@ -70,7 +34,7 @@ module.exports = {
     libraryTarget: 'commonjs2',
     path: r('./dist')
   },
-  plugins: [new CleanWebpackPlugin(), includeDependency(r('../webview/'))],
+  plugins: [new CleanWebpackPlugin()],
   resolve: {
     extensions: ['.ts', '.js']
   },
