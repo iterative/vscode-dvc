721861e0f3c6955fd87cb9f64fe2dd5398ee8203
Matt Seddon <mattseddon@hotmail.com>
2021-06-25 16:12:53 +1000

    test edge case

--- extension/src/test/suite/repository/index.test.ts
+++ extension/src/test/suite/repository/index.test.ts
@@ -29,9 +29,7 @@ suite('Repository Test Suite', () => {
       const config = disposable.track(new Config())
       const cliReader = disposable.track(new CliReader(config))
       const mockList = stub(cliReader, 'listDvcOnlyRecursive').resolves([])
-
       const mockDiff = stub(cliReader, 'diff').resolves({})
-
       const mockStatus = stub(cliReader, 'status').resolves({})
       const internalCommands = disposable.track(
         new InternalCommands(config, cliReader)
@@ -63,9 +61,7 @@ suite('Repository Test Suite', () => {
     const config = disposable.track(new Config())
     const cliReader = disposable.track(new CliReader(config))
     const mockList = stub(cliReader, 'listDvcOnlyRecursive').resolves([])
-
     const mockDiff = stub(cliReader, 'diff').resolves({})
-
     const mockStatus = stub(cliReader, 'status').resolves({})
     const internalCommands = disposable.track(
       new InternalCommands(config, cliReader)
@@ -92,13 +88,11 @@ suite('Repository Test Suite', () => {
     expect(mockStatus).to.be.calledTwice
   })
 
-  it('should queue a reset and return early if a reset is in progress and update is called', async () => {
+  it('should queue a reset and return early if a reset is in progress and any other calls are made', async () => {
     const config = disposable.track(new Config())
     const cliReader = disposable.track(new CliReader(config))
     const mockList = stub(cliReader, 'listDvcOnlyRecursive').resolves([])
-
     const mockDiff = stub(cliReader, 'diff').resolves({})
-
     const mockStatus = stub(cliReader, 'status').resolves({})
     const internalCommands = disposable.track(
       new InternalCommands(config, cliReader)
@@ -115,13 +109,46 @@ suite('Repository Test Suite', () => {
     await Promise.all([
       repository.resetState(),
       repository.updateState(),
+      repository.resetState(),
       repository.updateState(),
-      repository.updateState(),
+      repository.resetState(),
       repository.updateState()
     ])
 
     expect(mockList).to.be.calledTwice
     expect(mockDiff).to.be.calledTwice
     expect(mockStatus).to.be.calledTwice
+  })
+
+  it('will run both update and reset (and send further calls to the reset queue) if they are called in that order', async () => {
+    const config = disposable.track(new Config())
+    const cliReader = disposable.track(new CliReader(config))
+    const mockList = stub(cliReader, 'listDvcOnlyRecursive').resolves([])
+    const mockDiff = stub(cliReader, 'diff').resolves({})
+    const mockStatus = stub(cliReader, 'status').resolves({})
+    const internalCommands = disposable.track(
+      new InternalCommands(config, cliReader)
+    )
+
+    const repository = disposable.track(
+      new Repository(dvcDemoPath, internalCommands)
+    )
+    await repository.isReady()
+    mockList.resetHistory()
+    mockDiff.resetHistory()
+    mockStatus.resetHistory()
+
+    await Promise.all([
+      repository.updateState(),
+      repository.resetState(),
+      repository.updateState(),
+      repository.resetState(),
+      repository.updateState(),
+      repository.resetState()
+    ])
+
+    expect(mockList).to.be.calledTwice
+    expect(mockDiff).to.be.calledThrice
+    expect(mockStatus).to.be.calledThrice
   })
 })
