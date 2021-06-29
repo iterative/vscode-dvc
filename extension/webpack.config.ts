2e7ac9e5e38c5b6ca5bb04b18c861ad4447dd273
Matt Seddon <mattseddon@hotmail.com>
2021-06-29 14:46:21 +1000

    revert to using copy webpack plugin

--- extension/webpack.config.ts
+++ extension/webpack.config.ts
@@ -1,14 +1,39 @@
-import { resolve } from 'path'
+import { join, resolve } from 'path'
 import * as webpack from 'webpack'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import { readFileSync } from 'fs-extra'
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const r = (file: string) => resolve(__dirname, file)
+function includeDependency(location: string) {
+  const content = readFileSync(join(location, 'package.json'), {
+    encoding: 'utf8'
+  })
+  const pkgName = JSON.parse(content).name
+
+  return new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: `${location}/dist`,
+        to: r(`./dist/node_modules/${pkgName}/dist`)
+      },
+      {
+        from: `${location}/index.js`,
+        to: r(`./dist/node_modules/${pkgName}/index.js`)
+      },
+      {
+        from: `${location}/package.json`,
+        to: r(`./dist/node_modules/${pkgName}/package.json`)
+      }
+    ]
+  })
+}
 
 module.exports = {
   devtool: 'source-map',
   entry: r('./src/extension'),
   externals: {
-    'dvc-vscode-webview': r('../webview/'),
+    'dvc-vscode-webview': 'dvc-vscode-webview',
     fsevents: "require('fsevents')",
     vscode: 'commonjs vscode'
   },
@@ -34,9 +59,11 @@ module.exports = {
     libraryTarget: 'commonjs2',
     path: r('./dist')
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin(), includeDependency(r('../webview/'))],
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    modules: ['node_modules'],
+    symlinks: false
   },
   target: 'node'
 } as webpack.Configuration
