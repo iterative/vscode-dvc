0becaa949151b47b2f67b2480d131a04128a38e0
Matt Seddon <mattseddon@hotmail.com>
2021-07-07 12:31:12 +1000

    add test for toggling status of parent

--- extension/src/test/suite/experiments/views/columnsTree.test.ts
+++ extension/src/test/suite/experiments/views/columnsTree.test.ts
@@ -50,12 +50,7 @@ suite('Extension Test Suite', () => {
     it('should be able to toggle whether an experiments column is selected with dvc.views.experimentColumnsTree.toggleSelected', async () => {
       const toggleCommand = 'dvc.views.experimentColumnsTree.toggleSelected'
       const relPath = join('params', 'params.yaml', 'learning_rate')
-      const absPath = join(
-        dvcDemoPath,
-        'params',
-        'params.yaml',
-        'learning_rate'
-      )
+      const absPath = join(dvcDemoPath, relPath)
 
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       stub((ExperimentsColumnsTree as any).prototype, 'getDetails').returns([
@@ -97,6 +92,62 @@ suite('Extension Test Suite', () => {
       )
 
       expect(isUnselectedAgain).to.be.false
+    })
+
+    it("should be able to toggle a parents and change the selected status of it's children with dvc.views.experimentColumnsTree.toggleSelected", async () => {
+      const toggleCommand = 'dvc.views.experimentColumnsTree.toggleSelected'
+      const relPath = join('params', 'params.yaml')
+      const absPath = join(dvcDemoPath, relPath)
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      stub((ExperimentsColumnsTree as any).prototype, 'getDetails').returns([
+        dvcDemoPath,
+        relPath
+      ])
+
+      const config = disposable.track(new Config())
+      const cliReader = disposable.track(new CliReader(config))
+      stub(cliReader, 'experimentShow').resolves(complexExperimentsOutput)
+
+      const internalCommands = disposable.track(
+        new InternalCommands(config, cliReader)
+      )
+
+      const resourceLocator = disposable.track(
+        new ResourceLocator(Uri.file(resourcePath))
+      )
+      const experimentsTable = disposable.track(
+        new ExperimentsTable(dvcDemoPath, internalCommands, resourceLocator)
+      )
+
+      await experimentsTable.isReady()
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      stub((Experiments as any).prototype, 'getTable').returns(experimentsTable)
+
+      const selectedChildren = experimentsTable.getChildColumns(relPath) || []
+      expect(selectedChildren).to.have.lengthOf.greaterThan(1)
+
+      const selectedGrandChildren =
+        experimentsTable.getChildColumns(join(relPath, 'process')) || []
+      expect(selectedGrandChildren).to.have.lengthOf.greaterThan(1)
+
+      const allChildren = [...selectedChildren, ...selectedGrandChildren]
+
+      allChildren.map(
+        column =>
+          expect(experimentsTable.getColumn(column.path)?.isSelected).to.be.true
+      )
+
+      const isUnselected = await commands.executeCommand(toggleCommand, absPath)
+
+      expect(isUnselected).to.be.false
+
+      allChildren.map(column =>
+        expect(experimentsTable.getColumn(column.path)?.isSelected).to.equal(
+          isUnselected
+        )
+      )
     })
   })
 })
