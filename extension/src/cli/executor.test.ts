a533bb420f85f3ccae4ad7de6250bff1e9f10aa3
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-08-17 13:43:00 +1000

    Use newly added DVC_ANALYTICS environment variable (#721)
    * update demo to use at least dvc s3 2.6.1
    
    * remove references to analytics in docs
    
    * add dvc analytics variable into env variables

--- extension/src/cli/executor.test.ts
+++ extension/src/cli/executor.test.ts
@@ -15,6 +15,7 @@ jest.mock('../env')
 const mockedExecuteProcess = mocked(executeProcess)
 const mockedGetProcessEnv = mocked(getProcessEnv)
 const mockedEnv = {
+  DVC_ANALYTICS: 'false',
   PATH: '/some/special/path'
 }
 
