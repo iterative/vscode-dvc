1b7da41409a579b8f406acf0100ed3364db7b79e
Matt Seddon <mattseddon@hotmail.com>
2021-08-27 13:19:01 +1000

    add stop watch class for use with analytics

--- extension/src/util/time.ts
+++ extension/src/util/time.ts
@@ -2,3 +2,15 @@ export const delay = (ms: number): Promise<void> =>
   new Promise(resolve => setTimeout(resolve, ms))
 
 export const getCurrentEpoch = (): number => new Date().getTime()
+
+export class StopWatch {
+  private started = getCurrentEpoch()
+
+  public getElapsedTime() {
+    return getCurrentEpoch() - this.started
+  }
+
+  public reset() {
+    this.started = getCurrentEpoch()
+  }
+}
