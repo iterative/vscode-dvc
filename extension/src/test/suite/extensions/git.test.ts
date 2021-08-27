46ccbcdd3853566cc7ae99c09ebec8736b3a75e4
Matt Seddon <mattseddon@hotmail.com>
2021-08-27 11:20:43 +1000

    remove boilerplate from test

--- extension/src/test/suite/extensions/git.test.ts
+++ extension/src/test/suite/extensions/git.test.ts
@@ -1,25 +1,13 @@
 import { resolve } from 'path'
-import { afterEach, beforeEach, describe, it, suite } from 'mocha'
+import { describe, it, suite } from 'mocha'
 import { expect } from 'chai'
 import { window } from 'vscode'
-import { restore } from 'sinon'
-import { Disposable } from '../../../extension'
 import { getGitRepositoryRoots } from '../../../extensions/git'
 
 suite('Git Extension Test Suite', () => {
   window.showInformationMessage('Start all git extension tests.')
 
   const workspacePath = resolve(__dirname, '..', '..', '..', '..', '..')
-
-  const disposable = Disposable.fn()
-
-  beforeEach(() => {
-    restore()
-  })
-
-  afterEach(() => {
-    disposable.dispose()
-  })
 
   describe('getGitRepositoryRoots', () => {
     it('should be able to return the root path of each open repository', async () => {
