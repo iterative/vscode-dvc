0472703958ccfec926c9b18b24d9e10a6f1390d3
Stephanie Roy <stephanie@iterative.ai>
2021-09-14 13:01:53 -0400

    Fix the headers when there is column nesting and different levels

--- webview/src/util/buildDynamicColumns.tsx
+++ webview/src/util/buildDynamicColumns.tsx
@@ -6,6 +6,13 @@ import { Experiment, ParamOrMetric } from 'dvc/src/experiments/webview/contract'
 import { formatFloat } from './numberFormatting'
 
 type Value = string | number
+type FullColumn = Column<Experiment> & {
+  columns?: Column<Experiment>[]
+  sortType?: string
+  type?: string[]
+  parent?: Column<Experiment>
+  placeholderOf?: Column<Experiment>
+}
 
 const UndefinedCell = <>-</>
 
@@ -34,16 +41,13 @@ const buildDynamicColumns = (
     .filter(column => column.parentPath === parentPath)
     .map(data => {
       const { path } = data
+
       const Cell = getCellComponent()
       const childColumns = buildDynamicColumns(properties, path)
 
       const pathArray = splitParamOrMetricPath(path)
 
-      const column: Column<Experiment> & {
-        columns?: Column<Experiment>[]
-        sortType?: string
-        type?: string[]
-      } = {
+      const column: FullColumn = {
         Cell,
         Header: data.name,
         accessor: buildAccessor(pathArray),
@@ -61,4 +65,46 @@ const buildDynamicColumns = (
       return column
     })
 
-export default buildDynamicColumns
+const findMaxDepth = (columns: FullColumn[], depth = 1): number =>
+  columns.reduce(
+    (prev: number, curr: FullColumn) =>
+      curr.columns
+        ? Math.max(prev, findMaxDepth(curr.columns, depth + 1))
+        : depth,
+    1
+  )
+
+const fixColumnsNesting = (
+  columns: FullColumn[],
+  parent?: Column<Experiment>,
+  depth = 0,
+  maxDepth = 0
+) =>
+  columns.map((column: FullColumn) => {
+    const deepest =
+      !depth && column.columns ? findMaxDepth(column.columns) : maxDepth
+    const newDepth = depth + 1
+    if (column.columns) {
+      column.columns = fixColumnsNesting(
+        column.columns,
+        column,
+        newDepth,
+        deepest
+      )
+    } else if (deepest > depth) {
+      const nextCol = { ...column }
+      column = {
+        Header: '',
+        id: column.id + '_previous_placeholder',
+        parent,
+        placeholderOf: column
+      }
+      column.columns = fixColumnsNesting([nextCol], column, newDepth, deepest)
+    }
+    return column
+  })
+
+const buildColumns = (properties: ParamOrMetric[], parentPath: string) =>
+  fixColumnsNesting(buildDynamicColumns(properties, parentPath))
+
+export default buildColumns
