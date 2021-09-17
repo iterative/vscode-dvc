7d4301d6b05797db26e7e03a06456d7918f0cf9d
Matt Seddon <mattseddon@hotmail.com>
2021-09-17 11:04:14 +1000

    group registration of commands by the section in which they appear in the ui

--- extension/src/vscode/commands.ts
+++ extension/src/vscode/commands.ts
@@ -5,19 +5,7 @@ import { InternalCommands } from '../commands/internal'
 const executeCommand = (name: string, path: string) =>
   commands.executeCommand(name, Uri.file(path))
 
-export const reRegisterVsCodeCommands = (
-  internalCommands: InternalCommands
-) => {
-  internalCommands.registerExternalCommand<string>(
-    RegisteredCommands.TRACKED_EXPLORER_COPY_FILE_PATH,
-    path => executeCommand('copyFilePath', path)
-  )
-
-  internalCommands.registerExternalCommand<string>(
-    RegisteredCommands.TRACKED_EXPLORER_COPY_REL_FILE_PATH,
-    path => executeCommand('copyRelativeFilePath', path)
-  )
-
+const registerNavigationCommands = (internalCommands: InternalCommands) => {
   internalCommands.registerExternalCommand<Uri>(
     RegisteredCommands.TRACKED_EXPLORER_OPEN_FILE,
     resource => commands.executeCommand('vscode.open', resource)
@@ -27,12 +15,9 @@ export const reRegisterVsCodeCommands = (
     RegisteredCommands.TRACKED_EXPLORER_OPEN_TO_THE_SIDE,
     path => executeCommand('explorer.openToSide', path)
   )
+}
 
-  internalCommands.registerExternalCommand<string>(
-    RegisteredCommands.TRACKED_EXPLORER_FIND_IN_FOLDER,
-    path => executeCommand('filesExplorer.findInFolder', path)
-  )
-
+const registerCompareCommands = (internalCommands: InternalCommands) => {
   internalCommands.registerExternalCommand<string>(
     RegisteredCommands.TRACKED_EXPLORER_SELECT_FOR_COMPARE,
     path => executeCommand('selectForCompare', path)
@@ -42,4 +27,32 @@ export const reRegisterVsCodeCommands = (
     RegisteredCommands.TRACKED_EXPLORER_COMPARE_SELECTED,
     path => executeCommand('compareFiles', path)
   )
+}
+
+const registerSearchCommands = (internalCommands: InternalCommands) => {
+  internalCommands.registerExternalCommand<string>(
+    RegisteredCommands.TRACKED_EXPLORER_FIND_IN_FOLDER,
+    path => executeCommand('filesExplorer.findInFolder', path)
+  )
+}
+
+const registerCutCopyPasteCommands = (internalCommands: InternalCommands) => {
+  internalCommands.registerExternalCommand<string>(
+    RegisteredCommands.TRACKED_EXPLORER_COPY_FILE_PATH,
+    path => executeCommand('copyFilePath', path)
+  )
+
+  internalCommands.registerExternalCommand<string>(
+    RegisteredCommands.TRACKED_EXPLORER_COPY_REL_FILE_PATH,
+    path => executeCommand('copyRelativeFilePath', path)
+  )
+}
+
+export const reRegisterVsCodeCommands = (
+  internalCommands: InternalCommands
+) => {
+  registerNavigationCommands(internalCommands)
+  registerCompareCommands(internalCommands)
+  registerSearchCommands(internalCommands)
+  registerCutCopyPasteCommands(internalCommands)
 }
