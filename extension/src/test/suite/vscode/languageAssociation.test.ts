2095e0cddc7714d9aa182aa598cb9c8cc3156699
Matt Seddon <mattseddon@hotmail.com>
2021-10-11 12:42:01 +1100

    clean up integration tests

--- extension/src/test/suite/vscode/languageAssociation.test.ts
+++ extension/src/test/suite/vscode/languageAssociation.test.ts
@@ -1,31 +1,32 @@
 import { join } from 'path'
-import { afterEach, beforeEach, describe, it, suite } from 'mocha'
+import { beforeEach, describe, it, suite } from 'mocha'
 import { expect } from 'chai'
 import { commands, MessageItem, Uri, window } from 'vscode'
 import { restore, stub } from 'sinon'
 import { dvcDemoPath } from '../util'
-import { Disposable } from '../../../extension'
 
-suite('Output Channel Test Suite', () => {
-  const disposable = Disposable.fn()
-
+suite('Language Association Test Suite', () => {
   beforeEach(() => {
     restore()
   })
 
-  afterEach(() => {
-    disposable.dispose()
-  })
-
   describe('tryAssociateYamlOnce', () => {
     it('should only try and associate .dvc and dvc.lock files once per session', async () => {
+      const getUri = (fileName: string) => Uri.file(join(dvcDemoPath, fileName))
+
       const mockShowInformationMessage = stub(
         window,
         'showInformationMessage'
       ).resolves('No' as unknown as MessageItem)
 
-      const getDvcLock = () =>
-        window.showTextDocument(Uri.file(join(dvcDemoPath, 'dvc.lock')))
+      await window.showTextDocument(getUri('.gitignore'))
+
+      expect(
+        mockShowInformationMessage,
+        'should not ask the user if they want to associate .dvc and dvc.lock files with yaml on a normal editor change'
+      ).not.to.be.called
+
+      const getDvcLock = () => window.showTextDocument(getUri('dvc.lock'))
 
       await getDvcLock()
 
