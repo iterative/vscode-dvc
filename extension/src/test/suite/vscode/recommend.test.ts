f6ebdf584c2732a5a8c2bc319a93d9d1e35ca448
Matt Seddon <mattseddon@hotmail.com>
2021-10-13 13:33:05 +1100

    add integraton tests for recommend add dvc schema

--- extension/src/test/suite/vscode/recommend.test.ts
+++ extension/src/test/suite/vscode/recommend.test.ts
@@ -4,16 +4,18 @@ import { expect } from 'chai'
 import { commands, MessageItem, Uri, window } from 'vscode'
 import { restore, stub } from 'sinon'
 import { dvcDemoPath } from '../util'
+import * as Extensions from '../../../vscode/extensions'
 
 suite('Language Association Test Suite', () => {
   beforeEach(() => {
     restore()
   })
 
+  const getUri = (fileName: string) => Uri.file(join(dvcDemoPath, fileName))
+
   describe('recommendAssociateYamlOnce', () => {
     it('should only try and associate .dvc and dvc.lock files once per session', async () => {
-      const getUri = (fileName: string) => Uri.file(join(dvcDemoPath, fileName))
-
+      stub(Extensions, 'isAvailable').resolves(true)
       const mockShowInformationMessage = stub(
         window,
         'showInformationMessage'
@@ -44,6 +46,43 @@ suite('Language Association Test Suite', () => {
       expect(
         mockShowInformationMessage,
         'should not ask the user if they want to associate .dvc and dvc.lock files with yaml on subsequent calls'
+      ).not.to.be.called
+    })
+  })
+
+  describe('recommendAddDvcYamlSchemaOnce', () => {
+    it('should only try to add dvc.yaml schema validation once per session', async () => {
+      stub(Extensions, 'isAvailable').resolves(true)
+      const mockShowInformationMessage = stub(
+        window,
+        'showInformationMessage'
+      ).resolves('No' as unknown as MessageItem)
+
+      await window.showTextDocument(getUri('.gitignore'))
+
+      expect(
+        mockShowInformationMessage,
+        'should not ask the user if they want to add the dvc.yaml schema validation on a normal editor change'
+      ).not.to.be.called
+
+      const getDvcYaml = () => window.showTextDocument(getUri('dvc.yaml'))
+
+      await getDvcYaml()
+
+      expect(
+        mockShowInformationMessage,
+        'should ask the user if they want to add the dvc.yaml schema validation on the first call'
+      ).to.be.calledOnce
+
+      await commands.executeCommand('workbench.action.closeAllEditors')
+
+      mockShowInformationMessage.resetHistory()
+
+      await getDvcYaml()
+
+      expect(
+        mockShowInformationMessage,
+        'should not ask the user if they want to add the dvc.yaml schema validation on subsequent calls'
       ).not.to.be.called
     })
   })
