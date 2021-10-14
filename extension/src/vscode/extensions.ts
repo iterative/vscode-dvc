cc472e83fff7dd70f340a9c48295ca10a3369aa7
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-10-15 05:05:29 +1100

    remove isAvailable as it does not take into account activation events (#911)
    Co-authored-by: Roger <rogermparent@gmail.com>

--- extension/src/vscode/extensions.ts
+++ extension/src/vscode/extensions.ts
@@ -35,7 +35,5 @@ export const getExtensionVersion = <T>(id: string): string | undefined => {
 export const isInstalled = (id: string): boolean =>
   !!extensions.all.find(extension => extension.id === id)
 
-export const isAvailable = (id: string): boolean => !!getExtension(id)?.isActive
-
 export const showExtension = (id: string) =>
   commands.executeCommand('workbench.extensions.search', `@id:${id}`)
