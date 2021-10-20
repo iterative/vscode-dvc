7ecfbe7e133e03b8c586f9b1cea1357f4761ab14
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-10-21 04:59:21 +1100

    Remove git extension API dependency (#930)
    * remove git extension API dependency
    
    * move finding of git root down into on did change data

--- extension/src/experiments/workspace.ts
+++ extension/src/experiments/workspace.ts
@@ -13,8 +13,6 @@ import {
   AvailableCommands,
   InternalCommands
 } from '../commands/internal'
-import { getGitRepositoryRoots } from '../extensions/git'
-import { findDvcRootPaths } from '../fileSystem'
 import { BaseWorkspace, IWorkspace } from '../workspace'
 
 export class WorkspaceExperiments
@@ -222,17 +220,7 @@ export class WorkspaceExperiments
     )
 
     Promise.all(experiments.map(experiments => experiments.isReady())).then(
-      async () => {
-        this.deferred.resolve()
-        const gitRoots = await getGitRepositoryRoots()
-        gitRoots.forEach(async gitRoot => {
-          const dvcRoots = await findDvcRootPaths(gitRoot)
-
-          dvcRoots.forEach(dvcRoot =>
-            this.getRepository(dvcRoot).onDidChangeData(gitRoot)
-          )
-        })
-      }
+      () => this.deferred.resolve()
     )
 
     return experiments
@@ -286,6 +274,8 @@ export class WorkspaceExperiments
     )
 
     this.setRepository(dvcRoot, experiments)
+
+    experiments.onDidChangeData()
 
     experiments.dispose.track(
       experiments.onDidChangeIsWebviewFocused(
