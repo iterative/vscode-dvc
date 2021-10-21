11b62977b9c971caca8ac068344f5260039f0fc6
mattseddon <37993418+mattseddon@users.noreply.github.com>
2021-10-22 08:35:07 +1100

    Move params and metrics file path collection (#934)
    * move file path collection into shared data folder
    
    * test and condense code

---
+++ extension/src/data/collect.test.ts
@@ -0,0 +1,88 @@
+import { join } from 'path'
+import { collectFiles } from './collect'
+import { ExperimentsRepoJSONOutput } from '../cli/reader'
+import complexExperimentsOutput from '../test/fixtures/complex-output-example'
+
+describe('collectFiles', () => {
+  it('should collect all of the available files from the test fixture', () => {
+    expect(collectFiles(complexExperimentsOutput)).toEqual([
+      'params.yaml',
+      join('nested', 'params.yaml'),
+      'summary.json'
+    ])
+  })
+
+  it('should handle an error being returned', () => {
+    const workspace = {
+      workspace: {
+        baseline: {
+          error: { msg: 'bad things are happening', type: 'today' }
+        }
+      }
+    }
+
+    expect(collectFiles(workspace)).toEqual([])
+  })
+
+  it('should handle a missing params key', () => {
+    const workspace = {
+      workspace: {
+        baseline: {
+          data: {
+            metrics: {
+              'logs.json': {}
+            }
+          }
+        }
+      }
+    }
+
+    expect(collectFiles(workspace)).toEqual(['logs.json'])
+  })
+
+  it('should handle a missing metrics key', () => {
+    const workspace = {
+      workspace: {
+        baseline: {
+          data: {
+            params: {
+              'params.yaml': {}
+            }
+          }
+        }
+      }
+    }
+
+    expect(collectFiles(workspace)).toEqual(['params.yaml'])
+  })
+
+  it('should collect all of the available files from a more complex example', () => {
+    const workspace = {
+      workspace: {
+        baseline: {
+          data: {
+            metrics: {
+              'logs.json': {},
+              'metrics.json': {},
+              'summary.json': {}
+            },
+            params: {
+              'further/nested/params.yaml': {},
+              'nested/params.yaml': {},
+              'params.yaml': {}
+            }
+          }
+        }
+      }
+    } as ExperimentsRepoJSONOutput
+
+    expect(collectFiles(workspace).sort()).toEqual([
+      'further/nested/params.yaml',
+      'logs.json',
+      'metrics.json',
+      'nested/params.yaml',
+      'params.yaml',
+      'summary.json'
+    ])
+  })
+})
---
+++ extension/src/data/collect.ts
@@ -0,0 +1,12 @@
+import { ExperimentsRepoJSONOutput } from '../cli/reader'
+
+export const collectFiles = (data: ExperimentsRepoJSONOutput): string[] => {
+  const files = new Set<string>(
+    Object.keys({
+      ...data?.workspace.baseline?.data?.params,
+      ...data?.workspace.baseline?.data?.metrics
+    }).filter(Boolean)
+  )
+
+  return [...files]
+}
