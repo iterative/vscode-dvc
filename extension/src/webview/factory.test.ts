0b761e94e919bf9231baec50064439eb96e490ba
rogermparent <rogermparent@gmail.com>
2021-10-30 00:52:00 -0400

    Fix broken state validation

---
+++ extension/src/webview/factory.test.ts
@@ -0,0 +1,23 @@
+import { ViewKey } from './constants'
+import { isValidState } from './factory'
+import complexPlotsData from '../test/fixtures/complex-plots-example'
+
+describe('isValidState', () => {
+  const dvcRoot = 'test'
+  it('Successfully validates example plots data', () => {
+    expect(
+      isValidState(ViewKey.PLOTS, {
+        data: complexPlotsData,
+        dvcRoot
+      })
+    ).toBe(true)
+  })
+  it('Successfully validates an empty plots state', () => {
+    expect(
+      isValidState(ViewKey.PLOTS, {
+        data: [],
+        dvcRoot
+      })
+    ).toBe(true)
+  })
+})
