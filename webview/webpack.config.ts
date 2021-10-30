ed770f2470fb3c3e737263e3c631d723a8fec5a0
rogermparent <rogermparent@gmail.com>
2021-10-29 20:21:00 -0400

    Rename table webview subdirectory to experiments

--- webview/webpack.config.ts
+++ webview/webpack.config.ts
@@ -21,7 +21,7 @@ module.exports = {
   },
   devtool: 'source-map',
   entry: {
-    main: { dependOn: 'react', import: r('src/table/index.tsx') },
+    main: { dependOn: 'react', import: r('src/experiments/index.tsx') },
     plots: { dependOn: 'react', import: r('src/plots/index.tsx') },
     react: ['react', 'react-dom']
   },
