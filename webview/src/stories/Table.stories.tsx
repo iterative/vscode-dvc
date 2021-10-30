4807269563c06a57d2ec887d8e0b27c7fbe17659
rogermparent <rogermparent@gmail.com>
2021-10-29 20:50:12 -0400

    Remove separate table data fixture file

--- webview/src/stories/Table.stories.tsx
+++ webview/src/stories/Table.stories.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import { action } from '@storybook/addon-actions'
-import complexTableData from './complex-table-data'
+import complexRowData from 'dvc/src/test/fixtures/complex-row-example'
+import complexColumnData from 'dvc/src/test/fixtures/complex-column-example'
+import complexChangesData from 'dvc/src/test/fixtures/complex-changes-example'
 import Experiments from '../experiments/components/Experiments'
 
 import './test-vscode-styles.scss'
-import '../table/style.scss'
+import '../experiments/style.scss'
 
 const dummyVsCodeApi = {
   postMessage: action('postMessage')
@@ -20,7 +22,16 @@ export default {
     }
   },
   args: {
-    tableData: complexTableData,
+    tableData: {
+      changes: complexChangesData,
+      columns: complexColumnData,
+      columnsOrder: [],
+      rows: complexRowData,
+      sorts: [
+        { descending: true, path: 'params:params.yaml:epochs' },
+        { descending: false, path: 'params:params.yaml:log_file' }
+      ]
+    },
     vsCodeApi: dummyVsCodeApi
   },
   component: Experiments,
