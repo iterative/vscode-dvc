dde3302632bdb4101193b1a703f978d3bd6bfbe8
rogermparent <rogermparent@gmail.com>
2021-11-08 16:33:43 -0500

    Add legacy actions in Storybook vscode api mock

--- webview/.storybook/preview.ts
+++ webview/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import { InternalVsCodeApi } from '../src/shared/api'
+import { action } from '@storybook/addon-actions'
 
 declare global {
   interface Window {
@@ -10,10 +11,10 @@ declare global {
 window.webviewData = {}
 window.acquireVsCodeApi = () =>
   ({
-    getState: () => {},
-    postMessage: () => {},
-    setState: () => {}
-  } as InternalVsCodeApi)
+    getState: action('getState'),
+    postMessage: action('postMessage'),
+    setState: action('setState')
+  } as unknown as InternalVsCodeApi)
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
