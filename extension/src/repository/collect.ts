2cf847810e9892badf077c9fc8e3ece89abbe115
Matt Seddon <mattseddon@hotmail.com>
2021-11-11 10:31:51 +1100

    refactor tree collection

--- extension/src/repository/collect.ts
+++ extension/src/repository/collect.ts
@@ -2,29 +2,42 @@ import { join, sep } from 'path'
 import { Uri } from 'vscode'
 import { addToMapSet } from '../util/map'
 
+const getPath = (pathArray: string[], idx: number) =>
+  pathArray.slice(0, idx).join(sep)
+
+const getDirectChild = (pathArray: string[], idx: number) =>
+  getPath(pathArray, idx + 1)
+
+const transform = (
+  dvcRoot: string,
+  acc: Map<string, Set<string>>
+): Map<string, Uri[]> => {
+  const treeMap = new Map<string, Uri[]>()
+
+  acc.forEach((paths, path) => {
+    const uris = [...paths].map(path => Uri.file(join(dvcRoot, path)))
+    const absPath = join(dvcRoot, path)
+    treeMap.set(absPath, uris)
+  })
+
+  return treeMap
+}
+
 export const collectTree = (
   dvcRoot: string,
   paths: string[]
 ): Map<string, Uri[]> => {
-  const map = new Map<string, Set<string>>()
+  const acc = new Map<string, Set<string>>()
 
   paths.forEach(path => {
     const pathArray = path.split(sep)
 
-    pathArray.reduce((map, _, i) => {
-      const path = pathArray.slice(0, i).join(sep)
-      addToMapSet(map, path, pathArray.slice(0, i + 1).join(sep))
-      return map
-    }, map)
+    pathArray.reduce((acc, _, i) => {
+      const path = getPath(pathArray, i)
+      addToMapSet(acc, path, getDirectChild(pathArray, i))
+      return acc
+    }, acc)
   })
 
-  const newMap = new Map<string, Uri[]>()
-
-  map.forEach((paths, path) => {
-    const uris = [...paths].map(path => Uri.file(join(dvcRoot, path)))
-    const absPath = join(dvcRoot, path)
-    newMap.set(absPath, uris)
-  })
-
-  return newMap
+  return transform(dvcRoot, acc)
 }
