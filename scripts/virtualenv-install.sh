2f0f867e547401d871483feb67ba7d106fb2cfa4
Matt Seddon <mattseddon@hotmail.com>
2021-11-15 13:19:30 +1100

    move all scripts into scripts dir

--- scripts/virtualenv-install.sh
+++ scripts/virtualenv-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cd to demo and setup venv if needed
-source ./activate-demo-venv.sh
+source ./scripts/activate-demo-venv.sh
 
 python -m pip install --upgrade pip
 pip install -U -r ./requirements.txt
