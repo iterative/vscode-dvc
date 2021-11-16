93fb16aa279006cb071f13e1361a1176404ab518
rogermparent <rogermparent@gmail.com>
2021-11-16 15:50:04 -0500

    Remove unnecessary fragment

--- webview/src/experiments/components/GUI.tsx
+++ webview/src/experiments/components/GUI.tsx
@@ -7,11 +7,7 @@ import { Model } from '../model'
 export const GUI: React.FC<{ model: Model }> = observer(({ model }) => {
   try {
     const { data } = model as Model
-    return (
-      <>
-        <ExperimentsGUI tableData={data} />
-      </>
-    )
+    return <ExperimentsGUI tableData={data} />
   } catch (e: unknown) {
     return <p>{(e as Error).toString()}</p>
   }
