5d7067cefe2aa3d57d3e86704427e9a86c738161
rogermparent <rogermparent@gmail.com>
2021-11-22 18:26:52 -0500

    Fix svgr in storybook

--- webview/.storybook/main.ts
+++ webview/.storybook/main.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import { Configuration, RuleSetRule } from 'webpack'
+
+export default {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
@@ -20,5 +22,34 @@ module.exports = {
   },
   typescript: {
     reactDocgen: false
+  },
+  webpackFinal: (config: Configuration) => {
+    const rules = config?.module?.rules as RuleSetRule[]
+    if (rules) {
+      const existingSvgRuleIndex = rules.findIndex(
+        rule => rule.test instanceof RegExp && rule.test.test('.svg')
+      )
+      if (existingSvgRuleIndex >= 0) {
+        const existingSvgRule = rules[existingSvgRuleIndex]
+        const { test, ...rest } = existingSvgRule
+        const newSvgRule = {
+          test,
+          oneOf: [
+            {
+              test: /\.svg$/,
+              resourceQuery: /svgr/,
+              use: '@svgr/webpack'
+            },
+            {
+              test: /\.svg$/,
+              type: 'asset/inline'
+            },
+            rest
+          ]
+        }
+        rules.splice(existingSvgRuleIndex, 1, newSvgRule)
+        return config
+      }
+    }
   }
 }
