4dc2aa16be805c2b7a9549e2ca370b7844b6bf7d
Roger <rogermparent@gmail.com>
2021-11-24 23:21:30 -0500

    Add copy button (#1073)
    * Refactor Cell components
    
    * Add @svgr/webpack
    
    * Add copy button to table cells
    
    * Move svg declaration to match other PR
    
    * Rename duplicated name Cell component
    
    * Add CopyButton component tests and config to support them
    
    * Make svgr usable with import query
    
    * Fix svgr in storybook
    
    * Ignore .storybook in codeclimate
    
    * Remove space from between cell content and copy button
    
    * justify-content: center metaCell
    
    * Update svg mock for default exports
    
    * Complete small styling consistency refactor
    
    * Remove unrelated scss refactor
    
    * Clear timeout on unmount
    
    * userEvent -> fireEvent
    
    * Remove webpack svgr additions
    
    * Use preprocessed copy icon
    
    * Refactor unnecessary default out
    
    * Fix use of preprocessed SVG
    
    * Remove new codeclimate rule since it's not needed
    
    Co-authored-by: mattseddon <37993418+mattseddon@users.noreply.github.com>

---
+++ webview/src/experiments/components/CopyButton/index.tsx
@@ -0,0 +1,67 @@
+import React, { FC, useEffect, useRef, useState } from 'react'
+import CopyIcon from '../../../shared/components/icons/Copy'
+import styles from '../Table/styles.module.scss'
+
+const enum CopyButtonState {
+  DEFAULT,
+  SUCCESS,
+  FAILURE
+}
+
+const SuccessIcon = () => (
+  <span className={styles.copySuccess} title="Copy successful">
+    &#10004;
+  </span>
+)
+
+const FailureIcon = () => (
+  <span className={styles.copyFailed} title="Copy failed">
+    &#10005;
+  </span>
+)
+
+const copyIconComponents: Record<CopyButtonState, FC> = {
+  [CopyButtonState.DEFAULT]: CopyIcon,
+  [CopyButtonState.SUCCESS]: SuccessIcon,
+  [CopyButtonState.FAILURE]: FailureIcon
+}
+
+export const CopyButton: React.FC<{ value: string }> = ({ value }) => {
+  const timer = useRef<number>()
+  const [state, setState] = useState<CopyButtonState>(CopyButtonState.DEFAULT)
+  const IconComponent = copyIconComponents[state]
+  useEffect(
+    () => () => {
+      if (timer.current) {
+        window.clearTimeout(timer.current)
+      }
+    },
+    []
+  )
+  return (
+    <button
+      title="Copy cell contents"
+      className={styles.copyButton}
+      onClick={() => {
+        navigator.clipboard
+          .writeText(value)
+          .then(() => {
+            setState(CopyButtonState.SUCCESS)
+          })
+          .catch(() => {
+            setState(CopyButtonState.FAILURE)
+          })
+          .finally(() => {
+            if (timer.current) {
+              window.clearTimeout(timer.current)
+            }
+            timer.current = window.setTimeout(() => {
+              setState(CopyButtonState.DEFAULT)
+            }, 1000)
+          })
+      }}
+    >
+      <IconComponent />
+    </button>
+  )
+}
