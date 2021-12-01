aafaead41c99eb912862cfbad7650b141c9a9bb7
Stephanie Roy <stephanie@iterative.ai>
2021-12-01 10:53:39 -0500

    Merge master in toggle-plots

--- extension/src/plots/data/index.ts
+++ extension/src/plots/data/index.ts
@@ -1,6 +1,6 @@
 import { AvailableCommands, InternalCommands } from '../../commands/internal'
 import { BaseData } from '../../data'
-import { PlotsOutput } from '../../cli/reader'
+import { PlotsOutput } from '../../plots/webview/contract'
 
 export class PlotsData extends BaseData<PlotsOutput> {
   constructor(dvcRoot: string, internalCommands: InternalCommands) {
