1924267d5b07ba56fde5aeaa9dddde1a97b51cdb
rogermparent <rogermparent@gmail.com>
2021-12-06 21:20:49 -0500

    Refactor message handling into a hook shared between both Webviews

--- webview/src/experiments/components/App.tsx
+++ webview/src/experiments/components/App.tsx
@@ -1,38 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import {
-  MessageFromWebviewType,
   MessageToWebview,
   MessageToWebviewType
 } from 'dvc/src/webview/contract'
 import { TableData } from 'dvc/src/experiments/webview/contract'
 import Experiments from './Experiments'
 import { vsCodeApi } from '../../shared/api'
-
-const signalInitialized = () =>
-  vsCodeApi.postMessage({ type: MessageFromWebviewType.INITIALIZED })
+import { useVsCodeMessages } from '../../shared/hooks/useVsCodeMessages'
 
 export const App: React.FC<Record<string, unknown>> = () => {
   const [tableData, setTableData] = useState<TableData>()
-  useEffect(() => {
-    signalInitialized()
-  }, [])
-  useEffect(() => {
-    const messageListener = ({
-      data
-    }: {
-      data: MessageToWebview<TableData>
-    }) => {
-      switch (data.type) {
-        case MessageToWebviewType.SET_DATA:
-          setTableData(data.data)
-          return
-        case MessageToWebviewType.SET_DVC_ROOT:
-          vsCodeApi.setState({ dvcRoot: data.dvcRoot })
-      }
-    }
-    window.addEventListener('message', messageListener)
-    return () => window.removeEventListener('message', messageListener)
-  }, [setTableData])
+  useVsCodeMessages(
+    useCallback(
+      ({ data }: { data: MessageToWebview<TableData> }) => {
+        switch (data.type) {
+          case MessageToWebviewType.SET_DATA:
+            setTableData(data.data)
+            return
+          case MessageToWebviewType.SET_DVC_ROOT:
+            vsCodeApi.setState({ dvcRoot: data.dvcRoot })
+        }
+      },
+      [setTableData]
+    )
+  )
 
   return <Experiments tableData={tableData} />
 }
