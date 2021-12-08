65f1558bba80df0d85ec71a4633a8e5b7e79eaf1
Stephanie Roy <stephanie@iterative.ai>
2021-12-08 09:51:26 -0500

    add tests

--- webview/src/plots/components/App.test.tsx
+++ webview/src/plots/components/App.test.tsx
@@ -16,10 +16,10 @@ import {
   MessageFromWebviewType,
   MessageToWebviewType
 } from 'dvc/src/webview/contract'
+import { PlotSize } from 'dvc/src/plots/webview/contract'
 import { mocked } from 'ts-jest/utils'
 import { App } from './App'
 import Plots from './Plots'
-
 import { vsCodeApi } from '../../shared/api'
 
 import {
@@ -317,5 +317,36 @@ describe('App', () => {
     fireEvent.click(largeButton)
     svg = await getPlotSvg()
     expect(svg.getAttribute('height')).toBe(toStringSize(750, 48))
+  })
+
+  it('should send a message to the extension with the selected size when changing the size of plots', () => {
+    const initialState = {
+      collapsedSections: defaultCollapsibleSectionsState,
+      data: {
+        live: livePlotsFixture
+      }
+    }
+    mockGetState.mockReturnValue(initialState)
+    render(<App />)
+
+    const [, sizeButton] = screen.getAllByTestId('icon-menu-item')
+    fireEvent.mouseEnter(sizeButton)
+    fireEvent.click(sizeButton)
+
+    const largeButton = screen.getByText('Large')
+    fireEvent.click(largeButton)
+
+    expect(mockPostMessage).toBeCalledWith({
+      payload: PlotSize.LARGE,
+      type: MessageFromWebviewType.PLOTS_RESIZED
+    })
+
+    const smallButton = screen.getByText('Small')
+    fireEvent.click(smallButton)
+
+    expect(mockPostMessage).toBeCalledWith({
+      payload: PlotSize.SMALL,
+      type: MessageFromWebviewType.PLOTS_RESIZED
+    })
   })
 })
