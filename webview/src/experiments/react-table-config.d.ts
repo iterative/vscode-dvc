051d2b0ecafc4e86a7a3934419ac73bbf6f789cd
rogermparent <rogermparent@gmail.com>
2021-12-14 14:29:57 -0500

    Remove comments

--- webview/src/experiments/react-table-config.d.ts
+++ webview/src/experiments/react-table-config.d.ts
@@ -16,69 +16,32 @@ import {
 
 declare module 'react-table' {
   export interface TableOptions<D extends Record<string, unknown>>
-    // UseFiltersOptions<D>,
-    // UseGlobalFiltersOptions<D>,
-    // UseGroupByOptions<D>,
-    // UsePaginationOptions<D>,
-    // UseRowSelectOptions<D>,
-    // UseRowStateOptions<D>,
-    // UseSortByOptions<D>,
     extends UseExpandedOptions<D>,
       UseResizeColumnsOptions<D> {}
 
   export interface Hooks<
-      D extends Record<string, unknown> = Record<string, unknown>
-    >
-    // UseGroupByHooks<D>,
-    // UseRowSelectHooks<D>,
-    // UseSortByHooks<D>
-    extends UseExpandedHooks<D> {}
+    D extends Record<string, unknown> = Record<string, unknown>
+  > extends UseExpandedHooks<D> {}
 
   export interface TableInstance<
-      D extends Record<string, unknown> = Record<string, unknown>
-    >
-    // UseFiltersInstanceProps<D>,
-    // UseGlobalFiltersInstanceProps<D>,
-    // UseGroupByInstanceProps<D>,
-    // UsePaginationInstanceProps<D>,
-    // UseRowSelectInstanceProps<D>,
-    // UseRowStateInstanceProps<D>,
-    // UseSortByInstanceProps<D>,
-    extends UseColumnOrderInstanceProps<D>,
+    D extends Record<string, unknown> = Record<string, unknown>
+  > extends UseColumnOrderInstanceProps<D>,
       UseExpandedInstanceProps<D> {}
 
   export interface TableState<
-      D extends Record<string, unknown> = Record<string, unknown>
-    >
-    // UseFiltersState<D>,
-    // UseGlobalFiltersState<D>,
-    // UseGroupByState<D>,
-    // UsePaginationState<D>,
-    // UseRowSelectState<D>,
-    // UseRowStateState<D>,
-    // UseSortByState<D>,
-    extends UseColumnOrderState<D>,
+    D extends Record<string, unknown> = Record<string, unknown>
+  > extends UseColumnOrderState<D>,
       UseExpandedState<D>,
       UseResizeColumnsState<D> {}
 
   export interface ColumnInterface<
-      D extends Record<string, unknown> = Record<string, unknown>
-    >
-    // UseFiltersColumnOptions<D>,
-    // UseGlobalFiltersColumnOptions<D>,
-    // UseGroupByColumnOptions<D>,
-    // UseSortByColumnOptions<D>,
-    extends UseResizeColumnsColumnOptions<D> {}
+    D extends Record<string, unknown> = Record<string, unknown>
+  > extends UseResizeColumnsColumnOptions<D> {}
 
-  // UseFiltersColumnProps<D>,
-  // UseGroupByColumnProps<D>,
-  // UseSortByColumnProps<D>
   export interface ColumnInstance<
     D extends Record<string, unknown> = Record<string, unknown>
   > extends UseResizeColumnsColumnProps<D> {}
 
-  // UseGroupByCellProps<D>,
-  // UseRowStateCellProps<D>
   export interface Cell<
     D extends Record<string, unknown> = Record<string, unknown>,
     V = unknown
@@ -86,9 +49,6 @@ declare module 'react-table' {
     isPlaceholder: boolean
   }
 
-  // UseGroupByRowProps<D>,
-  // UseRowSelectRowProps<D>,
-  // UseRowStateRowProps<D>
   export interface Row<
     D extends Record<string, unknown> = Record<string, unknown>
   > extends UseExpandedRowProps<D> {
