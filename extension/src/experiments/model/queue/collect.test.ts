5a5fc8c5b5be7cad54c62a9df9a37276edaf6578
Matt Seddon <mattseddon@hotmail.com>
2021-12-15 16:32:08 +1100

    fix collect test

--- extension/src/experiments/model/queue/collect.test.ts
+++ extension/src/experiments/model/queue/collect.test.ts
@@ -1,18 +1,41 @@
 import { collectFlatExperimentParams } from './collect'
 import rowsFixture from '../../../test/fixtures/expShow/rows'
+import { joinParamOrMetricFilePath } from '../../paramsAndMetrics/paths'
+import { join } from '../../../test/util/path'
 
-describe('flattenExperimentParams', () => {
-  it('should do the thing', () => {
+describe('collectFlatExperimentParams', () => {
+  it('should flatten the params into an array', () => {
     const params = collectFlatExperimentParams(rowsFixture[0].params)
     expect(params).toEqual([
-      { path: 'params.yaml:epochs', value: 2 },
-      { path: 'params.yaml:learning_rate', value: 2.2e-7 },
-      { path: 'params.yaml:dvc_logs_dir', value: 'dvc_logs' },
-      { path: 'params.yaml:log_file', value: 'logs.csv' },
-      { path: 'params.yaml:dropout', value: 0.122 },
-      { path: 'params.yaml:process.threshold', value: 0.86 },
-      { path: 'params.yaml:process.test_arg', value: 'string' },
-      { path: 'nested/params.yaml:test', value: true }
+      { path: joinParamOrMetricFilePath('params.yaml', 'epochs'), value: 2 },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'learning_rate'),
+        value: 2.2e-7
+      },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'dvc_logs_dir'),
+        value: 'dvc_logs'
+      },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'log_file'),
+        value: 'logs.csv'
+      },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'dropout'),
+        value: 0.122
+      },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'process.threshold'),
+        value: 0.86
+      },
+      {
+        path: joinParamOrMetricFilePath('params.yaml', 'process.test_arg'),
+        value: 'string'
+      },
+      {
+        path: joinParamOrMetricFilePath(join('nested', 'params.yaml'), 'test'),
+        value: true
+      }
     ])
   })
 })
