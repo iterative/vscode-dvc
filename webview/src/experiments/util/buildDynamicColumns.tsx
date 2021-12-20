7d001d088d154d09bfc4baab72f0e58a58351dcd
rogermparent <rogermparent@gmail.com>
2021-12-20 15:11:55 -0500

    Remove fixColumnsNesting

--- webview/src/experiments/util/buildDynamicColumns.tsx
+++ webview/src/experiments/util/buildDynamicColumns.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import get from 'lodash/get'
-import { Column, Accessor, ColumnGroup, ColumnInstance } from 'react-table'
+import { Column, Accessor, ColumnGroup } from 'react-table'
 import { splitParamOrMetricPath } from 'dvc/src/experiments/paramsAndMetrics/paths'
 import { Experiment, ParamOrMetric } from 'dvc/src/experiments/webview/contract'
 import { formatFloat } from './numberFormatting'
@@ -59,67 +59,4 @@ const buildDynamicColumns = (
       return column
     })
 
-const findMaxDepth = (columns: ColumnGroup<Experiment>[], depth = 1): number =>
-  columns.reduce(
-    (prev: number, curr: ColumnGroup<Experiment>) =>
-      curr.columns
-        ? Math.max(
-            prev,
-            findMaxDepth(curr.columns as ColumnGroup<Experiment>[], depth + 1)
-          )
-        : depth,
-    1
-  )
-
-const findDeepest = (
-  depth: number,
-  columns: ColumnGroup<Experiment>[] | undefined,
-  maxDepth: number
-) => (!depth && columns ? findMaxDepth(columns) : maxDepth)
-
-const fixColumnsNesting = (
-  columns: Column<Experiment>[],
-  parent?: Column<Experiment>,
-  depth = 0,
-  maxDepth = 0
-) =>
-  (columns as ColumnGroup<Experiment>[]).map(
-    (column: ColumnGroup<Experiment>) => {
-      const deepest = findDeepest(
-        depth,
-        column.columns as ColumnGroup<Experiment>[],
-        maxDepth
-      )
-      const needsPlaceholder = deepest > depth
-
-      if (column.columns || needsPlaceholder) {
-        const newDepth = depth + 1
-        const nextColumns = (column.columns || [
-          { ...column }
-        ]) as ColumnGroup<Experiment>[]
-
-        if (!column.columns) {
-          ;(column as Partial<ColumnInstance<Experiment>>) = {
-            Header: '',
-            id: `${column.id}_previous_placeholder`,
-            parent: parent as ColumnInstance<Experiment>,
-            placeholderOf: column as ColumnInstance<{}>
-          }
-        }
-
-        column.columns = fixColumnsNesting(
-          nextColumns,
-          column,
-          newDepth,
-          deepest
-        )
-      }
-
-      return column
-    }
-  )
-
-const buildColumns = (properties: ParamOrMetric[], parentPath: string) =>
-  fixColumnsNesting(buildDynamicColumns(properties, parentPath))
-
-export default buildColumns
+export default buildDynamicColumns
