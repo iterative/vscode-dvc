2e766537efd7f4cfe77bd5f465b30913f3c43d90
rogermparent <rogermparent@gmail.com>
2022-01-27 14:25:04 -0500

    Use path.join and joinMetricOrParamPath in columns test

--- webview/src/experiments/util/buildDynamicColumns.test.ts
+++ webview/src/experiments/util/buildDynamicColumns.test.ts
@@ -5,12 +5,15 @@ import { Experiment } from 'dvc/src/experiments/webview/contract'
 import { collectMetricsAndParams } from 'dvc/src/experiments/metricsAndParams/collect'
 import { columns as deeplyNestedColumnsFixture } from 'dvc/src/test/fixtures/expShow/deeplyNested'
 import columnsFixture from 'dvc/src/test/fixtures/expShow/columns'
+import { joinMetricOrParamPath } from 'dvc/src/experiments/metricsAndParams/paths'
 import buildDynamicColumns from './buildDynamicColumns'
 
 interface MinimalColumn {
   id?: string
   columns?: MinimalColumn[]
 }
+
+const nestedParamsFile = join('nested', 'params.yaml')
 
 const simplifyColumns = (columns: ColumnGroup<Experiment>[]) =>
   columns.map(({ id, columns }) => {
@@ -31,48 +34,108 @@ describe('buildDynamicColumns', () => {
     ).toEqual([
       {
         columns: [
-          { id: 'metrics:summary.json:loss' },
-          { id: 'metrics:summary.json:accuracy' },
-          { id: 'metrics:summary.json:val_loss' },
-          { id: 'metrics:summary.json:val_accuracy' }
+          { id: joinMetricOrParamPath('metrics', 'summary.json', 'loss') },
+          { id: joinMetricOrParamPath('metrics', 'summary.json', 'accuracy') },
+          { id: joinMetricOrParamPath('metrics', 'summary.json', 'val_loss') },
+          {
+            id: joinMetricOrParamPath('metrics', 'summary.json', 'val_accuracy')
+          }
         ],
-        id: 'metrics:summary.json'
+        id: joinMetricOrParamPath('metrics', 'summary.json')
       },
       {
         columns: [
           {
-            columns: [{ id: 'params:params.yaml:epochs' }],
-            id: 'params:params.yaml:epochs_previous_placeholder'
-          },
-          {
-            columns: [{ id: 'params:params.yaml:learning_rate' }],
-            id: 'params:params.yaml:learning_rate_previous_placeholder'
-          },
-          {
-            columns: [{ id: 'params:params.yaml:dvc_logs_dir' }],
-            id: 'params:params.yaml:dvc_logs_dir_previous_placeholder'
-          },
-          {
-            columns: [{ id: 'params:params.yaml:log_file' }],
-            id: 'params:params.yaml:log_file_previous_placeholder'
-          },
-          {
-            columns: [{ id: 'params:params.yaml:dropout' }],
-            id: 'params:params.yaml:dropout_previous_placeholder'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'epochs') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'epochs_previous_placeholder'
+            )
           },
           {
             columns: [
-              { id: 'params:params.yaml:process.threshold' },
-              { id: 'params:params.yaml:process.test_arg' }
+              {
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'learning_rate'
+                )
+              }
             ],
-            id: 'params:params.yaml:process'
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'learning_rate_previous_placeholder'
+            )
+          },
+          {
+            columns: [
+              {
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'dvc_logs_dir'
+                )
+              }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'dvc_logs_dir_previous_placeholder'
+            )
+          },
+          {
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'log_file') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'log_file_previous_placeholder'
+            )
+          },
+          {
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'dropout') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'dropout_previous_placeholder'
+            )
+          },
+          {
+            columns: [
+              {
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'process',
+                  'threshold'
+                )
+              },
+              {
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'process',
+                  'test_arg'
+                )
+              }
+            ],
+            id: joinMetricOrParamPath('params', 'params.yaml', 'process')
           }
         ],
-        id: 'params:params.yaml'
+        id: joinMetricOrParamPath('params', 'params.yaml')
       },
       {
-        columns: [{ id: `params:${join('nested', 'params.yaml')}:test` }],
-        id: `params:${join('nested', 'params.yaml')}`
+        columns: [
+          { id: joinMetricOrParamPath('params', nestedParamsFile, 'test') }
+        ],
+        id: joinMetricOrParamPath('params', nestedParamsFile)
       }
     ])
   })
@@ -88,32 +151,33 @@ describe('buildDynamicColumns', () => {
               {
                 columns: [
                   {
-                    columns: [{ id: 'params:params.yaml:nested1.doubled' }],
-                    id: 'params:params.yaml:nested1.doubled_previous_placeholder'
-                  }
-                ],
-                id: 'params:params.yaml:nested1.doubled_previous_placeholder'
-              }
-            ],
-            id: 'params:params.yaml:nested1'
-          },
-          {
-            columns: [
-              {
-                columns: [
-                  {
                     columns: [
                       {
-                        id: 'params:params.yaml:nested1%2Enested2%2Enested3%2Enested4.nested5.nested6.nested7'
+                        id: joinMetricOrParamPath(
+                          'params',
+                          'params.yaml',
+                          'nested1',
+                          'doubled'
+                        )
                       }
                     ],
-                    id: 'params:params.yaml:nested1%2Enested2%2Enested3%2Enested4.nested5.nested6'
+                    id: joinMetricOrParamPath(
+                      'params',
+                      'params.yaml',
+                      'nested1',
+                      'doubled_previous_placeholder'
+                    )
                   }
                 ],
-                id: 'params:params.yaml:nested1%2Enested2%2Enested3%2Enested4.nested5'
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'nested1',
+                  'doubled_previous_placeholder'
+                )
               }
             ],
-            id: 'params:params.yaml:nested1%2Enested2%2Enested3%2Enested4'
+            id: joinMetricOrParamPath('params', 'params.yaml', 'nested1')
           },
           {
             columns: [
@@ -122,36 +186,125 @@ describe('buildDynamicColumns', () => {
                   {
                     columns: [
                       {
-                        id: 'params:params.yaml:nested1%2Enested2%2Enested3.nested4.nested5b.nested6'
+                        id: joinMetricOrParamPath(
+                          'params',
+                          'params.yaml',
+                          'nested1%2Enested2%2Enested3%2Enested4',
+                          'nested5',
+                          'nested6',
+                          'nested7'
+                        )
+                      }
+                    ],
+                    id: joinMetricOrParamPath(
+                      'params',
+                      'params.yaml',
+                      'nested1%2Enested2%2Enested3%2Enested4',
+                      'nested5',
+                      'nested6'
+                    )
+                  }
+                ],
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'nested1%2Enested2%2Enested3%2Enested4',
+                  'nested5'
+                )
+              }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'nested1%2Enested2%2Enested3%2Enested4'
+            )
+          },
+          {
+            columns: [
+              {
+                columns: [
+                  {
+                    columns: [
+                      {
+                        id: joinMetricOrParamPath(
+                          'params',
+                          'params.yaml',
+                          'nested1%2Enested2%2Enested3',
+                          'nested4',
+                          'nested5b',
+                          'nested6'
+                        )
                       },
                       {
-                        id: 'params:params.yaml:nested1%2Enested2%2Enested3.nested4.nested5b.doubled'
+                        id: joinMetricOrParamPath(
+                          'params',
+                          'params.yaml',
+                          'nested1%2Enested2%2Enested3',
+                          'nested4',
+                          'nested5b',
+                          'doubled'
+                        )
                       }
                     ],
-                    id: 'params:params.yaml:nested1%2Enested2%2Enested3.nested4.nested5b'
+                    id: joinMetricOrParamPath(
+                      'params',
+                      'params.yaml',
+                      'nested1%2Enested2%2Enested3',
+                      'nested4',
+                      'nested5b'
+                    )
                   }
                 ],
-                id: 'params:params.yaml:nested1%2Enested2%2Enested3.nested4'
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'nested1%2Enested2%2Enested3',
+                  'nested4'
+                )
               }
             ],
-            id: 'params:params.yaml:nested1%2Enested2%2Enested3'
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'nested1%2Enested2%2Enested3'
+            )
           },
           {
             columns: [
               {
                 columns: [
                   {
-                    columns: [{ id: 'params:params.yaml:outlier' }],
-                    id: 'params:params.yaml:outlier_previous_placeholder'
+                    columns: [
+                      {
+                        id: joinMetricOrParamPath(
+                          'params',
+                          'params.yaml',
+                          'outlier'
+                        )
+                      }
+                    ],
+                    id: joinMetricOrParamPath(
+                      'params',
+                      'params.yaml',
+                      'outlier_previous_placeholder'
+                    )
                   }
                 ],
-                id: 'params:params.yaml:outlier_previous_placeholder'
+                id: joinMetricOrParamPath(
+                  'params',
+                  'params.yaml',
+                  'outlier_previous_placeholder'
+                )
               }
             ],
-            id: 'params:params.yaml:outlier_previous_placeholder'
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'outlier_previous_placeholder'
+            )
           }
         ],
-        id: 'params:params.yaml'
+        id: joinMetricOrParamPath('params', 'params.yaml')
       }
     ])
   })
@@ -179,15 +332,23 @@ describe('buildDynamicColumns', () => {
       {
         columns: [
           {
-            columns: [{ id: 'params:params.yaml:a' }],
-            id: 'params:params.yaml:a_previous_placeholder'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'a') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'a_previous_placeholder'
+            )
           },
           {
-            columns: [{ id: 'params:params.yaml:c.d' }],
-            id: 'params:params.yaml:c'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'c', 'd') }
+            ],
+            id: joinMetricOrParamPath('params', 'params.yaml', 'c')
           }
         ],
-        id: 'params:params.yaml'
+        id: joinMetricOrParamPath('params', 'params.yaml')
       }
     ])
   })
@@ -215,15 +376,23 @@ describe('buildDynamicColumns', () => {
       {
         columns: [
           {
-            columns: [{ id: 'params:params.yaml:1' }],
-            id: 'params:params.yaml:1_previous_placeholder'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', '1') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              '1_previous_placeholder'
+            )
           },
           {
-            columns: [{ id: 'params:params.yaml:c.d' }],
-            id: 'params:params.yaml:c'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'c', 'd') }
+            ],
+            id: joinMetricOrParamPath('params', 'params.yaml', 'c')
           }
         ],
-        id: 'params:params.yaml'
+        id: joinMetricOrParamPath('params', 'params.yaml')
       }
     ])
   })
@@ -247,19 +416,31 @@ describe('buildDynamicColumns', () => {
         }
       }
     })
-    expect(simplifyColumns(buildDynamicColumns(input, 'params'))).toEqual([
+    expect(
+      simplifyColumns(
+        buildDynamicColumns(input, joinMetricOrParamPath('params'))
+      )
+    ).toEqual([
       {
         columns: [
           {
-            columns: [{ id: 'params:params.yaml:c.d' }],
-            id: 'params:params.yaml:c'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'c', 'd') }
+            ],
+            id: joinMetricOrParamPath('params', 'params.yaml', 'c')
           },
           {
-            columns: [{ id: 'params:params.yaml:f' }],
-            id: 'params:params.yaml:f_previous_placeholder'
+            columns: [
+              { id: joinMetricOrParamPath('params', 'params.yaml', 'f') }
+            ],
+            id: joinMetricOrParamPath(
+              'params',
+              'params.yaml',
+              'f_previous_placeholder'
+            )
           }
         ],
-        id: 'params:params.yaml'
+        id: joinMetricOrParamPath('params', 'params.yaml')
       }
     ])
   })
