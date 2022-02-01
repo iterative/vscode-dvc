0b445dd30cff2e72e82a5002d698d5599ad1f0e7
Matt Seddon <mattseddon@hotmail.com>
2022-02-01 14:05:44 +1100

    revert timeout of test

--- extension/src/test/suite/plots/index.test.ts
+++ extension/src/test/suite/plots/index.test.ts
@@ -151,6 +151,6 @@ suite('Plots Test Suite', () => {
 
       expect(webview.isActive()).to.be.true
       expect(webview.isVisible()).to.be.true
-    }).timeout(80000)
+    }).timeout(8000)
   })
 })
