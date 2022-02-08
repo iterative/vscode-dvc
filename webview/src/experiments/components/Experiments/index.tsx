29ab8366036dc4ccb32b501d3afa88aacbe7fc5b
rogermparent <rogermparent@gmail.com>
2022-02-07 21:06:32 -0500

    Use parent id in generated ids

--- webview/src/experiments/components/Experiments/index.tsx
+++ webview/src/experiments/components/Experiments/index.tsx
@@ -106,7 +106,11 @@ const reportResizedColumn = (state: TableState<Experiment>) => {
 export const ExperimentsTable: React.FC<{
   tableData: InitiallyUndefinedTableData
 }> = ({ tableData: initiallyUndefinedTableData }) => {
-  const getRowId = useCallback(({ id }: Experiment) => id, [])
+  const getRowId = useCallback(
+    (experiment: Experiment, _index, parent?: Row<Experiment>) =>
+      parent ? [parent.id, experiment.id].join('.') : experiment.id,
+    []
+  )
   const [tableData, columns, defaultColumn, initialState] =
     React.useMemo(() => {
       const tableData: TableData = {
