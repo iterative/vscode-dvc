6ff3e032f5cbb7b1ee355e8163adceb774aad3c7
rogermparent <rogermparent@gmail.com>
2022-02-15 18:18:14 -0500

    Revert "Remove parent consideration in getRowId"
    This reverts commit f9de2fb8e4cd06b2cf4345adee3b9b2cc76d208c.

--- webview/src/experiments/components/Experiments/index.tsx
+++ webview/src/experiments/components/Experiments/index.tsx
@@ -104,8 +104,11 @@ const reportResizedColumn = (state: TableState<Experiment>) => {
 export const ExperimentsTable: React.FC<{
   tableData: InitiallyUndefinedTableData
 }> = ({ tableData: initiallyUndefinedTableData }) => {
-  const getRowId = useCallback(({ id }: Experiment) => id, [])
-
+  const getRowId = useCallback(
+    (experiment: Experiment, _index, parent?: Row<Experiment>) =>
+      parent ? [parent.id, experiment.id].join('.') : experiment.id,
+    []
+  )
   const [tableData, columns, defaultColumn, initialState] =
     React.useMemo(() => {
       const tableData: TableData = {
