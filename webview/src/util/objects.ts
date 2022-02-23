3859880f84fe221d566d5c9f27cf3bd78a533ae9
Stephanie Roy <stephanie@iterative.ai>
2022-02-23 13:31:58 -0500

    Fix codeclimate issue

--- webview/src/util/objects.ts
+++ webview/src/util/objects.ts
@@ -1,11 +1,21 @@
+type Items = { [key: string]: unknown }[]
+
+export const stringToObjectList = (
+  stringList: string[],
+  items: Items,
+  compareKey: string
+) => {
+  return stringList
+    .map(orderedItem => items.find(item => item[compareKey] === orderedItem))
+    .filter(Boolean)
+}
+
 export const performOrderedUpdate = (
   order: string[],
-  items: { [key: string]: unknown }[],
+  items: Items,
   key: string
 ): string[] => {
-  const current = order
-    .map(orderedItem => items.find(item => item[key] === orderedItem))
-    .filter(Boolean)
+  const current = stringToObjectList(order, items, key)
   const added = items.filter(item => !order.includes(item[key] as string))
 
   return [...current, ...added].map(item => item?.[key]) as string[]
