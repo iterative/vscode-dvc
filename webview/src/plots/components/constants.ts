028a469823156fc545e93ec1c5bd1e8c9cc0903c
Stephanie Roy <stephanie@iterative.ai>
2022-03-03 13:45:35 -0500

    Made the plot axis labels more readable

--- webview/src/plots/components/constants.ts
+++ webview/src/plots/components/constants.ts
@@ -1,5 +1,5 @@
 import { LivePlotsColors } from 'dvc/src/plots/webview/contract'
-import { Config } from 'vega'
+import { Config, FontWeight } from 'vega'
 import { VisualizationSpec } from 'react-vega'
 
 export const createSpec = (
@@ -93,19 +93,29 @@ export const createSpec = (
 
 const foregroundColor = 'var(--vscode-foreground)'
 const backgroundColor = 'var(--vscode-editor-foreground-transparency-1)'
+const font = 'var(--vscode-editor-font-family)'
+const fontWeight = 'normal' as FontWeight
+
+const title = {
+  font,
+  fontSize: 16,
+  fontWeight
+}
+
 export const config: Config = {
   axis: {
     domain: false,
     gridOpacity: 0.25,
     labelAngle: 0,
     tickColor: foregroundColor,
-    titleColor: foregroundColor
+    titleColor: foregroundColor,
+    titlePadding: 15
   },
   background: backgroundColor,
   mark: {
     stroke: foregroundColor
   },
-  padding: 10,
+  padding: 20,
   rule: {
     stroke: foregroundColor
   },
@@ -115,15 +125,19 @@ export const config: Config = {
     },
     'group-title': {
       fill: foregroundColor,
-      stroke: foregroundColor
+      stroke: foregroundColor,
+      ...title
     },
     'guide-label': {
       fill: foregroundColor,
-      stroke: foregroundColor
+      stroke: foregroundColor,
+      font,
+      fontWeight
     },
     'guide-title': {
       fill: foregroundColor,
-      stroke: foregroundColor
+      stroke: foregroundColor,
+      ...title
     },
     rule: {
       fill: foregroundColor,
