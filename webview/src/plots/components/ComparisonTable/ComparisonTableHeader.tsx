97e5797d150e78cd9d0bb7cc2fff7e6ce75ef360
Stephanie Roy <sroy3@users.noreply.github.com>
2022-03-04 14:13:46 -0500

    Add gripper to comparison table (#1390)

--- webview/src/plots/components/ComparisonTable/ComparisonTableHeader.tsx
+++ webview/src/plots/components/ComparisonTable/ComparisonTableHeader.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames'
 import React from 'react'
 import styles from './styles.module.scss'
 import { Pin } from '../../../shared/components/icons'
+import { GripIcon } from '../../../shared/components/dragDrop/GripIcon'
 
 export interface ComparisonTableHeaderProps {
   displayColor: string
@@ -21,6 +22,7 @@ export const ComparisonTableHeader: React.FC<ComparisonTableHeaderProps> = ({
 
   return (
     <button className={styles.header} onClick={onClicked}>
+      {!isPinned && <GripIcon className={styles.gripIcon} />}
       <div className={pinClasses}>
         <Pin />
       </div>
