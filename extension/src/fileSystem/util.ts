424aa6521d3801e92e428223a37de9cfa21de861
Matt Seddon <mattseddon@hotmail.com>
2022-03-08 15:52:47 +1100

    collect extra plot path info

---
+++ extension/src/fileSystem/util.ts
@@ -0,0 +1,17 @@
+import { sep } from 'path'
+
+export const getPathArray = (path: string): string[] => path.split(sep)
+
+export const getPath = (pathArray: string[], idx: number) =>
+  pathArray.slice(0, idx).join(sep)
+
+export const getDirectChild = (pathArray: string[], idx: number) =>
+  getPath(pathArray, idx + 1)
+
+export const getParent = (pathArray: string[], idx: number) => {
+  const parent = getPath(pathArray, idx - 1)
+  if (!parent) {
+    return undefined
+  }
+  return parent
+}
