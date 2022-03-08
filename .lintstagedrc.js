f8ecb4dfaa457899dcf54a8e392aae4610af0843
rogermparent <rogermparent@gmail.com>
2022-03-08 14:06:19 -0500

    Re-add jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '**/*.{js,ts,tsx}': 'eslint --cache',
+  '**/*.{js,ts,tsx}': ['eslint --cache', 'jest --bail --findRelatedTests'],
   '**/*.{md,json,yml,yaml,js,ts,tsx}': 'prettier --write'
 }
