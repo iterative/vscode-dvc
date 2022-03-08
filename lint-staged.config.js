bd21ad947cb594c1250a902bbe2fb58e6d48eb59
Matt Seddon <mattseddon@hotmail.com>
2022-03-08 13:15:46 +1100

    try commit error

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '**/*.{js,ts,tsx}': 'eslint --fix',
+  '**/*.{js,ts,tsx}': 'eslint --cache "**/*.{js,ts,tsx}" --fix',
   '**/*.{md,json,yml,yaml,js,ts,tsx}': 'prettier --write'
 }
