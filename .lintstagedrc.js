280786ca5e0e4aa44a891b642c7cf9a337887437
rogermparent <rogermparent@gmail.com>
2022-03-08 13:58:46 -0500

    Refactor lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '**/*.{js,ts,tsx}': 'eslint --cache "**/*.{js,ts,tsx}" --fix',
+  '**/*.{js,ts,tsx}': 'eslint --cache',
   '**/*.{md,json,yml,yaml,js,ts,tsx}': 'prettier --write'
 }
