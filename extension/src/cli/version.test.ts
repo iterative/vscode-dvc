aa528bafeb79cecfd40545a440ed7dd593840383
Matt Seddon <mattseddon@hotmail.com>
2022-03-15 16:20:39 +1100

    self review

--- extension/src/cli/version.test.ts
+++ extension/src/cli/version.test.ts
@@ -1,4 +1,4 @@
-import { isVersionCompatible, MIN_VERSION, splitSemver } from './version'
+import { isVersionCompatible, MIN_VERSION, extractSemver } from './version'
 import { getConfigValue, setUserConfigValue } from '../vscode/config'
 import { Response } from '../vscode/response'
 import { Toast } from '../vscode/toast'
@@ -16,17 +16,17 @@ beforeEach(() => {
   jest.resetAllMocks()
 })
 
-describe('splitSemver', () => {
+describe('extractSemver', () => {
   it('should return undefined if the string does not contain a semver', () => {
-    expect(splitSemver('obviously not a version')).toBeUndefined()
+    expect(extractSemver('obviously not a version')).toBeUndefined()
   })
 
   it('should return undefined if the string contains a malformed semver', () => {
-    expect(splitSemver('A1.2.1')).toBeUndefined()
+    expect(extractSemver('A1.2.1')).toBeUndefined()
   })
 
   it('should return the expected values from a semver string', () => {
-    expect(splitSemver('1.2.1')).toStrictEqual({
+    expect(extractSemver('1.2.1')).toStrictEqual({
       major: 1,
       minor: 2,
       patch: 1
@@ -34,7 +34,7 @@ describe('splitSemver', () => {
   })
 
   it('should return the expected values from a dev semver string', () => {
-    expect(splitSemver('2.9.6.dev11+gab024a47')).toStrictEqual({
+    expect(extractSemver('2.9.6.dev11+gab024a47')).toStrictEqual({
       major: 2,
       minor: 9,
       patch: 6
--- extension/src/cli/version.ts
+++ extension/src/cli/version.ts
@@ -6,7 +6,7 @@ export const MIN_VERSION = '2.9.5'
 const MAX_VERSION = '3'
 const DO_NOT_WARN_UNEXPECTED_VERSION = 'dvc.doNotWarnCLIVersion'
 
-export const splitSemver = (
+export const extractSemver = (
   stdout: string
 ): { major: number; minor: number; patch: number } | undefined => {
   const semver = stdout.match(/^(\d+\.)?(\d+\.)?(\*|\d+)/)
@@ -21,42 +21,37 @@ const getWarningText = (
   currentVersion: string,
   warn: string,
   update: 'CLI' | 'extension'
-) => `You are using version ${currentVersion} of the DVC CLI. The expected version is ${MIN_VERSION} <= DVC < ${MAX_VERSION}.
+): string => `You are using version ${currentVersion} of the DVC CLI. The expected version is ${MIN_VERSION} <= DVC < ${MAX_VERSION}.
 ${warn} will lead to the extension behaving in unexpected ways. It is recommended that you upgrade to the most recent version of the ${update}.`
 
-const sendWarning = async (text: string) => {
+const sendWarning = async (text: string): Promise<void> => {
   const response = await Toast.warnWithOptions(text, Response.NEVER)
-  if (response !== Response.NEVER) {
-    return
+  if (response === Response.NEVER) {
+    setUserConfigValue(DO_NOT_WARN_UNEXPECTED_VERSION, true)
   }
-  setUserConfigValue(DO_NOT_WARN_UNEXPECTED_VERSION, true)
 }
 
-export const isVersionCompatible = (version: string) => {
-  if (getConfigValue<boolean>(DO_NOT_WARN_UNEXPECTED_VERSION)) {
-    return
-  }
+const getTextAndSend = (
+  version: string,
+  warn: string,
+  update: 'CLI' | 'extension'
+): Promise<void> => {
+  const text = getWarningText(version, warn, update)
+  return sendWarning(text)
+}
 
-  const currentSemVer = splitSemver(version)
-  if (!currentSemVer) {
-    return sendWarning(
-      'Unable to verify the DVC CLI version. The extension could behave in unexpected ways.'
-    )
-  }
-
+const checkCLIVersion = (
+  version: string,
+  currentSemVer: { major: number; minor: number; patch: number }
+): Promise<void> | void => {
   const {
     major: currentMajor,
     minor: currentMinor,
     patch: currentPatch
   } = currentSemVer
 
-  if (currentMajor > 2) {
-    const aheadWarning = getWarningText(
-      version,
-      'Being a major version ahead',
-      'extension'
-    )
-    return sendWarning(aheadWarning)
+  if (currentMajor >= Number(MAX_VERSION)) {
+    return getTextAndSend(version, 'Being a major version ahead', 'extension')
   }
 
   const [minMajor, minMinor, minPatch] = MIN_VERSION.split('.')
@@ -66,11 +61,25 @@ export const isVersionCompatible = (version: string) => {
     currentMinor < Number(minMinor) ||
     currentPatch < Number(minPatch)
   ) {
-    const behindWarning = getWarningText(
+    return getTextAndSend(
       version,
       `Using any version before ${MIN_VERSION}`,
       'CLI'
     )
-    return sendWarning(behindWarning)
   }
+}
+
+export const isVersionCompatible = (version: string): Promise<void> | void => {
+  if (getConfigValue<boolean>(DO_NOT_WARN_UNEXPECTED_VERSION)) {
+    return
+  }
+
+  const currentSemVer = extractSemver(version)
+  if (!currentSemVer) {
+    return sendWarning(
+      'Unable to verify the DVC CLI version. The extension could behave in unexpected ways.'
+    )
+  }
+
+  return checkCLIVersion(version, currentSemVer)
 }
