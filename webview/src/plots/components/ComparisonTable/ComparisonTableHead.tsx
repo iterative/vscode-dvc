87f4a52c6ab1019cc2b3fd4ead21651b3b9fffa0
Matt Seddon <mattseddon@hotmail.com>
2022-03-23 12:49:31 +1100

    send reordered message when column is pinned

--- webview/src/plots/components/ComparisonTable/ComparisonTableHead.tsx
+++ webview/src/plots/components/ComparisonTable/ComparisonTableHead.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { ComparisonRevision } from 'dvc/src/plots/webview/contract'
-import { MessageFromWebviewType } from 'dvc/src/webview/contract'
 import cx from 'classnames'
 import styles from './styles.module.scss'
 import { ComparisonTableHeader } from './ComparisonTableHeader'
 import { DragDropContainer } from '../../../shared/components/dragDrop/DragDropContainer'
-import { reorderObjectList } from '../../../util/objects'
-import { sendMessage } from '../../../shared/vscode'
 
 export type ComparisonTableColumn = ComparisonRevision
 
 interface ComparisonTableHeadProps {
   columns: ComparisonTableColumn[]
   pinnedColumn: string
-  setColumnsOrder: (columns: ComparisonTableColumn[]) => void
+  setColumnsOrder: (columns: string[]) => void
   setPinnedColumn: (column: string) => void
 }
 
@@ -23,19 +20,6 @@ export const ComparisonTableHead: React.FC<ComparisonTableHeadProps> = ({
   setColumnsOrder,
   setPinnedColumn
 }) => {
-  const setOrder = (order: string[]) => {
-    const newOrder = reorderObjectList(
-      order,
-      columns,
-      'revision'
-    ) as ComparisonRevision[]
-    setColumnsOrder(newOrder)
-    sendMessage({
-      payload: newOrder.map(({ revision }) => revision),
-      type: MessageFromWebviewType.PLOTS_COMPARISON_REORDERED
-    })
-  }
-
   const items = columns.map(({ revision, displayColor }) => {
     const isPinned = revision === pinnedColumn
     return (
@@ -62,7 +46,7 @@ export const ComparisonTableHead: React.FC<ComparisonTableHeadProps> = ({
       <tr>
         <DragDropContainer
           order={columns.map(col => col.revision)}
-          setOrder={setOrder}
+          setOrder={setColumnsOrder}
           disabledDropIds={[pinnedColumn]}
           items={items}
           group="comparison"
