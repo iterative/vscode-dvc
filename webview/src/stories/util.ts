e21712f6bdb28393d5d8baa3f4b5d2834aba181e
Stephanie Roy <stephanie@iterative.ai>
2022-03-25 12:47:10 -0400

    New chromatic test

--- webview/src/stories/util.ts
+++ webview/src/stories/util.ts
@@ -43,8 +43,17 @@ export const viewports = {
   }
 }
 
-export const chromaticViewports = { viewports: [1024, 1280, 1440, 1600] }
+const viewportsWidths = Object.values(viewports)
+  .map(viewport => {
+    const value = Number.parseInt(viewport.styles.width, 10)
+
+    // Chromatic only allows viewports between 320px and 1800px (https://www.chromatic.com/docs/viewports#what-viewports-can-i-choose)
+    return value <= 1800 ? value : null
+  })
+  .filter(Boolean)
+
+export const chromaticViewports = { viewports: viewportsWidths }
 
 export const chromaticeParameters = {
-  chromatic: { viewports: chromaticViewports }
+  chromatic: { viewports: [1600, 1440] }
 }
