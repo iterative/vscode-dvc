f5c38d170889535a4fd3847e77be042c674e2628
Matt Seddon <mattseddon@hotmail.com>
2022-04-06 15:07:52 +1000

    improve eslint glob

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
       }
     },
     {
-      files: ['extension/src/test/suite/**/*', 'extension/src/test/cli/**/*'],
+      files: ['extension/src/test/**/*.test.ts'],
       rules: {
         // These aren't jest tests, but still use `expect`
         'jest/valid-expect': 'off',
