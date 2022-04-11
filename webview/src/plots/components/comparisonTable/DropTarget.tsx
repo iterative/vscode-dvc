42973ec6b11ae25a29c1862f20cb6df288f10f8b
Stephanie Roy <stephanie@iterative.ai>
2022-04-11 11:03:02 -0400

    Cleanup useless ids

--- webview/src/plots/components/comparisonTable/DropTarget.tsx
+++ webview/src/plots/components/comparisonTable/DropTarget.tsx
@@ -3,11 +3,7 @@ import { AllIcons, Icon } from '../../../shared/components/Icon'
 import styles from '../styles.module.scss'
 
 export const DropTarget: React.FC = () => (
-  <div
-    data-testid="plot_drop-target"
-    id="plot-drop-target"
-    className={styles.dropTarget}
-  >
+  <div className={styles.dropTarget}>
     <Icon
       icon={AllIcons.ELLIPSIS}
       className={styles.smallDropIcon}
