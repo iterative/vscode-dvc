5310240a6cfbcaa3821f1c817964acf19de398d3
Matt Seddon <mattseddon@hotmail.com>
2022-04-11 14:17:07 +1000

    get pip freeze output

--- extension/src/test/cli/util.ts
+++ extension/src/test/cli/util.ts
@@ -5,6 +5,7 @@ import { CliReader } from '../../cli/reader'
 import { Config } from '../../config'
 import { exists } from '../../fileSystem'
 import { getVenvBinPath } from '../../python'
+import { createProcessWithOutput } from '../../processExecution'
 
 const config = {
   getCliPath: () => getVenvBinPath(TEMP_DIR, ENV_DIR, 'dvc')
@@ -12,6 +13,12 @@ const config = {
 
 export const cliReader = new CliReader(config)
 export const cliExecutor = new CliExecutor(config)
+
+createProcessWithOutput({
+  args: ['-m', 'pip', 'freeze'],
+  cwd: TEMP_DIR,
+  executable: getVenvBinPath(TEMP_DIR, ENV_DIR, 'python')
+})
 
 export const initializeEmptyDvc = (): Promise<string> => {
   if (exists(join(TEMP_DIR, '.dvc'))) {
