06a499e9cc75ca89fc3d77ee8bbd4371ca4c97e1
rogermparent <rogermparent@gmail.com>
2022-04-12 16:16:46 -0400

    Refactor table components into individual modules

---
+++ webview/src/experiments/components/table/interfaces.ts
@@ -0,0 +1,22 @@
+import { Experiment, TableData } from 'dvc/src/experiments/webview/contract'
+import { Cell, Row, TableInstance } from 'react-table'
+
+export interface InstanceProp {
+  instance: TableInstance<Experiment>
+}
+
+export interface TableProps extends InstanceProp {
+  tableData: TableData
+}
+
+export interface WithChanges {
+  changes?: string[]
+}
+
+export interface RowProp {
+  row: Row<Experiment>
+}
+
+export interface CellProp {
+  cell: Cell<Experiment>
+}
