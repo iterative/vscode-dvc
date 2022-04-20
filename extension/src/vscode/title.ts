0a9a46837a2d0ff4d97952a69cc43de33f0c03fa
Roger <rogermparent@gmail.com>
2022-04-20 14:36:10 -0400

    Unify name of "Modify Experiment Param(s) and Queue" command (#1590)
    * Change "Vary Params" to "Modify"
    
    * Unify all references to "modify and queue" command
    
    * Remove more references to "queueExperimentFromExisting"
    
    * Add "Experiment" for even more consistency

--- extension/src/vscode/title.ts
+++ extension/src/vscode/title.ts
@@ -11,7 +11,7 @@ export enum Title {
   SELECT_OPERATOR = 'Select an Operator',
   SELECT_PARAM_OR_METRIC_FILTER = 'Select a Param or Metric to Filter by',
   SELECT_PARAM_OR_METRIC_SORT = 'Select a Param or Metric to Sort by',
-  SELECT_PARAM_TO_VARY = 'Select Param(s) to Vary',
+  SELECT_PARAM_TO_MODIFY = 'Select Param(s) to Modify',
   SELECT_SORT_DIRECTION = 'Select Sort Direction',
   SELECT_SORTS_TO_REMOVE = 'Select Sort(s) to Remove',
   SETUP_WORKSPACE = 'Setup the Workspace'
