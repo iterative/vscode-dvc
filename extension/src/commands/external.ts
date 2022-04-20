4f9f2ca64e6dcbb410fa0b4c8e67f6024a81b1c3
rogermparent <rogermparent@gmail.com>
2022-04-19 20:29:06 -0400

    Remove more references to "queueExperimentFromExisting"

--- extension/src/commands/external.ts
+++ extension/src/commands/external.ts
@@ -43,7 +43,7 @@ export enum RegisteredCommands {
   EXPERIMENT_SORTS_REMOVE = 'dvc.removeExperimentsTableSorts',
   EXPERIMENT_SORTS_REMOVE_ALL = 'dvc.views.experimentsSortByTree.removeAllSorts',
   EXPERIMENT_TOGGLE = 'dvc.views.experimentsTree.toggleStatus',
-  MODIFY_PARAMS_AND_QUEUE = 'dvc.queueExperimentFromExisting',
+  MODIFY_PARAMS_AND_QUEUE = 'dvc.modifyParamsAndQueue',
   STOP_EXPERIMENT = 'dvc.stopRunningExperiment',
 
   PLOTS_PATH_TOGGLE = 'dvc.views.plotsPathsTree.toggleStatus',
