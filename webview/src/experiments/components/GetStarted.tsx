607fa281d2d2bf851071d2af9c3405d37ea77bbd
Matt Seddon <mattseddon@hotmail.com>
2022-05-25 12:30:42 +1000

    make code more readable

--- webview/src/experiments/components/GetStarted.tsx
+++ webview/src/experiments/components/GetStarted.tsx
@@ -24,7 +24,7 @@ export const GetStarted = ({
                 type: MessageFromWebviewType.SELECT_COLUMNS
               })
             }
-            text={'Add Columns'}
+            text="Add Columns"
           />
         </div>
       )}
@@ -32,14 +32,13 @@ export const GetStarted = ({
         <div>
           <p>No Experiments to Display.</p>
           <p>
-            {'Get started with '}
-            <a href="https://dvc.org/doc/start/experiments">experiments</a>
-            {'.'}
+            Get started with{' '}
+            <a href="https://dvc.org/doc/start/experiments">experiments</a>.
           </p>
           <p>
-            {'Learn about the '}
+            Learn about the{' '}
             <a href="https://dvc.org/doc/command-reference/exp">exp commands</a>
-            {'.'}
+            .
           </p>
         </div>
       )}
