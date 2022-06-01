7537d38eff1c4c5b50512d78793b3484b3e539b5
Matt Seddon <mattseddon@hotmail.com>
2022-06-01 15:52:36 +1000

    extend unit tests

--- extension/src/repository/model/collect.test.ts
+++ extension/src/repository/model/collect.test.ts
@@ -193,14 +193,43 @@ describe('collectTree', () => {
 })
 
 describe('collectSelected', () => {
-  it('should exclude all children from the final list', () => {
-    const logsPathItem = {
-      dvcRoot: dvcDemoPath,
-      isDirectory: true,
-      isTracked: true,
-      resourceUri: makeUri('logs')
-    }
+  const logsPathItem = {
+    dvcRoot: dvcDemoPath,
+    isDirectory: true,
+    isTracked: true,
+    resourceUri: makeUri('logs')
+  }
 
+  it('should return an empty object if no path items are provided', () => {
+    expect(collectSelected([])).toStrictEqual({})
+  })
+
+  it('should return the original item if only one is provided', () => {
+    const selected = collectSelected([logsPathItem])
+
+    expect(selected).toStrictEqual({
+      [dvcDemoPath]: [logsPathItem]
+    })
+  })
+
+  it('should return a root given it is select', () => {
+    const selected = collectSelected([
+      dvcDemoPath,
+      logsPathItem,
+      {
+        dvcRoot: dvcDemoPath,
+        isDirectory: false,
+        isTracked: true,
+        resourceUri: makeUri('logs', 'acc.tsv')
+      }
+    ])
+
+    expect(selected).toStrictEqual({
+      [dvcDemoPath]: [dvcDemoPath]
+    })
+  })
+
+  it('should exclude all children from the final list', () => {
     const selected = collectSelected([
       {
         dvcRoot: dvcDemoPath,
@@ -216,7 +245,33 @@ describe('collectSelected', () => {
       },
       logsPathItem
     ])
+
     expect(selected).toStrictEqual({
+      [dvcDemoPath]: [logsPathItem]
+    })
+  })
+
+  it('should return multiple entries when multiple roots are provided', () => {
+    const mockOtherRepoItem = {
+      dvcRoot: __dirname,
+      isDirectory: true,
+      isTracked: true,
+      resourceUri: Uri.file(join(__dirname, 'mock', 'path'))
+    }
+
+    const selected = collectSelected([
+      mockOtherRepoItem,
+      {
+        dvcRoot: dvcDemoPath,
+        isDirectory: false,
+        isTracked: true,
+        resourceUri: makeUri('logs', 'acc.tsv')
+      },
+      logsPathItem
+    ])
+
+    expect(selected).toStrictEqual({
+      [__dirname]: [mockOtherRepoItem],
       [dvcDemoPath]: [logsPathItem]
     })
   })
