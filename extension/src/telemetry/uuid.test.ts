da490a3e50b6020bef2659e1945fb0a8fd959b3d
Matt Seddon <mattseddon@hotmail.com>
2022-06-07 12:19:37 +1000

    update creation of uuid and config migration

--- extension/src/telemetry/uuid.test.ts
+++ extension/src/telemetry/uuid.test.ts
@@ -14,11 +14,23 @@ beforeEach(() => {
 })
 
 const mockedUserId = v4()
-const mockedConfig = { user_id: mockedUserId }
+const mockedConfig = { 'do-not-track': true, user_id: mockedUserId }
 
 describe('readOrCreateUserId', () => {
-  it('should try to migrate the legacy DVC config if the new config cannot be found', () => {
-    mockedExists.mockReturnValueOnce(false).mockReturnValueOnce(true)
+  it('should create two completely new configs if neither config can be found', () => {
+    mockedExists.mockReturnValueOnce(false).mockReturnValueOnce(false)
+
+    readOrCreateUserId()
+    expect(mockedReadJson).not.toBeCalled()
+    expect(mockedWriteJson).toBeCalledTimes(2)
+    expect(mockedWriteJson).toBeCalledWith(
+      expect.stringContaining(sep + 'telemetry'),
+      expect.objectContaining({ user_id: expect.stringContaining('-') })
+    )
+  })
+
+  it('should migrate the legacy DVC config if the new config cannot be found', () => {
+    mockedExists.mockReturnValueOnce(true).mockReturnValueOnce(false)
     mockedReadJson.mockReturnValueOnce(mockedConfig)
 
     readOrCreateUserId()
@@ -27,17 +39,48 @@ describe('readOrCreateUserId', () => {
       expect.stringContaining(sep + 'telemetry'),
       mockedConfig
     )
+    expect(mockedWriteJson).not.toBeCalledWith(
+      expect.stringContaining('dvc'),
+      mockedConfig
+    )
   })
 
-  it('should create a completely new config if neither config can be found', () => {
-    mockedExists.mockReturnValueOnce(false).mockReturnValueOnce(false)
+  it('should create a legacy config if only the new one can be found', () => {
+    mockedExists.mockReturnValueOnce(false).mockReturnValueOnce(true)
+    mockedReadJson.mockReturnValueOnce(mockedConfig)
 
     readOrCreateUserId()
-    expect(mockedReadJson).not.toBeCalled()
+    expect(mockedReadJson).toBeCalledTimes(1)
     expect(mockedWriteJson).toBeCalledTimes(1)
+
+    expect(mockedWriteJson).not.toBeCalledWith(
+      expect.stringContaining(sep + 'telemetry'),
+      mockedConfig
+    )
+
+    expect(mockedWriteJson).toBeCalledWith(
+      expect.stringContaining('dvc'),
+      expect.objectContaining({ user_id: expect.stringContaining('-') })
+    )
+  })
+
+  it('should overwrite the user_id in the new config if it differs from the legacy id', () => {
+    mockedExists.mockReturnValueOnce(true).mockReturnValueOnce(true)
+    mockedReadJson
+      .mockReturnValueOnce(mockedConfig)
+      .mockReturnValueOnce({ 'some-other-info': true, user_id: 'bogus-id' })
+
+    readOrCreateUserId()
+    expect(mockedReadJson).toBeCalledTimes(2)
+    expect(mockedWriteJson).toBeCalledTimes(1)
+
     expect(mockedWriteJson).toBeCalledWith(
       expect.stringContaining(sep + 'telemetry'),
-      expect.objectContaining({ user_id: expect.stringContaining('-') })
+      {
+        'do-not-track': true,
+        'some-other-info': true,
+        user_id: expect.not.stringMatching('bogus-id')
+      }
     )
   })
 })
@@ -50,7 +93,7 @@ describe('getUserId', () => {
     const user_id = getUserId()
 
     expect(user_id).toStrictEqual(mockedUserId)
-    expect(mockedExists).toBeCalledTimes(1)
+    expect(mockedExists).toBeCalledTimes(2)
     expect(mockedReadJson).toBeCalledTimes(1)
 
     mockedExists.mockClear()
--- extension/src/telemetry/uuid.ts
+++ extension/src/telemetry/uuid.ts
@@ -1,20 +1,52 @@
 import { join } from 'path'
+import isEmpty from 'lodash.isempty'
 import { v4 } from 'uuid'
 import { getProcessPlatform } from '../env'
 import { exists, loadJson, writeJson } from '../fileSystem'
 
 type UserConfig = {
-  user_id: string
+  user_id?: string
 }
 
-const readOrCreateConfig = (): UserConfig | undefined => {
-  const { userConfigDir } = require('appdirs') as {
-    userConfigDir: (appName: string) => string
+const loadConfig = (configPath: string): UserConfig => {
+  if (!exists(configPath)) {
+    return {}
   }
 
-  const configPath = userConfigDir(join('iterative', 'telemetry'))
-  if (exists(configPath)) {
-    return loadJson<UserConfig>(configPath)
+  return loadJson<UserConfig>(configPath) || {}
+}
+
+const writeMissingConfigs = (
+  user_id: string,
+  legacyConfig: UserConfig,
+  legacyConfigPath: string,
+  config: UserConfig,
+  configPath: string
+) => {
+  if (isEmpty(legacyConfig) && isEmpty(config)) {
+    writeJson(legacyConfigPath, { user_id })
+    writeJson(configPath, { user_id })
+    return
+  }
+
+  if (isEmpty(config)) {
+    writeJson(configPath, { ...legacyConfig, user_id })
+    return
+  }
+
+  if (config.user_id !== user_id) {
+    writeJson(configPath, { ...config, ...legacyConfig, user_id })
+    return
+  }
+
+  if (isEmpty(legacyConfig)) {
+    writeJson(legacyConfigPath, { ...config, user_id })
+  }
+}
+
+const readOrCreateConfig = (): string | undefined => {
+  const { userConfigDir } = require('appdirs') as {
+    userConfigDir: (appName: string) => string
   }
 
   const legacyDirectory =
@@ -23,22 +55,28 @@ const readOrCreateConfig = (): UserConfig | undefined => {
       : join('dvc', 'user_id')
 
   const legacyConfigPath = userConfigDir(legacyDirectory)
-  if (exists(legacyConfigPath)) {
-    const oldConfig = loadJson<UserConfig>(legacyConfigPath)
-    if (oldConfig) {
-      writeJson(configPath, oldConfig)
-      return oldConfig
-    }
+  const legacyConfig = loadConfig(legacyConfigPath)
+
+  const configPath = userConfigDir(join('iterative', 'telemetry'))
+  const config = loadConfig(configPath)
+
+  const user_id = legacyConfig.user_id || config.user_id || v4()
+
+  if (legacyConfig.user_id !== user_id || config.user_id !== user_id) {
+    writeMissingConfigs(
+      user_id,
+      legacyConfig,
+      legacyConfigPath,
+      config,
+      configPath
+    )
   }
 
-  const newConfig = { user_id: v4() }
-  writeJson(configPath, newConfig)
-  return newConfig
+  return user_id
 }
 
 export const readOrCreateUserId = () => {
-  const config = readOrCreateConfig()
-  return config?.user_id || 'unknown'
+  return readOrCreateConfig() || 'unknown'
 }
 
 let user_id: string
