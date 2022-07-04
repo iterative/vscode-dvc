aa3942fb167891bfd2302ef88e7e0afe4d40db42
Matt Seddon <mattseddon@hotmail.com>
2022-07-05 06:07:06 +1000

    Use create root to instantiate React apps

--- webview/src/experiments/index.tsx
+++ webview/src/experiments/index.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import '../shared/style.scss'
 import { App } from './components/App'
 
-const elem = document.createElement('div')
-elem.className = 'react-root'
-document.body.append(elem)
-ReactDOM.render(<App />, elem)
+const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
+root.render(<App />)
