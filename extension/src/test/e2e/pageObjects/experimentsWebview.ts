6010bf0a70b9f08cd1c35d7934b5e865202bed05
Matt Seddon <mattseddon@hotmail.com>
2022-07-08 19:45:10 +1000

    check on contract buttons

--- extension/src/test/e2e/pageObjects/experimentsWebview.ts
+++ extension/src/test/e2e/pageObjects/experimentsWebview.ts
@@ -12,6 +12,12 @@ export class ExperimentsWebview extends BaseWebview {
     const expandRowButtons = await this.expandRowButton$$
     // eslint-disable-next-line no-console
     console.error('expand buttons', JSON.stringify(expandRowButtons.length))
+    // eslint-disable-next-line no-console
+    console.error(
+      'contract buttons',
+      // eslint-disable-next-line unicorn/no-await-expression-member
+      JSON.stringify((await this.contractRowButton$$).length)
+    )
     for (const button of expandRowButtons) {
       await button.click()
     }
