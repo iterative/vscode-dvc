436a1ab8f78ee2562073ede94c3b745bbd9ffb22
Roger <rogermparent@gmail.com>
2022-07-12 08:43:47 -0400

    Fix non-standard data type tooltips and add tests (#1991)
    * Fix nonstandard type tooltips and refactor
    
    * Refactor stopPropagation event on tooltips
    
    * Add generic tooltip test builder and use it for multiple data types
    
    * Use more static values in tests
    
    * Add data types fixture
    
    * Add failing tests using new fixture
    
    * Rename columns and fix tooltip tests
    
    * Fix grammar of rows tests
    
    * Make text of columns tests similar to rows tests
    
    * Move expectTooltipValue in only function it's used, remove module-wide exception
    
    * fix typo
    
    * Remove unnecessary abstraction
    
    * Add interactive cell tooltip test
    
    * Change test wording
    
    * Use @storybook/testing-library for Storybook play functions
    
    This should make writing play functions easier, since we can use the
    full testing-library query suite
    
    * Stop using screen
    
    * Add .play to show bool tooltip on data types Storybook
    
    * Increase chromatic delay for multiview zoomed plot
    
    * Further increase chromatic delay
    
    * Remove chromatic delay on MultiView Zoomed plot
    
    * Remove chromatic delays
    
    * Await multiview plot

---
+++ extension/src/test/fixtures/expShow/dataTypes.ts
@@ -0,0 +1,193 @@
+import { ExperimentsOutput } from '../../../cli/reader'
+import {
+  Column,
+  ColumnType,
+  Row,
+  TableData
+} from '../../../experiments/webview/contract'
+
+export const dataTypesOutput: ExperimentsOutput = {
+  workspace: {
+    baseline: {
+      data: {
+        timestamp: null,
+        params: {
+          'params.yaml': {
+            data: {
+              bool1: true,
+              bool2: false,
+              zero: 0,
+              negative: -123,
+              float: 1.9293040037155151,
+              string: 'string',
+              emptyString: '',
+              array: [true, false, 'string', 2]
+            }
+          }
+        },
+        queued: false,
+        running: false,
+        executor: null
+      }
+    }
+  },
+  '53c3851f46955fa3e2b8f6e1c52999acc8c9ea77': {
+    baseline: {
+      data: {
+        timestamp: '2020-11-21T19:58:22',
+        queued: false,
+        running: false,
+        executor: null,
+        name: 'main'
+      }
+    }
+  }
+}
+
+export const columns: Column[] = [
+  {
+    hasChildren: true,
+    label: 'params.yaml',
+    parentPath: ColumnType.PARAMS,
+    path: 'params:params.yaml',
+    type: ColumnType.PARAMS
+  },
+  {
+    hasChildren: false,
+    label: 'bool1',
+    maxStringLength: 4,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:bool1',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'bool1'],
+    type: ColumnType.PARAMS,
+    types: ['boolean']
+  },
+  {
+    hasChildren: false,
+    label: 'bool2',
+    maxStringLength: 5,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:bool2',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'bool2'],
+    type: ColumnType.PARAMS,
+    types: ['boolean']
+  },
+  {
+    hasChildren: false,
+    label: 'zero',
+    maxNumber: 0,
+    maxStringLength: 1,
+    minNumber: 0,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:zero',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'zero'],
+    type: ColumnType.PARAMS,
+    types: ['number']
+  },
+  {
+    hasChildren: false,
+    label: 'negative',
+    maxNumber: -123,
+    maxStringLength: 4,
+    minNumber: -123,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:negative',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'negative'],
+    type: ColumnType.PARAMS,
+    types: ['number']
+  },
+  {
+    hasChildren: false,
+    label: 'float',
+    maxNumber: 1.9293040037155151,
+    maxStringLength: 18,
+    minNumber: 1.9293040037155151,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:float',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'float'],
+    type: ColumnType.PARAMS,
+    types: ['number']
+  },
+  {
+    hasChildren: false,
+    label: 'string',
+    maxStringLength: 6,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:string',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'string'],
+    type: ColumnType.PARAMS,
+    types: ['string']
+  },
+  {
+    hasChildren: false,
+    label: 'emptyString',
+    maxStringLength: 0,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:emptyString',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'emptyString'],
+    type: ColumnType.PARAMS,
+    types: ['string']
+  },
+  {
+    hasChildren: false,
+    label: 'array',
+    maxStringLength: 19,
+    parentPath: 'params:params.yaml',
+    path: 'params:params.yaml:array',
+    pathArray: [ColumnType.PARAMS, 'params.yaml', 'array'],
+    type: ColumnType.PARAMS,
+    types: ['array']
+  }
+]
+export const rows: Row[] = [
+  {
+    displayColor: '#945dd6',
+    executor: null,
+    id: 'workspace',
+    label: 'workspace',
+    params: {
+      'params.yaml': {
+        array: [true, false, 'string', 2],
+        emptyString: '',
+        bool2: false,
+        float: 1.9293040037155151,
+        negative: -123,
+        string: 'string',
+        bool1: true,
+        zero: 0
+      }
+    },
+    queued: false,
+    running: false,
+    selected: true,
+    timestamp: null
+  },
+  {
+    displayColor: '#13adc7',
+    executor: null,
+    id: 'main',
+    label: 'main',
+    name: 'main',
+    queued: false,
+    running: false,
+    selected: true,
+    sha: '53c3851f46955fa3e2b8f6e1c52999acc8c9ea77',
+    timestamp: '2020-11-21T19:58:22'
+  }
+]
+
+export const dataTypesTableData: TableData = {
+  changes: [],
+  columnOrder: [],
+  columnWidths: {},
+  filteredCounts: { experiments: 0, checkpoints: 0 },
+  filters: [],
+  hasCheckpoints: false,
+  hasRunningExperiment: false,
+  sorts: [],
+  columns,
+  hasColumns: true,
+  rows
+}
+
+export default dataTypesTableData
