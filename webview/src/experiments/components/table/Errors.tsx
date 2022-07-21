1d90d6f93c93a50cf32cfb1ee0889219898a2a12
Matt Seddon <mattseddon@hotmail.com>
2022-07-22 08:11:23 +1000

    Standardise how the webviews access icons

--- webview/src/experiments/components/table/Errors.tsx
+++ webview/src/experiments/components/table/Errors.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import ErrorIcon from '../../../shared/components/icons/Error'
+import { Error } from '../../../shared/components/icons'
 import Tooltip from '../../../shared/components/tooltip/Tooltip'
 
 export const ErrorTooltip: React.FC<{
@@ -10,7 +10,7 @@ export const ErrorTooltip: React.FC<{
   <Tooltip
     content={
       <div className={styles.errorTooltip}>
-        <ErrorIcon className={styles.errorIcon} />
+        <Error className={styles.errorIcon} />
         {error}
       </div>
     }
