16bb5259c71ceea877c556898a0956314b1722db
Stephanie Roy <stephanie@iterative.ai>
2022-07-21 12:00:11 -0400

    Try to fix windows tests

--- webview/src/test/nodes.ts
+++ webview/src/test/nodes.ts
@@ -1,3 +1,3 @@
 export const idToNodeNode = (id: string) =>
-  // CSS.escape is needed for weird ids (ex.: :loss, plot/1...)
-  (id && document.querySelector(`#${CSS.escape(id)}`)) || null
+  // eslint-disable-next-line unicorn/prefer-query-selector
+  (id && document.getElementById(id)) || null
