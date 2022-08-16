ba5b8b54387644f3ea4192659fdf346725a4f613
Julie <43496356+julieg18@users.noreply.github.com>
2022-08-16 07:22:02 -0500

    Update table styles (#2133)
    * add cell hover style
    * add row hover styles
    * add a bottom border to every row
    * remove even row bg color
    * keep header bg color the same as body bg
    * use the same line style for header item separators
    * keep row arrow the same color as left icons
    * update workspace row, making it sticky and using default row styles

--- webview/src/experiments/components/table/TableBody.tsx
+++ webview/src/experiments/components/table/TableBody.tsx
@@ -1,33 +1,61 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import cx from 'classnames'
+import { useInView } from 'react-intersection-observer'
 import styles from './styles.module.scss'
 import { BatchSelectionProp, RowContent } from './Row'
 import { InstanceProp, RowProp } from './interfaces'
 import { ExperimentGroup } from './ExperimentGroup'
 
+const WorkspaceRowGroupWrapper: React.FC<
+  {
+    children: React.ReactNode
+    root: HTMLElement | null
+    tableHeaderHeight: number
+  } & InstanceProp
+> = ({ children, instance, root, tableHeaderHeight }) => {
+  const [ref, needsShadow] = useInView({
+    root,
+    rootMargin: `-${tableHeaderHeight + 15}px 0px 0px 0px`,
+    threshold: 1
+  })
+
+  return (
+    <div
+      style={
+        { '--table-head-height': `${tableHeaderHeight}px` } as CSSProperties
+      }
+      ref={ref}
+      {...instance.getTableBodyProps({
+        className: cx(
+          styles.rowGroup,
+          styles.tbody,
+          styles.workspaceRowGroup,
+          needsShadow && styles.withShadow
+        )
+      })}
+    >
+      {children}
+    </div>
+  )
+}
 export const TableBody: React.FC<
-  RowProp & InstanceProp & BatchSelectionProp
+  RowProp &
+    InstanceProp &
+    BatchSelectionProp & { root: HTMLElement | null; tableHeaderHeight: number }
 > = ({
   row,
   instance,
   contextMenuDisabled,
   projectHasCheckpoints,
   hasRunningExperiment,
-  batchRowSelection
+  batchRowSelection,
+  root,
+  tableHeaderHeight
 }) => {
   instance.prepareRow(row)
-  return (
-    <div
-      {...instance.getTableBodyProps({
-        className: cx(
-          styles.rowGroup,
-          styles.tbody,
-          row.values.id === 'workspace'
-            ? styles.workspaceRowGroup
-            : styles.normalRowGroup
-        )
-      })}
-    >
+
+  const content = (
+    <>
       <RowContent
         row={row}
         projectHasCheckpoints={projectHasCheckpoints}
@@ -47,6 +75,23 @@ export const TableBody: React.FC<
             batchRowSelection={batchRowSelection}
           />
         ))}
+    </>
+  )
+  return row.values.id === 'workspace' ? (
+    <WorkspaceRowGroupWrapper
+      tableHeaderHeight={tableHeaderHeight}
+      root={root}
+      instance={instance}
+    >
+      {content}
+    </WorkspaceRowGroupWrapper>
+  ) : (
+    <div
+      {...instance.getTableBodyProps({
+        className: cx(styles.rowGroup, styles.tbody, styles.normalRowGroup)
+      })}
+    >
+      {content}
     </div>
   )
 }
