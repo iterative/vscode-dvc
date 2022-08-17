ffe81ce4a814f5914f20b343d05de5655f76d7ed
mattseddon <37993418+mattseddon@users.noreply.github.com>
2022-08-18 08:39:34 +1000

    Do not remove watcher from previously collected metric file (exp show live updates) (#2203)

--- extension/src/experiments/data/collect.ts
+++ extension/src/experiments/data/collect.ts
@@ -1,12 +1,16 @@
 import { ExperimentsOutput } from '../../cli/reader'
 
-export const collectFiles = (data: ExperimentsOutput): string[] => {
-  const files = new Set<string>(
-    Object.keys({
-      ...data?.workspace?.baseline?.data?.params,
-      ...data?.workspace?.baseline?.data?.metrics
-    }).filter(Boolean)
-  )
+export const collectFiles = (
+  data: ExperimentsOutput,
+  existingFiles: string[]
+): string[] => {
+  const files = new Set<string>([
+    ...Object.keys({
+      ...data?.workspace.baseline?.data?.params,
+      ...data?.workspace.baseline?.data?.metrics
+    }).filter(Boolean),
+    ...existingFiles
+  ])
 
   return [...files]
 }
