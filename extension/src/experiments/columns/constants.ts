5826109f57df88a96cd946f391572e2791dd14e5
julieg18 <juliannagal18@gmail.com>
2022-08-26 10:22:46 -0500

    Make column parentPath optional

--- extension/src/experiments/columns/constants.ts
+++ extension/src/experiments/columns/constants.ts
@@ -3,7 +3,6 @@ import { ColumnType } from '../webview/contract'
 export const timestampColumn = {
   hasChildren: false,
   label: 'Timestamp',
-  parentPath: ColumnType.TIMESTAMP,
   path: 'Timestamp',
   type: ColumnType.TIMESTAMP
 }
