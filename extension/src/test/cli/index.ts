435a74c5a3c1f789b6ce70ec729a5a30d7fc0450
Matt Seddon <mattseddon@hotmail.com>
2022-09-01 12:30:11 +1000

    test 2.23.0

--- extension/src/test/cli/index.ts
+++ extension/src/test/cli/index.ts
@@ -19,11 +19,7 @@ async function main() {
       'ts',
       async () => {
         await mkdirp(TEMP_DIR)
-        await setupVenv(
-          TEMP_DIR,
-          ENV_DIR,
-          'git+https://github.com/iterative/dvc#egg=dvc[s3]'
-        )
+        await setupVenv(TEMP_DIR, ENV_DIR, 'dvc[s3]')
       },
       () => {
         removeDir(TEMP_DIR)
