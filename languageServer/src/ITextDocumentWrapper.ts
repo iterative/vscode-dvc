040c3a67ec04145a569be0d21a7190eac434ba85
Wolmir Nemitz <wolmir.nemitz@gmail.com>
2022-09-12 10:45:20 -0300

    Rename IDvcTextDocument->ITextDocumentWrapper

--- languageServer/src/ITextDocumentWrapper.ts
+++ languageServer/src/ITextDocumentWrapper.ts
@@ -1,7 +1,7 @@
 import { Position } from 'vscode-languageserver-textdocument'
 import { Document } from 'yaml'
 
-export interface IDvcTextDocument {
+export interface ITextDocumentWrapper {
   getYamlDocument(): Document
   offsetAt(position: Position): number
 }
