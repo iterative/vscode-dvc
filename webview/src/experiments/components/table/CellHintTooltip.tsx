53c34d26dec4c5e76c98966573ac7ed973e5b35d
julieg18 <juliannagal18@gmail.com>
2022-09-16 11:03:30 -0500

    Update table hints * Have table indicators hints to have same styles as other cell hints * make tooltipContent optional in Indicator and JustCounter components

--- webview/src/experiments/components/table/CellHintTooltip.tsx
+++ webview/src/experiments/components/table/CellHintTooltip.tsx
@@ -1,19 +1,26 @@
 import React, { ReactNode, ReactElement } from 'react'
+import { TippyProps } from '@tippyjs/react'
 import styles from './styles.module.scss'
 import Tooltip, {
   CELL_TOOLTIP_DELAY
 } from '../../../shared/components/tooltip/Tooltip'
 
-export const CellHintTooltip: React.FC<{
+export type CellHintTooltipProps = {
   tooltipContent: ReactNode
   children: ReactElement
-}> = ({ tooltipContent, children }) => {
+}
+
+export const CellHintTooltip: React.FC<CellHintTooltipProps & TippyProps> = ({
+  tooltipContent,
+  children,
+  delay = [CELL_TOOLTIP_DELAY, 0]
+}) => {
   return (
     <Tooltip
       content={<span className={styles.cellHintTooltip}>{tooltipContent}</span>}
       placement="bottom-start"
       offset={[0, -2]}
-      delay={[CELL_TOOLTIP_DELAY, 0]}
+      delay={delay}
     >
       {children}
     </Tooltip>
