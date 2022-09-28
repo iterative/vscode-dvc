f71cc0f9a3ca5761fda534a748a174a471dfedd1
Matt Seddon <mattseddon@hotmail.com>
2022-09-29 08:17:49 +1000

    remove extra timeout

--- extension/src/test/e2e/pageObjects/baseWebview.ts
+++ extension/src/test/e2e/pageObjects/baseWebview.ts
@@ -23,7 +23,7 @@ export class BaseWebview extends BasePage<
     await this.outerFrame$.waitForDisplayed()
 
     await browser.switchToFrame(webviewContainer)
-    await this.innerFrame$.waitForDisplayed({ timeout: 20000 })
+    await this.innerFrame$.waitForDisplayed()
     const webviewInner = await browser.findElement(
       'css selector',
       this.locators.innerFrame
