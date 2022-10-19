4a7d62e7c2dceb76df80387c0aacaa9538954003
Matt Seddon <mattseddon@hotmail.com>
2022-10-19 12:11:50 +1100

    add further tests

--- extension/src/test/suite/fileSystem/index.test.ts
+++ extension/src/test/suite/fileSystem/index.test.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { join, resolve } from 'path'
 import { afterEach, beforeEach, describe, it, suite } from 'mocha'
 import { expect } from 'chai'
 import { Disposable } from '@hediet/std/disposable'
@@ -8,6 +8,7 @@ import { getGitPath } from '../../../fileSystem'
 import { gitPath } from '../../../cli/git/constants'
 import { GitReader } from '../../../cli/git/reader'
 import { standardizePath } from '../../../fileSystem/path'
+import { EXPERIMENTS_GIT_REFS } from '../../../experiments/data/constants'
 
 suite('File System Watcher Test Suite', () => {
   const disposable = Disposable.fn()
@@ -21,26 +22,32 @@ suite('File System Watcher Test Suite', () => {
   })
 
   describe('getGitPath', () => {
-    it('should get the expected path for the demo repository (submodule)', async () => {
+    it('should get the expected paths for the demo repository (submodule)', async () => {
       const reader = disposable.track(new GitReader())
       const root = await reader.getGitRepositoryRoot(__dirname)
       const submoduleDotGit = standardizePath(
         resolve(root, gitPath.DOT_GIT, 'modules', 'demo')
-      )
+      ) as string
 
       const dotGitPath = getGitPath(dvcDemoPath, gitPath.DOT_GIT)
-
       expect(dotGitPath).to.equal(submoduleDotGit)
+
+      const expRefPaths = getGitPath(dvcDemoPath, EXPERIMENTS_GIT_REFS)
+      expect(expRefPaths).to.equal(join(submoduleDotGit, EXPERIMENTS_GIT_REFS))
     })
 
     it('should get the expected paths for this project', async () => {
       const reader = disposable.track(new GitReader())
       const root = await reader.getGitRepositoryRoot(__dirname)
-      const rootDotGit = standardizePath(resolve(root, gitPath.DOT_GIT))
+      const rootDotGit = standardizePath(
+        resolve(root, gitPath.DOT_GIT)
+      ) as string
 
       const dotGitPath = getGitPath(root, gitPath.DOT_GIT)
-
       expect(dotGitPath).to.equal(rootDotGit)
+
+      const expRefPaths = getGitPath(rootDotGit, EXPERIMENTS_GIT_REFS)
+      expect(expRefPaths).to.equal(join(rootDotGit, EXPERIMENTS_GIT_REFS))
     })
   })
 })
