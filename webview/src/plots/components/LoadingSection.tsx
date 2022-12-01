ac39fd8052667eea1b8b36b85ef085765826445c
Matt Seddon <mattseddon@hotmail.com>
2022-12-01 21:05:49 +1100

    update rule to account for multiple revisions being loaded

--- webview/src/plots/components/LoadingSection.tsx
+++ webview/src/plots/components/LoadingSection.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { EmptyState } from '../../shared/components/emptyState/EmptyState'
 
 export const sectionIsLoading = (selectedRevisions: Revision[]): boolean =>
-  selectedRevisions.length === 1 && selectedRevisions[0].fetched === false
+  selectedRevisions.length > 0 &&
+  !selectedRevisions.some(({ fetched }) => fetched)
 
 export const LoadingSection: React.FC = () => (
   <EmptyState isFullScreen={false}>Loading...</EmptyState>
