a1486c7717cbf5a002675d7453ff187737b911d3
Julie <43496356+julieg18@users.noreply.github.com>
2022-12-20 09:52:05 -0600

    Refactor `getDataFromColumnPath` logic  (#2971)
    * simplify code and reduce duplication

--- webview/src/util/number.ts
+++ webview/src/util/number.ts
@@ -1,6 +1,5 @@
-const defaultPrecision = 5 // for when we can't calculate real precision yet
-
 export const formatFloat = (value: number): string => {
+  const defaultPrecision = 5 // for when we can't calculate real precision yet
   const automatic = value.toString()
   if (automatic.length > 7) {
     return value.toPrecision(defaultPrecision)
