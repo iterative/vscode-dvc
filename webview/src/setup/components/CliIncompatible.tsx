9e80196352fb5233239b84e46a788e28c798d71e
Matt Seddon <mattseddon@hotmail.com>
2022-12-29 09:30:37 +1100

    add check compatible button to setup webview

--- webview/src/setup/components/CliIncompatible.tsx
+++ webview/src/setup/components/CliIncompatible.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { MIN_CLI_VERSION } from 'dvc/src/cli/dvc/contract'
 import { EmptyState } from '../../shared/components/emptyState/EmptyState'
+import { Button } from '../../shared/components/button/Button'
 
-export const CliIncompatible: React.FC = () => (
+type CliIncompatibleProps = { checkCompatibility: () => void }
+
+export const CliIncompatible: React.FC<CliIncompatibleProps> = ({
+  checkCompatibility
+}) => (
   <EmptyState>
     <div>
       <h1>DVC is incompatible</h1>
-      <p>The located CLI is incompatible with the extension</p>
-      <p>The minimum version is {MIN_CLI_VERSION}</p>
-      <p>Please update your install and try again</p>
+      <p>The located CLI is incompatible with the extension.</p>
+      <p>The minimum version is {MIN_CLI_VERSION}.</p>
+      <p>Please update your install and try again.</p>
+      <Button text="Check Compatibility" onClick={checkCompatibility} />
     </div>
   </EmptyState>
 )
