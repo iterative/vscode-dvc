0957d9cd6a9c73d22801544a6e9b5f2c3bb598e4
Matt Seddon <mattseddon@hotmail.com>
2023-01-31 09:21:34 +1100

    Remove unnecessay interface from LSP

--- languageServer/src/ITextDocumentWrapper.ts
+++
@@ -1,7 +0,0 @@
-import { Position } from 'vscode-languageserver-textdocument'
-import { Document } from 'yaml'
-
-export interface ITextDocumentWrapper {
-  getYamlDocument(): Document
-  offsetAt(position: Position): number
-}
