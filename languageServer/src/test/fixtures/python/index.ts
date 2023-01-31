43493b040ea1e9f370946cde7bbb1091b5c9ff82
Matt Seddon <mattseddon@hotmail.com>
2023-01-31 15:40:05 +1100

    fix test data

--- languageServer/src/test/fixtures/python/index.ts
+++ languageServer/src/test/fixtures/python/index.ts
@@ -1,7 +1,7 @@
 export const train = `from secret_notebook import model
 
 def main():
-"""Don't tell the boss that the model in production is actually a notebook on my laptop."""
+    """Don't tell the boss that the model in production is actually a notebook on my laptop."""
     model.train()
 
 if __name__ == "__main__":
