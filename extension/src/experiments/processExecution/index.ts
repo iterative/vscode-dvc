a1e642149253d817af46e6e333218e25f74e64b0
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-02-10 12:13:33 +1100

    Add ability to stop non-queue experiments from the table's context menu (#3249)
    * add prototype for stopping all experiments from the table's context menu
    
    * add webview integration test for passing stop message
    
    * update extension integration test
    
    * refactor
    
    * rename processes folder to processExecution
    
    * do not show divider when stop is in workspace context menu

---
+++ extension/src/experiments/processExecution/index.ts
@@ -0,0 +1,23 @@
+import { join } from 'path'
+import { EXP_RWLOCK_FILE } from '../../cli/dvc/constants'
+import { getPidFromFile } from '../../fileSystem'
+import { Toast } from '../../vscode/toast'
+import { processExists, stopProcesses } from '../../processExecution'
+
+export const stopWorkspaceExperiment = async (dvcRoot: string) => {
+  const pid = await getPidFromFile(join(dvcRoot, EXP_RWLOCK_FILE))
+  if (!pid) {
+    return
+  }
+  if (!(await processExists(pid))) {
+    return
+  }
+
+  void Toast.showOutput(
+    stopProcesses([pid]).then(stopped =>
+      stopped
+        ? 'The experiment running in the workspace was stopped.'
+        : 'Failed to stop the experiment running in the workspace.'
+    )
+  )
+}
