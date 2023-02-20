2f9288d85fe5c48d0ef1b2cc9fb1ac909901716d
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-02-21 06:45:58 +1100

    Disable unnecessary chromatic snapshots (#3310)
    * disable initial list of snapshots
    
    * remove more snapshots

--- webview/src/stories/Tooltip.stories.tsx
+++ webview/src/stories/Tooltip.stories.tsx
@@ -3,6 +3,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 
 import './test-vscode-styles.scss'
 import '../shared/style.scss'
+import { DISABLE_CHROMATIC_SNAPSHOTS } from './util'
 import Tooltip from '../shared/components/tooltip/Tooltip'
 import { SelectMenu } from '../shared/components/selectMenu/SelectMenu'
 
@@ -17,6 +18,7 @@ export default {
     }
   },
   component: Tooltip,
+  parameters: DISABLE_CHROMATIC_SNAPSHOTS,
   title: 'Tooltip'
 } as Meta
 
