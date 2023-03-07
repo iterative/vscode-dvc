1b4a76a3bdc687fe7b3bf0b5457fdfc1c42af456
Stephanie Roy <stephanie@iterative.ai>
2023-03-07 14:04:38 -0500

    Self review

--- webview/src/plots/components/util.ts
+++ webview/src/plots/components/util.ts
@@ -1,4 +1,4 @@
 export const shouldUseVirtualizedGrid = (
   nbItems: number,
   nbItemsPerRow: number
-) => nbItems > nbItemsPerRow * 10 - (nbItemsPerRow - 1) * 5
+) => nbItems > (nbItemsPerRow + 1) * 5
