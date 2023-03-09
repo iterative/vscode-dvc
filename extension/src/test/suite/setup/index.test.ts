22fe748a3abf7a32778df2eaa3629bf94523b16d
Matt Seddon <mattseddon@hotmail.com>
2023-03-10 10:42:35 +1100

    remove .only

--- extension/src/test/suite/setup/index.test.ts
+++ extension/src/test/suite/setup/index.test.ts
@@ -36,7 +36,7 @@ import { MIN_CLI_VERSION } from '../../../cli/dvc/contract'
 import { run } from '../../../setup/runner'
 import * as Python from '../../../extensions/python'
 
-suite.only('Setup Test Suite', () => {
+suite('Setup Test Suite', () => {
   const disposable = Disposable.fn()
 
   beforeEach(() => {
