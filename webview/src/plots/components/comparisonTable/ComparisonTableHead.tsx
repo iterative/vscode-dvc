a1673bf7acbb11d30f63013875d1dedfd40e915b
Stephanie Roy <stephanie@iterative.ai>
2023-03-17 11:19:12 -0400

    Work around sticky limitations with overflows

--- webview/src/plots/components/comparisonTable/ComparisonTableHead.tsx
+++ webview/src/plots/components/comparisonTable/ComparisonTableHead.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef, useEffect, useState } from 'react'
 import { Revision } from 'dvc/src/plots/webview/contract'
 import cx from 'classnames'
 import { useSelector } from 'react-redux'
@@ -28,6 +28,26 @@ export const ComparisonTableHead: React.FC<ComparisonTableHeadProps> = ({
     (state: PlotsState) => state.dragAndDrop.draggedRef?.itemId
   )
   const ribbonHeight = useSelector((state: PlotsState) => state.ribbon.height)
+  const [top, setTop] = useState(0)
+  const headRef = createRef<HTMLTableSectionElement>()
+
+  useEffect(() => {
+    const calculateTop = () => {
+      const headerPosition = headRef.current?.getBoundingClientRect().top || 0
+      const positionAdjustments = 51 - 4 // 4 is equal to the gap in the comparison table and 51 is the height of the section header
+      setTop(
+        headerPosition - positionAdjustments - 40 < 0
+          ? ribbonHeight - headerPosition + positionAdjustments
+          : 0
+      )
+    }
+
+    window.addEventListener('scroll', calculateTop)
+
+    return () => {
+      window.removeEventListener('scroll', calculateTop)
+    }
+  }, [ribbonHeight, headRef])
 
   const items = columns.map(({ revision, displayColor, group }) => {
     const isPinned = revision === pinnedColumn
@@ -39,7 +59,7 @@ export const ComparisonTableHead: React.FC<ComparisonTableHeadProps> = ({
           [styles.pinnedColumnHeader]: isPinned,
           [styles.draggedColumn]: draggedId === revision
         })}
-        style={{ top: ribbonHeight - 4 + 51 }} // 4 is equal to the gap in the comparison table and 51 is the height of the section header
+        style={{ top }}
       >
         <ComparisonTableHeader
           isPinned={isPinned}
@@ -54,7 +74,7 @@ export const ComparisonTableHead: React.FC<ComparisonTableHeadProps> = ({
   })
 
   return (
-    <thead data-testid="comparison-table-head">
+    <thead data-testid="comparison-table-head" ref={headRef}>
       <tr>
         <DragDropContainer
           order={columns.map(col => col.revision)}
