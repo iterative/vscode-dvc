8b37f1cbc30cb22d49e07e811bc57189bfdef166
Stephanie Roy <stephanie@iterative.ai>
2023-03-20 11:51:12 -0400

    Create multiple Redux actions for the table data

--- webview/src/experiments/components/App.tsx
+++ webview/src/experiments/components/App.tsx
@@ -6,7 +6,22 @@ import {
 } from 'dvc/src/webview/contract'
 import { TableData } from 'dvc/src/experiments/webview/contract'
 import Experiments from './Experiments'
-import { update } from './table/tableDataSlice'
+import {
+  update,
+  updateChanges,
+  updateColumnOrder,
+  updateColumns,
+  updateColumnWidths,
+  updateFilteredCounts,
+  updateFilters,
+  updateHasCheckpoints,
+  updateHasColumns,
+  updateHasConfig,
+  updateHasRunningExperiment,
+  updateHasValidDvcYaml,
+  updateRows,
+  updateSorts
+} from './table/tableDataSlice'
 import { useVsCodeMessaging } from '../../shared/hooks/useVsCodeMessaging'
 
 export const App: React.FC<Record<string, unknown>> = () => {
@@ -14,9 +29,57 @@ export const App: React.FC<Record<string, unknown>> = () => {
 
   useVsCodeMessaging(
     useCallback(
+      // eslint-disable-next-line sonarjs/cognitive-complexity
       ({ data }: { data: MessageToWebview<TableData> }) => {
         if (data.type === MessageToWebviewType.SET_DATA) {
-          dispatch(update(data.data))
+          dispatch(update(!!data.data))
+          for (const key of Object.keys(data.data)) {
+            switch (key) {
+              case 'changes':
+                dispatch(updateChanges(data.data.changes))
+                continue
+              case 'columnOrder':
+                dispatch(updateColumnOrder(data.data.columnOrder))
+                continue
+              case 'columns':
+                dispatch(updateColumns(data.data.columns))
+                continue
+              case 'columnsWidths':
+                dispatch(updateColumnWidths(data.data.columnWidths))
+                continue
+              case 'filteredCounts':
+                dispatch(updateFilteredCounts(data.data.filteredCounts))
+                continue
+              case 'filters':
+                dispatch(updateFilters(data.data.filters))
+                continue
+              case 'hasCheckpoints':
+                dispatch(updateHasCheckpoints(data.data.hasCheckpoints))
+                continue
+              case 'hasColumns':
+                dispatch(updateHasColumns(data.data.hasColumns))
+                continue
+              case 'hasConfig':
+                dispatch(updateHasConfig(data.data.hasConfig))
+                continue
+              case 'hasRunningExperiment':
+                dispatch(
+                  updateHasRunningExperiment(data.data.hasRunningExperiment)
+                )
+                continue
+              case 'hasValidDvcYaml':
+                dispatch(updateHasValidDvcYaml(data.data.hasValidDvcYaml))
+                continue
+              case 'rows':
+                dispatch(updateRows(data.data.rows))
+                continue
+              case 'sorts':
+                updateSorts(data.data.sorts)
+                continue
+              default:
+                continue
+            }
+          }
         }
       },
       [dispatch]
