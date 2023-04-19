e18ac4ce905154c0bcb4b499c0fb8b6314931c66
Julie G <43496356+julieg18@users.noreply.github.com>
2023-04-19 09:14:09 -0500

    Clean up shared styles (#3716)

--- webview/src/setup/index.tsx
+++ webview/src/setup/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../shared/style.scss'
+import '../shared/styles.scss'
 import { App } from './components/App'
 
 const root = ReactDOM.createRoot(document.querySelector('#root') as HTMLElement)
