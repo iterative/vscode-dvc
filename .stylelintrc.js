e7450653524ef3288abf967a67f52fbd1bfd5bf7
julieg18 <juliannagal18@gmail.com>
2023-04-21 11:18:42 -0500

    Try out stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'stylelint-config-standard-scss',
+  rules: {
+    'custom-property-pattern': null,
+    'selector-class-pattern': null,
+    'scss/percent-placeholder-pattern': null,
+    'selector-pseudo-class-no-unknown': null,
+    'scss/percent-placeholder-pattern': null
+  }
+}
