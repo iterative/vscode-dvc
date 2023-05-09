c526d269863cccab44695b861938c01fab1e78a0
julieg18 <juliannagal18@gmail.com>
2023-05-09 10:57:08 -0500

    Adjust front end tests

--- webview/src/setup/components/App.test.tsx
+++ webview/src/setup/components/App.test.tsx
@@ -3,7 +3,10 @@ import {
   MessageFromWebviewType,
   MessageToWebviewType
 } from 'dvc/src/webview/contract'
-import { MIN_CLI_VERSION } from 'dvc/src/cli/dvc/contract'
+import {
+  LATEST_TESTED_CLI_VERSION,
+  MIN_CLI_VERSION
+} from 'dvc/src/cli/dvc/contract'
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import { SetupSection, SetupData } from 'dvc/src/setup/webview/contract'
@@ -476,7 +479,7 @@ describe('App', () => {
       })
     })
 
-    it('should show the user the version if dvc is installed', () => {
+    it('should show the user the version, min version, and latested tested version if dvc is installed', () => {
       renderApp({
         canGitInitialize: false,
         cliCompatible: true,
@@ -496,10 +499,14 @@ describe('App', () => {
       })
 
       const envDetails = screen.getByTestId('dvc-env-details')
-      const command = `1.0.0 (required >= ${MIN_CLI_VERSION})`
+      const firstVersionLine = `1.0.0 (required >= ${MIN_CLI_VERSION})`
+      const secondVersionLine = `Extension has been tested on versions up to ${LATEST_TESTED_CLI_VERSION}.`
 
       expect(within(envDetails).getByText('Version')).toBeInTheDocument()
-      expect(within(envDetails).getByText(command)).toBeInTheDocument()
+      expect(within(envDetails).getByText(firstVersionLine)).toBeInTheDocument()
+      expect(
+        within(envDetails).getByText(secondVersionLine)
+      ).toBeInTheDocument()
     })
 
     it('should tell the user that version is not found if dvc is not installed', () => {
@@ -521,10 +528,10 @@ describe('App', () => {
         shareLiveToStudio: false
       })
       const envDetails = screen.getByTestId('dvc-env-details')
-      const command = `Not found (required >= ${MIN_CLI_VERSION})`
+      const version = `Not found (required >= ${MIN_CLI_VERSION})`
 
       expect(within(envDetails).getByText('Version')).toBeInTheDocument()
-      expect(within(envDetails).getByText(command)).toBeInTheDocument()
+      expect(within(envDetails).getByText(version)).toBeInTheDocument()
     })
 
     it('should show the user an example command if dvc is installed', () => {
