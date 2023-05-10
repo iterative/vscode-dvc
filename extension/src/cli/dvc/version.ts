3db8260dc405ab5afabbda2a21776a45ffe4e6d2
Julie G <43496356+julieg18@users.noreply.github.com>
2023-05-10 08:22:39 -0500

    Update DVC Setup Version Details (#3850)
    * remove information about max version in setup details
    * adds information about latest tested version setup details
    * removes latest tested version toast

--- extension/src/cli/dvc/version.ts
+++ extension/src/cli/dvc/version.ts
@@ -1,14 +1,9 @@
-import {
-  MAX_CLI_VERSION,
-  LATEST_TESTED_CLI_VERSION,
-  MIN_CLI_VERSION
-} from './contract'
+import { MAX_CLI_VERSION, MIN_CLI_VERSION } from './contract'
 
 export enum CliCompatible {
   NO_CANNOT_VERIFY = 'no-cannot-verify',
   NO_INCOMPATIBLE = 'no-incompatible',
   NO_NOT_FOUND = 'no-not-found',
-  YES_MINOR_VERSION_AHEAD_OF_TESTED = 'yes-minor-version-ahead-of-tested',
   YES = 'yes'
 }
 
@@ -21,21 +16,6 @@ export const extractSemver = (stdout: string): ParsedSemver | undefined => {
   }
   const [major, minor, patch] = semver[0].split('.')
   return { major: Number(major), minor: Number(minor), patch: Number(patch) }
-}
-
-const cliIsCompatible = (
-  currentMajor: number,
-  currentMinor: number
-): CliCompatible => {
-  const { major: latestTestedMajor, minor: latestTestedMinor } = extractSemver(
-    LATEST_TESTED_CLI_VERSION
-  ) as ParsedSemver
-
-  if (currentMajor === latestTestedMajor && currentMinor > latestTestedMinor) {
-    return CliCompatible.YES_MINOR_VERSION_AHEAD_OF_TESTED
-  }
-
-  return CliCompatible.YES
 }
 
 const checkCLIVersion = (currentSemVer: {
@@ -64,7 +44,7 @@ const checkCLIVersion = (currentSemVer: {
     return CliCompatible.NO_INCOMPATIBLE
   }
 
-  return cliIsCompatible(currentMajor, currentMinor)
+  return CliCompatible.YES
 }
 
 export const isVersionCompatible = (
