339124399889689a2a66ce73cfca98913d3ce1d5
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-05-18 09:26:23 +1000

    Fix typo in setup reducers (#3912)

--- webview/src/setup/components/App.tsx
+++ webview/src/setup/components/App.tsx
@@ -18,7 +18,7 @@ import {
   updateHasData as updateWebviewHasData
 } from '../state/webviewSlice'
 import {
-  updateCanGitInitalized,
+  updateCanGitInitialize,
   updateCliCompatible,
   updateDvcCliDetails,
   updateIsPythonExtensionUsed,
@@ -47,7 +47,7 @@ export const feedStore = (
   for (const key of Object.keys(data.data)) {
     switch (key) {
       case 'canGitInitialize':
-        dispatch(updateCanGitInitalized(data.data.canGitInitialize))
+        dispatch(updateCanGitInitialize(data.data.canGitInitialize))
         continue
       case 'cliCompatible':
         dispatch(updateCliCompatible(data.data.cliCompatible))
