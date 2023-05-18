53f93af3e0b28c5ce33ef664120b218ca6c30eda
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-05-19 07:06:03 +1000

    Fix test console errors (add tbody) (#3927)

--- webview/src/setup/components/remote/MultiProjectRemotes.tsx
+++ webview/src/setup/components/remote/MultiProjectRemotes.tsx
@@ -6,22 +6,24 @@ export const MultiProjectRemotes: React.FC<{
   remoteList: NonNullable<RemoteList>
 }> = ({ remoteList }) => (
   <table data-testid="remote-details" className={styles.remoteDetails}>
-    {Object.entries(remoteList).map(([dvcRoot, remotes]) => {
-      return remotes ? (
-        Object.entries(remotes).map(([alias, remote], i) => (
-          <tr key={[alias, remote].join('-')}>
-            {i === 0 ? <th className={styles.alias}>{dvcRoot}</th> : <th />}
-            <th className={styles.alias}>{alias}</th>
-            <th className={styles.remote}>{remote}</th>
+    <tbody>
+      {Object.entries(remoteList).map(([dvcRoot, remotes]) => {
+        return remotes ? (
+          Object.entries(remotes).map(([alias, remote], i) => (
+            <tr key={[alias, remote].join('-')}>
+              {i === 0 ? <th className={styles.alias}>{dvcRoot}</th> : <th />}
+              <th className={styles.alias}>{alias}</th>
+              <th className={styles.remote}>{remote}</th>
+            </tr>
+          ))
+        ) : (
+          <tr key={dvcRoot}>
+            <th className={styles.alias}>{dvcRoot}</th>
+            <th className={styles.empty}>-</th>
+            <th className={styles.empty}>-</th>
           </tr>
-        ))
-      ) : (
-        <tr key={dvcRoot}>
-          <th className={styles.alias}>{dvcRoot}</th>
-          <th className={styles.empty}>-</th>
-          <th className={styles.empty}>-</th>
-        </tr>
-      )
-    })}
+        )
+      })}
+    </tbody>
   </table>
 )
