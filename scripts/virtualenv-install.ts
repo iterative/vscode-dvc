7f21fb616aadcdfdcf42751b4a101c71de13c014
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-05-19 14:43:59 +1000

    Fix extension initialization on Windows (downgrade from esm only packages) (#3937)

---
+++ scripts/virtualenv-install.ts
@@ -0,0 +1,14 @@
+import { join, resolve } from 'path'
+require('dvc/src/vscode/mockModule')
+
+const importModuleAfterMockingVsCode = () => {
+  const { setupTestVenv } = require('dvc/src/python')
+
+  return setupTestVenv
+}
+
+const setupTestVenv = importModuleAfterMockingVsCode()
+
+const cwd = resolve(__dirname, '..', 'demo')
+
+setupTestVenv(cwd, '.env', '-r', join('.', 'requirements.txt'))
