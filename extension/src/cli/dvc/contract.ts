140a8108d35a07143c929202f8d567a28d0ab37a
Matt Seddon <mattseddon@hotmail.com>
2023-06-06 14:52:38 +1000

    Update demo project and latest tested CLI version (2.58.2)

--- extension/src/cli/dvc/contract.ts
+++ extension/src/cli/dvc/contract.ts
@@ -1,7 +1,7 @@
 import { Plot } from '../../plots/webview/contract'
 
 export const MIN_CLI_VERSION = '2.58.1'
-export const LATEST_TESTED_CLI_VERSION = '2.58.1'
+export const LATEST_TESTED_CLI_VERSION = '2.58.2'
 export const MAX_CLI_VERSION = '3'
 
 type ErrorContents = { type: string; msg: string }
