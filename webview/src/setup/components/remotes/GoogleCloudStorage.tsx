bfad0428b1fd7600ec3d18403d44e2a040a2f78e
Matt Seddon <mattseddon@hotmail.com>
2023-06-07 09:37:52 +1000

    Condense information on storage types for remote onboarding

--- webview/src/setup/components/remotes/GoogleCloudStorage.tsx
+++ webview/src/setup/components/remotes/GoogleCloudStorage.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import { CloudVersioning } from './CloudVersioning'
-import { CustomAuth } from './CustomAuth'
 import { ShowExtension } from './ShowExtension'
 import { Icon } from '../../../shared/components/Icon'
 import { Warning } from '../../../shared/components/icons'
 
 export const GoogleCloudStorage: React.FC = () => (
   <div className={styles.storageDetails}>
-    <p>
-      When needed, DVC will try to authenticate using your{' '}
-      <a href="https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html">
-        glcoud CLI authorization
-      </a>
-      . This reads the default GCP key file.
-    </p>
     <ShowExtension
       id="GoogleCloudTools.cloudcode"
       name="Google Cloud Code"
       capabilities="create projects and provides snippets for working with the Cloud Storage API"
     />
+    <p>
+      When needed, DVC will try to authenticate using your{' '}
+      <a href="https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html">
+        glcoud CLI authorization
+      </a>
+      . This reads the default GCP key file. For custom authentication see the{' '}
+      <a href="https://dvc.org/doc/user-guide/data-management/remote-storage/google-cloud-storage#custom-authentication">
+        docs
+      </a>
+      .
+    </p>
     <p>
       <Icon icon={Warning} width={16} height={16} className={styles.warnIcon} />{' '}
       Make sure to run{' '}
@@ -29,12 +31,5 @@ export const GoogleCloudStorage: React.FC = () => (
       unless you use a service account or other ways to authenticate (
       <a href="https://stackoverflow.com/a/53307505/298182">more info</a>).
     </p>
-    <CloudVersioning>
-      <a href="https://cloud.google.com/storage/docs/object-versioning">
-        Object versioning
-      </a>{' '}
-      enabled on the bucket.
-    </CloudVersioning>
-    <CustomAuth href="https://dvc.org/doc/user-guide/data-management/remote-storage/google-cloud-storage#custom-authentication" />
   </div>
 )
