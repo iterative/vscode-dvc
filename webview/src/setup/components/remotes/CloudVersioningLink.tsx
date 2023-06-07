95bed87bf9a041a789ecfd035cb4829ac35f59a0
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2023-06-08 05:02:25 +1000

    Condense information on storage types for remote onboarding (#4051)

--- webview/src/setup/components/remotes/CloudVersioningLink.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CloudVersioningLink: React.FC = () => (
-  <a href="https://dvc.org/doc/user-guide/data-management/cloud-versioning">
-    Cloud versioning
-  </a>
-)
