00d905b774f86bc7bc7e45d41a9a0e5b90c557ac
julieg18 <juliannagal18@gmail.com>
2023-07-28 12:04:35 -0500

    Fix slider breaking for missing images

--- webview/src/plots/components/comparisonTable/cell/ComparisonTableMultiCell.tsx
+++ webview/src/plots/components/comparisonTable/cell/ComparisonTableMultiCell.tsx
@@ -37,52 +37,7 @@ export const ComparisonTableMultiCell: React.FC<{
     e.stopPropagation()
   }, [])
 
-  const slider = (
-    // The div element has children that allow keyboard interaction
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div
-      className={styles.multiImageSlider}
-      onMouseEnter={addDisabled}
-      onMouseLeave={removeDisabled}
-      onClick={disableClick}
-      onKeyDown={disableClick}
-    >
-      <label htmlFor={`${plot.id}-step`}>Step</label>
-      <input
-        name={`${plot.id}-step`}
-        min="0"
-        max={plot.imgs.length - 1}
-        value={currentStep}
-        type="range"
-        onChange={event => {
-          dispatch(
-            setMultiPlotValue({ path, value: Number(event.target.value) })
-          )
-        }}
-      />
-      <p>{currentStep}</p>
-    </div>
-  )
-
-  if (loading) {
-    return (
-      <div className={styles.multiImageWrapper}>
-        <ComparisonTableLoadingCell />
-        {slider}
-      </div>
-    )
-  }
-
-  if (missing) {
-    return (
-      <div className={styles.multiImageWrapper}>
-        <ComparisonTableMissingCell plot={plot.imgs[currentStep]} />
-        {slider}
-      </div>
-    )
-  }
-
-  return (
+  let imageContent = (
     <button
       className={cx(styles.imageWrapper, styles.multiImageWrapper)}
       onClick={() => url && zoomPlot(url)}
@@ -94,7 +49,44 @@ export const ComparisonTableMultiCell: React.FC<{
         src={url}
         alt={`${currentStep} of ${path} (${plot.id})`}
       />
-      {slider}
     </button>
+  )
+
+  if (loading) {
+    imageContent = <ComparisonTableLoadingCell />
+  }
+
+  if (missing) {
+    imageContent = <ComparisonTableMissingCell plot={plot.imgs[currentStep]} />
+  }
+
+  return (
+    <div className={styles.multiImageWrapper}>
+      {imageContent}
+      {/* The div element has children that allow keyboard interaction */}
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+      <div
+        className={styles.multiImageSlider}
+        onMouseEnter={addDisabled}
+        onMouseLeave={removeDisabled}
+        onClick={disableClick}
+        onKeyDown={disableClick}
+      >
+        <label htmlFor={`${plot.id}-step`}>Step</label>
+        <input
+          name={`${plot.id}-step`}
+          min="0"
+          max={plot.imgs.length - 1}
+          value={currentStep}
+          type="range"
+          onChange={event => {
+            dispatch(
+              setMultiPlotValue({ path, value: Number(event.target.value) })
+            )
+          }}
+        />
+        <p>{currentStep}</p>
+      </div>
+    </div>
   )
 }
