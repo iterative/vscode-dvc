a37c549c3570f16de7021947eefcdc555aefcc0e
Matt Seddon <mattseddon@hotmail.com>
2023-08-02 13:57:52 +1000

    add experiment webview tests

--- extension/src/test/suite/experiments/index.test.ts
+++ extension/src/test/suite/experiments/index.test.ts
@@ -1297,6 +1297,28 @@ suite('Experiments Test Suite', () => {
       expect(mockShowPlots).to.be.calledWith(dvcDemoPath)
     }).timeout(WEBVIEW_TEST_TIMEOUT)
 
+    it.only(
+      'should be able to handle a message to toggle only changed columns',
+      async () => {
+        const { columnsModel, messageSpy, mockMessageReceived } =
+          await buildExperimentsWebview({
+            disposer: disposable
+          })
+
+        expect(columnsModel.getShowOnlyChanged()).to.be.false
+        messageSpy.resetHistory()
+
+        mockMessageReceived.fire({
+          type: MessageFromWebviewType.TOGGLE_SHOW_ONLY_CHANGED
+        })
+
+        expect(columnsModel.getShowOnlyChanged()).to.be.true
+        expect(messageSpy).to.be.calledWithMatch({
+          showOnlyChanged: true
+        })
+      }
+    ).timeout(WEBVIEW_TEST_TIMEOUT)
+
     it('should handle a message to stop experiments running', async () => {
       const { experiments, dvcExecutor } =
         stubWorkspaceExperimentsGetters(disposable)
