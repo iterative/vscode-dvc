b125766ecad1b183c049bef8034ea389e1ebb3ff
Matt Seddon <mattseddon@hotmail.com>
2023-08-07 13:14:12 +1000

    Fix Created column presence in show only changed

--- extension/src/experiments/columns/collect/index.ts
+++ extension/src/experiments/columns/collect/index.ts
@@ -7,7 +7,7 @@ import {
   collectMetricAndParamChanges,
   collectMetricsAndParams
 } from './metricsAndParams'
-import { Column, Commit, Experiment } from '../../webview/contract'
+import { Column, ColumnType, Commit, Experiment } from '../../webview/contract'
 import {
   ExpRange,
   ExpShowOutput,
@@ -173,7 +173,12 @@ const collectChangedPaths = (
   records: (Commit | Experiment)[]
 ) => {
   const acc: string[] = []
-  for (const { pathArray, path, hasChildren } of columns) {
+  for (const { type, pathArray, path, hasChildren } of columns) {
+    if (type === ColumnType.TIMESTAMP) {
+      collectChangedPath(acc, path, [path], records)
+      continue
+    }
+
     if (!pathArray || hasChildren) {
       continue
     }
