ad5cef3fe1183f1b68ea44fb04f5676f9f81ab40
julieg18 <juliannagal18@gmail.com>
2023-09-28 12:19:29 -0500

    add frontend styles

--- webview/src/experiments/components/table/content/BranchCellContent.tsx
+++ webview/src/experiments/components/table/content/BranchCellContent.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import cx from 'classnames'
 import { Experiment } from 'dvc/src/experiments/webview/contract'
 import { CellContext } from '@tanstack/react-table'
 import { CellValue } from './Cell'
 import styles from '../styles.module.scss'
 import { Icon } from '../../../../shared/components/Icon'
 import { GitMerge } from '../../../../shared/components/icons'
+import Tooltip, {
+  NORMAL_TOOLTIP_DELAY
+} from '../../../../shared/components/tooltip/Tooltip'
 
 export const BranchCellContent: React.FC<
   CellContext<Experiment, CellValue>
 > = cell => {
   const {
     row: {
-      original: { branch }
+      original: { branch, allBranches = [] }
     }
   } = cell as unknown as CellContext<Experiment, CellValue>
 
@@ -24,15 +28,36 @@ export const BranchCellContent: React.FC<
     )
   }
 
+  const hasMultiBranches = allBranches.length > 1
+
   return (
-    <div
-      className={styles.branchInnerCell}
-      style={{ width: cell.column.getSize() }}
+    <Tooltip
+      content={allBranches.join(', ')}
+      placement="bottom-end"
+      delay={NORMAL_TOOLTIP_DELAY}
     >
-      <div className={styles.cellContents}>
-        <Icon icon={GitMerge} width={13} height={13} />
-        <span>{branch}</span>
+      <div
+        className={styles.branchInnerCell}
+        style={{ width: cell.column.getSize() }}
+      >
+        <div className={styles.cellContents}>
+          <Icon icon={GitMerge} width={13} height={13} />
+          <span
+            className={cx(
+              styles.branchCellText,
+              hasMultiBranches && styles.branchCellTextMultiLine
+            )}
+          >
+            <span>{branch}</span>
+            {hasMultiBranches && (
+              <span>
+                {allBranches[1]}
+                {allBranches.length > 2 && ` + ${allBranches.length - 2} more`}
+              </span>
+            )}
+          </span>
+        </div>
       </div>
-    </div>
+    </Tooltip>
   )
 }
