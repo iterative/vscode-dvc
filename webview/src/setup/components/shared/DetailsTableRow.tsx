4114045bcd287c5fd247e4f9c77bf3b0c01c8a5f
julieg18 <juliannagal18@gmail.com>
2023-12-08 11:43:13 -0600

    Move dvc table into reusable component

---
+++ webview/src/setup/components/shared/DetailsTableRow.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export type DetailsTableRowActions = { text: string; onClick: () => void }[]
+
+type DetailsTableRowProps = {
+  title: string
+  text: string
+  actions?: DetailsTableRowActions
+}
+
+export const DetailsTableRow: React.FC<DetailsTableRowProps> = ({
+  title,
+  text,
+  actions
+}) => {
+  return (
+    <tr>
+      <td className={styles.detailsTableKey}>{title}:</td>
+      <td className={styles.detailsTableValue}>
+        {text}
+        {actions && (
+          <span>
+            {actions.map(({ text, onClick }, index) => (
+              <>
+                {index > 0 && <span className={styles.separator} />}
+                <button className={styles.buttonAsLink} onClick={onClick}>
+                  {text}
+                </button>
+              </>
+            ))}
+          </span>
+        )}
+      </td>
+    </tr>
+  )
+}
