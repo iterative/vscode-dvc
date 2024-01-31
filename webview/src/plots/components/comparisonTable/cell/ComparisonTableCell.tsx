a3f69e9d124fdfab67796b4e50d6ec781282e111
julieg18 <juliannagal18@gmail.com>
2024-01-31 16:34:11 -0600

    Apply review comments

--- webview/src/plots/components/comparisonTable/cell/ComparisonTableCell.tsx
+++ webview/src/plots/components/comparisonTable/cell/ComparisonTableCell.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { createSelector } from '@reduxjs/toolkit'
 import {
   ComparisonClassDetails,
-  ComparisonPlotClasses,
   ComparisonPlot
 } from 'dvc/src/plots/webview/contract'
 import { ComparisonTableLoadingCell } from './ComparisonTableLoadingCell'
@@ -11,14 +8,6 @@ import { ComparisonTableMissingCell } from './ComparisonTableMissingCell'
 import { ComparisonTableBoundingBoxImg } from './ComparisonTableBoundingBoxImg'
 import styles from '../styles.module.scss'
 import { zoomPlot } from '../../../util/messages'
-import { PlotsState } from '../../../store'
-
-const selectPlotClasses = (state: PlotsState) => state.comparison.plotClasses
-const selectClasses = createSelector(
-  [selectPlotClasses, (_, id: string) => id, (_, id, path: string) => path],
-  (plotClasses: ComparisonPlotClasses, id: string, path: string) =>
-    plotClasses[id]?.[path]
-)
 
 export const ComparisonTableCell: React.FC<{
   path: string
@@ -27,9 +16,6 @@ export const ComparisonTableCell: React.FC<{
   imgAlt?: string
 }> = ({ path, plot, imgAlt, classDetails }) => {
   const plotImg = plot.imgs[0]
-  const classes = useSelector((state: PlotsState) =>
-    selectClasses(state, plot.id, path)
-  )
 
   const loading = plotImg.loading
   const missing = !loading && !plotImg.url
@@ -49,10 +35,11 @@ export const ComparisonTableCell: React.FC<{
       onClick={() => zoomPlot(plotImg.url)}
       data-testid="image-plot-button"
     >
-      {plotImg.url && classes ? (
+      {plotImg.url && Object.keys(classDetails).length > 0 ? (
         <ComparisonTableBoundingBoxImg
           src={plotImg.url}
-          classes={classes}
+          id={plot.id}
+          path={path}
           classDetails={classDetails}
           alt={alt}
         />
