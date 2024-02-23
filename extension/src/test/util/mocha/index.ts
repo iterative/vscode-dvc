9293b610ffccb0105f0fd3f0971fa759fa8a335d
Matt Seddon <mattseddon@hotmail.com>
2024-02-23 14:59:38 +1100

    smash face on keyboard until tests work

--- extension/src/test/util/mocha/index.ts
+++ extension/src/test/util/mocha/index.ts
@@ -1,6 +1,6 @@
 import { resolve as resolvePath } from 'path'
 import Mocha from 'mocha'
-import glob from 'glob'
+import { glob } from 'glob'
 import chai from 'chai'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
@@ -29,28 +29,27 @@ export const runMocha = async (
   try {
     // eslint-disable-next-line sonarjs/cognitive-complexity
     await new Promise<void>((resolve, reject) => {
-      glob(`**/**.test.${ext}`, { cwd }, (err, files) => {
-        if (err) {
-          return reject(err)
-        }
+      glob(`**/**.test.${ext}`, { cwd }).then(
+        files => {
+          for (const f of files) {
+            mocha.addFile(resolvePath(cwd, f))
+          }
 
-        for (const f of files) {
-          mocha.addFile(resolvePath(cwd, f))
-        }
-
-        try {
-          mocha.run(failures => {
-            if (failures > 0) {
-              reject(new Error(`${failures} tests failed.`))
-            } else {
-              resolve()
-            }
-          })
-        } catch (error: unknown) {
-          Logger.error((error as Error).toString())
-          throw error
-        }
-      })
+          try {
+            mocha.run(failures => {
+              if (failures > 0) {
+                reject(new Error(`${failures} tests failed.`))
+              } else {
+                resolve()
+              }
+            })
+          } catch (error: unknown) {
+            Logger.error((error as Error).toString())
+            throw error
+          }
+        },
+        error => reject(error)
+      )
     })
   } finally {
     await teardown()
