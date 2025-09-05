ee981161067ffa272936128e523930c2c8285041
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-09-05 12:18:47 +0545

    pin vscode to 1.100.3

--- extension/src/test/e2e/wdio.conf.ts
+++ extension/src/test/e2e/wdio.conf.ts
@@ -47,7 +47,7 @@ export const config: Options.Testrunner = {
   capabilities: [
     {
       browserName: 'vscode',
-      browserVersion: 'stable',
+      browserVersion: '1.100.3',
       'wdio:vscodeOptions': {
         extensionPath,
         userSettings: {
